f0f515ac36d50f9d19290003dcc7019d80e13d45
Chris Alberti <chrisalberti@google.com>
2016-02-25 16:06:01 -0500

    Reducing size of parser_ops.so.

--- syntaxnet/neurosis/load_parser_ops.py
+++ syntaxnet/neurosis/load_parser_ops.py
@@ -3,7 +3,6 @@
 import os.path
 import tensorflow as tf
 
-parser_ops_module = tf.load_op_library(
+tf.load_op_library(
     os.path.join(tf.resource_loader.get_data_files_path(),
                  'parser_ops.so'))
-parser_ops = _parser_ops_module.parser_ops
