58eafd13e5ab10eb923c17afe1cd659c522ef8c8
Chris Alberti <chrisalberti@google.com>
2016-02-25 16:36:10 -0500

    Adding copyright notices.

--- syntaxnet/util/utf8/unilib_utf8_utils.h
+++ syntaxnet/util/utf8/unilib_utf8_utils.h
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef UTIL_UTF8_PUBLIC_UNILIB_UTF8_UTILS_H_
 #define UTIL_UTF8_PUBLIC_UNILIB_UTF8_UTILS_H_
 
