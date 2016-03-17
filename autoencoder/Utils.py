70702f79c7817b09fb87fef7729478af58532870
Jiří Vahala <snurkabill@gmail.com>
2016-03-17 21:59:29 +0100

    Autoencoders

---
+++ autoencoder/Utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensorflow as tf
+
+def xavier_init(fan_in, fan_out, constant = 1):
+    low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
+    high = constant * np.sqrt(6.0 / (fan_in + fan_out))
+    return tf.random_uniform((fan_in, fan_out),
+                             minval = low, maxval = high,
+                             dtype = tf.float32)
