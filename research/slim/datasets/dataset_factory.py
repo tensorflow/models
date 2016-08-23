19323116499f0bab92d204121cf2afae28260882
nsilberman <nsilberman@google.com>
2016-08-23 10:52:33 -0700

    PiperOrigin-RevId: 131073198

--- research/slim/datasets/dataset_factory.py
+++ research/slim/datasets/dataset_factory.py
@@ -20,10 +20,12 @@ from __future__ import print_function
 
 import google3
 from datasets import cifar10
+from datasets import imagenet
 from datasets import mnist
 
 datasets_map = {
     'cifar10': cifar10,
+    'imagenet': imagenet,
     'mnist': mnist,
 }
 
@@ -47,4 +49,8 @@ def get_dataset(name, split_name, dataset_dir, file_pattern=None, reader=None):
   """
   if name not in datasets_map:
     raise ValueError('Name of dataset unknown %s' % name)
-  return datasets_map[name].get_split(split_name, dataset_dir, file_pattern)
+  return datasets_map[name].get_split(
+      split_name,
+      dataset_dir,
+      file_pattern,
+      reader)
