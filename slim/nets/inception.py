2689758e369b6867cc6933a461517cdbbc74a2b6
Nathan Silberman <nsilberman@google.com>
2016-08-30 00:01:19 -0400

    Added jupyter notebook, added finetuning scripts, added inception_resnet_v2

--- slim/nets/inception.py
+++ slim/nets/inception.py
@@ -19,6 +19,8 @@ from __future__ import division
 from __future__ import print_function
 
 # pylint: disable=unused-import
+from nets.inception_resnet_v2 import inception_resnet_v2
+from nets.inception_resnet_v2 import inception_resnet_v2_arg_scope
 from nets.inception_v1 import inception_v1
 from nets.inception_v1 import inception_v1_arg_scope
 from nets.inception_v1 import inception_v1_base
