1f50270eeaeb9b19b9c3ca69e74e8517c3cb71db
amirbar <amirb4r@gmail.com>
2016-10-01 13:08:31 +0300

    Fix issue #459

--- slim/nets/resnet_v1.py
+++ slim/nets/resnet_v1.py
@@ -249,7 +249,7 @@ def resnet_v1_101(inputs,
   return resnet_v1(inputs, blocks, num_classes, is_training,
                    global_pool=global_pool, output_stride=output_stride,
                    include_root_block=True, reuse=reuse, scope=scope)
-
+resnet_v1_101.default_image_size = resnet_v1.default_image_size
 
 def resnet_v1_152(inputs,
                   num_classes=None,
@@ -271,7 +271,7 @@ def resnet_v1_152(inputs,
   return resnet_v1(inputs, blocks, num_classes, is_training,
                    global_pool=global_pool, output_stride=output_stride,
                    include_root_block=True, reuse=reuse, scope=scope)
-
+resnet_v1_152.default_image_size = resnet_v1.default_image_size
 
 def resnet_v1_200(inputs,
                   num_classes=None,
@@ -293,3 +293,4 @@ def resnet_v1_200(inputs,
   return resnet_v1(inputs, blocks, num_classes, is_training,
                    global_pool=global_pool, output_stride=output_stride,
                    include_root_block=True, reuse=reuse, scope=scope)
+resnet_v1_200.default_image_size = resnet_v1.default_image_size
