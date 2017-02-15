634479399664e6a908e6e68ef13ffabf0b49f33e
Andrew Selle <aselle@andyselle.com>
2017-02-14 17:28:07 -0800

    Update concat_v2 to be concat to match 1.0 final
    Fixes #1014

--- resnet/cifar_input.py
+++ resnet/cifar_input.py
@@ -100,7 +100,7 @@ def build_input(dataset, data_path, batch_size, mode):
   labels = tf.reshape(labels, [batch_size, 1])
   indices = tf.reshape(tf.range(0, batch_size, 1), [batch_size, 1])
   labels = tf.sparse_to_dense(
-      tf.concat_v2(values=[indices, labels], axis=1),
+      tf.concat(values=[indices, labels], axis=1),
       [batch_size, num_classes], 1.0, 0.0)
 
   assert len(images.get_shape()) == 4
