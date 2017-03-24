5382c526c20b44b21e61e88f43d0cae9b1e418c3
TYL <ljcmy1@gmail.com>
2017-03-24 17:42:43 +0800

    Update compat.py

--- textsum/compat.py
+++ textsum/compat.py
@@ -3,9 +3,9 @@ Usage:
 Add the following code in the other scripts for the compatibility with Python 3,
 'from compat import *'
 Prerequisites:
-For Python 3, you need "pip install future"
+For Python 3, you need "pip install six"
 """
 import sys
 if sys.version_info.major == 3:
     import queue as Queue
-    from past.builtins import xrange # NEED pip install future
+    from six.moves import xrange # NEED pip install six
