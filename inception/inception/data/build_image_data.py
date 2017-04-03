a4529759bc75e5c442b64bf2e02c97ffd64cd226
Mahmoud Lababidi <lababidi@gmail.com>
2017-04-03 11:42:53 -0400

    add binary to file reading

--- inception/inception/data/build_image_data.py
+++ inception/inception/data/build_image_data.py
@@ -198,7 +198,7 @@ def _process_image(filename, coder):
     width: integer, image width in pixels.
   """
   # Read the image file.
-  with tf.gfile.FastGFile(filename, 'r') as f:
+  with tf.gfile.FastGFile(filename, 'rb') as f:
     image_data = f.read()
 
   # Convert any PNG to JPEG's for consistency.
