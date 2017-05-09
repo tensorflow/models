3d3978c827898e8ceae515d3890a272eb4c2c9c4
Sergio Guadarrama <sguada@google.com>
2017-05-09 07:24:46 -0700

    PiperOrigin-RevId: 155501408

--- research/slim/scripts/train_cifarnet_on_cifar10.sh
+++ research/slim/scripts/train_cifarnet_on_cifar10.sh
@@ -7,7 +7,7 @@
 #
 # Usage:
 # cd slim
-# ./scripts/train_cifar_net_on_mnist.sh
+# ./scripts/train_cifarnet_on_cifar10.sh
 
 # Where the checkpoint and logs will be saved to.
 TRAIN_DIR=/tmp/cifarnet-model
