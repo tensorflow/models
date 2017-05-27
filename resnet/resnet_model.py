08012728c520896bbd3e129b107039a6c150ede3
Kongsea <kongsea@gmail.com>
2017-05-27 16:26:51 +0800

    Update resnet_model.py
    Improve line 54 from self.is_training = True is self.mode == 'train' else False to self.is_training = self.mode == 'train'.

--- resnet/resnet_model.py
+++ resnet/resnet_model.py
@@ -51,7 +51,7 @@ class ResNet(object):
     self._images = images
     self.labels = labels
     self.mode = mode
-    self.is_training = True if self.mode == 'train' else False
+    self.is_training = self.mode == 'train'
 
     self._extra_train_ops = []
 
