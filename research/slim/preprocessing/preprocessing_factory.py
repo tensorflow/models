0cf9932fde23c1714483309fcabd66a501a16399
howarda <howarda@google.com>
2017-05-31 18:22:19 -0700

    PiperOrigin-RevId: 157662895

--- research/slim/preprocessing/preprocessing_factory.py
+++ research/slim/preprocessing/preprocessing_factory.py
@@ -54,6 +54,7 @@ def get_preprocessing(name, is_training=False):
       'inception_v4': inception_preprocessing,
       'inception_resnet_v2': inception_preprocessing,
       'lenet': lenet_preprocessing,
+      'mobilenet_v1': inception_preprocessing,
       'resnet_v1_50': vgg_preprocessing,
       'resnet_v1_101': vgg_preprocessing,
       'resnet_v1_152': vgg_preprocessing,
