890b8db3d98144ccc458b1a64b72afb9505ca7ec
iramirsina <amirsina.torfi@gmail.com>
2017-06-03 11:00:27 -0400

    add accuracy

--- slim/train_image_classifier.py
+++ slim/train_image_classifier.py
@@ -219,161 +219,161 @@ FLAGS = tf.app.flags.FLAGS
 
 
 def _configure_learning_rate(num_samples_per_epoch, global_step):
-    """Configures the learning rate.
+  """Configures the learning rate.
 
-    Args:
-      num_samples_per_epoch: The number of samples in each epoch of training.
-      global_step: The global_step tensor.
+  Args:
+    num_samples_per_epoch: The number of samples in each epoch of training.
+    global_step: The global_step tensor.
 
-    Returns:
-      A `Tensor` representing the learning rate.
+  Returns:
+    A `Tensor` representing the learning rate.
 
-    Raises:
-      ValueError: if
-    """
-    decay_steps = int(num_samples_per_epoch / FLAGS.batch_size *
-                      FLAGS.num_epochs_per_decay)
-    if FLAGS.sync_replicas:
-        decay_steps /= FLAGS.replicas_to_aggregate
+  Raises:
+    ValueError: if
+  """
+  decay_steps = int(num_samples_per_epoch / FLAGS.batch_size *
+                    FLAGS.num_epochs_per_decay)
+  if FLAGS.sync_replicas:
+    decay_steps /= FLAGS.replicas_to_aggregate
 
-    if FLAGS.learning_rate_decay_type == 'exponential':
-        return tf.train.exponential_decay(FLAGS.learning_rate,
-                                          global_step,
-                                          decay_steps,
-                                          FLAGS.learning_rate_decay_factor,
-                                          staircase=True,
-                                          name='exponential_decay_learning_rate')
-    elif FLAGS.learning_rate_decay_type == 'fixed':
-        return tf.constant(FLAGS.learning_rate, name='fixed_learning_rate')
-    elif FLAGS.learning_rate_decay_type == 'polynomial':
-        return tf.train.polynomial_decay(FLAGS.learning_rate,
-                                         global_step,
-                                         decay_steps,
-                                         FLAGS.end_learning_rate,
-                                         power=1.0,
-                                         cycle=False,
-                                         name='polynomial_decay_learning_rate')
-    else:
-        raise ValueError('learning_rate_decay_type [%s] was not recognized',
-                         FLAGS.learning_rate_decay_type)
+  if FLAGS.learning_rate_decay_type == 'exponential':
+    return tf.train.exponential_decay(FLAGS.learning_rate,
+                                      global_step,
+                                      decay_steps,
+                                      FLAGS.learning_rate_decay_factor,
+                                      staircase=True,
+                                      name='exponential_decay_learning_rate')
+  elif FLAGS.learning_rate_decay_type == 'fixed':
+    return tf.constant(FLAGS.learning_rate, name='fixed_learning_rate')
+  elif FLAGS.learning_rate_decay_type == 'polynomial':
+    return tf.train.polynomial_decay(FLAGS.learning_rate,
+                                     global_step,
+                                     decay_steps,
+                                     FLAGS.end_learning_rate,
+                                     power=1.0,
+                                     cycle=False,
+                                     name='polynomial_decay_learning_rate')
+  else:
+    raise ValueError('learning_rate_decay_type [%s] was not recognized',
+                     FLAGS.learning_rate_decay_type)
 
 
 def _configure_optimizer(learning_rate):
-    """Configures the optimizer used for training.
+  """Configures the optimizer used for training.
 
-    Args:
-      learning_rate: A scalar or `Tensor` learning rate.
+  Args:
+    learning_rate: A scalar or `Tensor` learning rate.
 
-    Returns:
-      An instance of an optimizer.
+  Returns:
+    An instance of an optimizer.
 
-    Raises:
-      ValueError: if FLAGS.optimizer is not recognized.
-    """
-    if FLAGS.optimizer == 'adadelta':
-        optimizer = tf.train.AdadeltaOptimizer(
-            learning_rate,
-            rho=FLAGS.adadelta_rho,
-            epsilon=FLAGS.opt_epsilon)
-    elif FLAGS.optimizer == 'adagrad':
-        optimizer = tf.train.AdagradOptimizer(
-            learning_rate,
-            initial_accumulator_value=FLAGS.adagrad_initial_accumulator_value)
-    elif FLAGS.optimizer == 'adam':
-        optimizer = tf.train.AdamOptimizer(
-            learning_rate,
-            beta1=FLAGS.adam_beta1,
-            beta2=FLAGS.adam_beta2,
-            epsilon=FLAGS.opt_epsilon)
-    elif FLAGS.optimizer == 'ftrl':
-        optimizer = tf.train.FtrlOptimizer(
-            learning_rate,
-            learning_rate_power=FLAGS.ftrl_learning_rate_power,
-            initial_accumulator_value=FLAGS.ftrl_initial_accumulator_value,
-            l1_regularization_strength=FLAGS.ftrl_l1,
-            l2_regularization_strength=FLAGS.ftrl_l2)
-    elif FLAGS.optimizer == 'momentum':
-        optimizer = tf.train.MomentumOptimizer(
-            learning_rate,
-            momentum=FLAGS.momentum,
-            name='Momentum')
-    elif FLAGS.optimizer == 'rmsprop':
-        optimizer = tf.train.RMSPropOptimizer(
-            learning_rate,
-            decay=FLAGS.rmsprop_decay,
-            momentum=FLAGS.momentum,
-            epsilon=FLAGS.opt_epsilon)
-    elif FLAGS.optimizer == 'sgd':
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    else:
-        raise ValueError('Optimizer [%s] was not recognized', FLAGS.optimizer)
-    return optimizer
+  Raises:
+    ValueError: if FLAGS.optimizer is not recognized.
+  """
+  if FLAGS.optimizer == 'adadelta':
+    optimizer = tf.train.AdadeltaOptimizer(
+        learning_rate,
+        rho=FLAGS.adadelta_rho,
+        epsilon=FLAGS.opt_epsilon)
+  elif FLAGS.optimizer == 'adagrad':
+    optimizer = tf.train.AdagradOptimizer(
+        learning_rate,
+        initial_accumulator_value=FLAGS.adagrad_initial_accumulator_value)
+  elif FLAGS.optimizer == 'adam':
+    optimizer = tf.train.AdamOptimizer(
+        learning_rate,
+        beta1=FLAGS.adam_beta1,
+        beta2=FLAGS.adam_beta2,
+        epsilon=FLAGS.opt_epsilon)
+  elif FLAGS.optimizer == 'ftrl':
+    optimizer = tf.train.FtrlOptimizer(
+        learning_rate,
+        learning_rate_power=FLAGS.ftrl_learning_rate_power,
+        initial_accumulator_value=FLAGS.ftrl_initial_accumulator_value,
+        l1_regularization_strength=FLAGS.ftrl_l1,
+        l2_regularization_strength=FLAGS.ftrl_l2)
+  elif FLAGS.optimizer == 'momentum':
+    optimizer = tf.train.MomentumOptimizer(
+        learning_rate,
+        momentum=FLAGS.momentum,
+        name='Momentum')
+  elif FLAGS.optimizer == 'rmsprop':
+    optimizer = tf.train.RMSPropOptimizer(
+        learning_rate,
+        decay=FLAGS.rmsprop_decay,
+        momentum=FLAGS.momentum,
+        epsilon=FLAGS.opt_epsilon)
+  elif FLAGS.optimizer == 'sgd':
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+  else:
+    raise ValueError('Optimizer [%s] was not recognized', FLAGS.optimizer)
+  return optimizer
 
 def _get_init_fn():
-    """Returns a function run by the chief worker to warm-start the training.
+  """Returns a function run by the chief worker to warm-start the training.
 
-    Note that the init_fn is only run when initializing the model during the very
-    first global step.
+  Note that the init_fn is only run when initializing the model during the very
+  first global step.
 
-    Returns:
-      An init function run by the supervisor.
-    """
-    if FLAGS.checkpoint_path is None:
-        return None
+  Returns:
+    An init function run by the supervisor.
+  """
+  if FLAGS.checkpoint_path is None:
+    return None
 
-    # Warn the user if a checkpoint exists in the train_dir. Then we'll be
-    # ignoring the checkpoint anyway.
-    if tf.train.latest_checkpoint(FLAGS.train_dir):
-        tf.logging.info(
-            'Ignoring --checkpoint_path because a checkpoint already exists in %s'
-            % FLAGS.train_dir)
-        return None
+  # Warn the user if a checkpoint exists in the train_dir. Then we'll be
+  # ignoring the checkpoint anyway.
+  if tf.train.latest_checkpoint(FLAGS.train_dir):
+    tf.logging.info(
+        'Ignoring --checkpoint_path because a checkpoint already exists in %s'
+        % FLAGS.train_dir)
+    return None
 
-    exclusions = []
-    if FLAGS.checkpoint_exclude_scopes:
-        exclusions = [scope.strip()
-                      for scope in FLAGS.checkpoint_exclude_scopes.split(',')]
+  exclusions = []
+  if FLAGS.checkpoint_exclude_scopes:
+    exclusions = [scope.strip()
+                  for scope in FLAGS.checkpoint_exclude_scopes.split(',')]
 
-    # TODO(sguada) variables.filter_variables()
-    variables_to_restore = []
-    for var in slim.get_model_variables():
-        excluded = False
-        for exclusion in exclusions:
-            if var.op.name.startswith(exclusion):
-                excluded = True
-                break
-        if not excluded:
-            variables_to_restore.append(var)
+  # TODO(sguada) variables.filter_variables()
+  variables_to_restore = []
+  for var in slim.get_model_variables():
+    excluded = False
+    for exclusion in exclusions:
+      if var.op.name.startswith(exclusion):
+        excluded = True
+        break
+    if not excluded:
+      variables_to_restore.append(var)
 
-    if tf.gfile.IsDirectory(FLAGS.checkpoint_path):
-        checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
-    else:
-        checkpoint_path = FLAGS.checkpoint_path
+  if tf.gfile.IsDirectory(FLAGS.checkpoint_path):
+    checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
+  else:
+    checkpoint_path = FLAGS.checkpoint_path
 
-    tf.logging.info('Fine-tuning from %s' % checkpoint_path)
+  tf.logging.info('Fine-tuning from %s' % checkpoint_path)
 
-    return slim.assign_from_checkpoint_fn(
-        checkpoint_path,
-        variables_to_restore,
-        ignore_missing_vars=FLAGS.ignore_missing_vars)
+  return slim.assign_from_checkpoint_fn(
+      checkpoint_path,
+      variables_to_restore,
+      ignore_missing_vars=FLAGS.ignore_missing_vars)
 
 
 def _get_variables_to_train():
-    """Returns a list of variables to train.
+  """Returns a list of variables to train.
 
-    Returns:
-      A list of variables to train by the optimizer.
-    """
-    if FLAGS.trainable_scopes is None:
-        return tf.trainable_variables()
-    else:
-        scopes = [scope.strip() for scope in FLAGS.trainable_scopes.split(',')]
+  Returns:
+    A list of variables to train by the optimizer.
+  """
+  if FLAGS.trainable_scopes is None:
+    return tf.trainable_variables()
+  else:
+    scopes = [scope.strip() for scope in FLAGS.trainable_scopes.split(',')]
 
-    variables_to_train = []
-    for scope in scopes:
-        variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
-        variables_to_train.extend(variables)
-    return variables_to_train
+  variables_to_train = []
+  for scope in scopes:
+    variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
+    variables_to_train.extend(variables)
+  return variables_to_train
 
 
 def main(_):
