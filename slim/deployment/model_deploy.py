fc7342bf047ec5fc7a707202adaf108661bd373d
derekjchow <derekjchow@gmail.com>
2017-06-14 13:31:35 -0700

    Update model_deploy. (#1557)
    Fix slow down with only 1 GPU

--- slim/deployment/model_deploy.py
+++ slim/deployment/model_deploy.py
@@ -103,8 +103,6 @@ import collections
 
 import tensorflow as tf
 
-from tensorflow.python.ops import control_flow_ops
-
 slim = tf.contrib.slim
 
 
@@ -594,8 +592,7 @@ class DeploymentConfig(object):
     if self._clone_on_cpu:
       device += '/device:CPU:0'
     else:
-      if self._num_clones > 1:
-        device += '/device:GPU:%d' % clone_index
+      device += '/device:GPU:%d' % clone_index
     return device
 
   def clone_scope(self, clone_index):
