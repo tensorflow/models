8d91ce765a81ac596e6f8781bbdc75a0f419a65d
vivek rathod <vivekmrathod@gmail.com>
2017-06-16 16:29:47 -0700

    Change DEFINE_enum to DEFINE_string and delete unused file. (#1590)

--- object_detection/create_pascal_tf_record.py
+++ object_detection/create_pascal_tf_record.py
@@ -39,18 +39,20 @@ from object_detection.utils import label_map_util
 
 flags = tf.app.flags
 flags.DEFINE_string('data_dir', '', 'Root directory to raw PASCAL VOC dataset.')
-flags.DEFINE_enum('set', 'train', ['train', 'val', 'trainval', 'test'],
-                  'Convert training set, validation set or merged set.')
+flags.DEFINE_string('set', 'train', 'Convert training set, validation set or '
+                    'merged set.')
 flags.DEFINE_string('annotations_dir', 'Annotations',
                     '(Relative) path to annotations directory.')
-flags.DEFINE_enum('year', 'VOC2007', ['VOC2007', 'VOC2012', 'merged'],
-                  'Desired challenge year.')
+flags.DEFINE_string('year', 'VOC2007', 'Desired challenge year.')
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
 flags.DEFINE_string('label_map_path', 'data/pascal_label_map.pbtxt',
                     'Path to label map proto')
 flags.DEFINE_boolean('ignore_difficult_instances', False, 'Whether to ignore '
                      'difficult instances')
 FLAGS = flags.FLAGS
+
+SETS = ['train', 'val', 'trainval', 'test']
+YEARS = ['VOC2007', 'VOC2012', 'merged']
 
 
 def dict_to_tf_example(data,
@@ -139,6 +141,11 @@ def dict_to_tf_example(data,
 
 
 def main(_):
+  if FLAGS.set not in SETS:
+    raise ValueError('set must be in : {}'.format(SETS))
+  if FLAGS.year not in YEARS:
+    raise ValueError('year must be in : {}'.format(YEARS))
+
   data_dir = FLAGS.data_dir
   years = ['VOC2007', 'VOC2012']
   if FLAGS.year != 'merged':
