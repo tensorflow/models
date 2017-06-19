b9caf04efc32004191813347dcdd5c7296bdca1d
leVirve <gae.m.project@gmail.com>
2017-06-19 21:59:46 +0800

    use six.iteritem

--- object_detection/core/batcher.py
+++ object_detection/core/batcher.py
@@ -16,6 +16,7 @@
 """Provides functions to batch a dictionary of input tensors."""
 import collections
 
+import six
 import tensorflow as tf
 
 from object_detection.core import prefetcher
@@ -78,11 +79,11 @@ class BatchQueue(object):
     """
     # Remember static shapes to set shapes of batched tensors.
     static_shapes = collections.OrderedDict(
-        {key: tensor.get_shape() for key, tensor in tensor_dict.items()})
+        {key: tensor.get_shape() for key, tensor in six.iteritems(tensor_dict)})
     # Remember runtime shapes to unpad tensors after batching.
     runtime_shapes = collections.OrderedDict(
         {(key, 'runtime_shapes'): tf.shape(tensor)
-         for key, tensor in tensor_dict.items()})
+         for key, tensor in six.iteritems(tensor_dict)})
     all_tensors = tensor_dict
     all_tensors.update(runtime_shapes)
     batched_tensors = tf.train.batch(
@@ -109,7 +110,7 @@ class BatchQueue(object):
     # Separate input tensors from tensors containing their runtime shapes.
     tensors = {}
     shapes = {}
-    for key, batched_tensor in batched_tensors.items():
+    for key, batched_tensor in six.iteritems(batched_tensors):
       unbatched_tensor_list = tf.unstack(batched_tensor)
       for i, unbatched_tensor in enumerate(unbatched_tensor_list):
         if isinstance(key, tuple) and key[1] == 'runtime_shapes':
