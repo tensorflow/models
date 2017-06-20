6e53298e3176e3f36fe128fd018fa55972a44d13
Sergio Guadarrama <sguada@google.com>
2017-06-20 16:17:37 -0700

    Add optional arguments for the inception-v2 model (use_separable_conv, data_format).
    PiperOrigin-RevId: 159630310

--- research/slim/nets/inception_v2.py
+++ research/slim/nets/inception_v2.py
@@ -31,6 +31,8 @@ def inception_v2_base(inputs,
                       final_endpoint='Mixed_5c',
                       min_depth=16,
                       depth_multiplier=1.0,
+                      use_separable_conv=True,
+                      data_format='NHWC',
                       scope=None):
   """Inception v2 (6a2).
 
@@ -52,6 +54,9 @@ def inception_v2_base(inputs,
       for all convolution ops. The value must be greater than zero. Typical
       usage will be to set this value in (0, 1) to reduce the number of
       parameters or computation cost of the model.
+    use_separable_conv: Use a separable convolution for the first layer
+      Conv2d_1a_7x7. If this is False, use a normal convolution instead.
+    data_format: Data format of the activations ('NHWC' or 'NCHW').
     scope: Optional variable_scope.
 
   Returns:
@@ -73,28 +78,52 @@ def inception_v2_base(inputs,
     raise ValueError('depth_multiplier is not greater than zero.')
   depth = lambda d: max(int(d * depth_multiplier), min_depth)
 
+  if data_format != 'NHWC' and data_format != 'NCHW':
+    raise ValueError('data_format must be either NHWC or NCHW.')
+  if data_format == 'NCHW' and use_separable_conv:
+    raise ValueError(
+        'separable convolution only supports NHWC layout. NCHW data format can'
+        ' only be used when use_separable_conv is False.'
+    )
+
+  concat_dim = 3 if data_format == 'NHWC' else 1
   with tf.variable_scope(scope, 'InceptionV2', [inputs]):
     with slim.arg_scope(
-        [slim.conv2d, slim.max_pool2d, slim.avg_pool2d, slim.separable_conv2d],
-        stride=1, padding='SAME'):
+        [slim.conv2d, slim.max_pool2d, slim.avg_pool2d],
+        stride=1,
+        padding='SAME',
+        data_format=data_format):
 
       # Note that sizes in the comments below assume an input spatial size of
       # 224x224, however, the inputs can be of any size greater 32x32.
 
       # 224 x 224 x 3
       end_point = 'Conv2d_1a_7x7'
-      # depthwise_multiplier here is different from depth_multiplier.
-      # depthwise_multiplier determines the output channels of the initial
-      # depthwise conv (see docs for tf.nn.separable_conv2d), while
-      # depth_multiplier controls the # channels of the subsequent 1x1
-      # convolution. Must have
-      #   in_channels * depthwise_multipler <= out_channels
-      # so that the separable convolution is not overparameterized.
-      depthwise_multiplier = min(int(depth(64) / 3), 8)
-      net = slim.separable_conv2d(
-          inputs, depth(64), [7, 7], depth_multiplier=depthwise_multiplier,
-          stride=2, weights_initializer=trunc_normal(1.0),
-          scope=end_point)
+
+      if use_separable_conv:
+        # depthwise_multiplier here is different from depth_multiplier.
+        # depthwise_multiplier determines the output channels of the initial
+        # depthwise conv (see docs for tf.nn.separable_conv2d), while
+        # depth_multiplier controls the # channels of the subsequent 1x1
+        # convolution. Must have
+        #   in_channels * depthwise_multipler <= out_channels
+        # so that the separable convolution is not overparameterized.
+        depthwise_multiplier = min(int(depth(64) / 3), 8)
+        net = slim.separable_conv2d(
+            inputs, depth(64), [7, 7],
+            depth_multiplier=depthwise_multiplier,
+            stride=2,
+            padding='SAME',
+            weights_initializer=trunc_normal(1.0),
+            scope=end_point)
+      else:
+        # Use a normal convolution instead of a separable convolution.
+        net = slim.conv2d(
+            inputs,
+            depth(64), [7, 7],
+            stride=2,
+            weights_initializer=trunc_normal(1.0),
+            scope=end_point)
       end_points[end_point] = net
       if end_point == final_endpoint: return net, end_points
       # 112 x 112 x 64
@@ -146,7 +175,7 @@ def inception_v2_base(inputs,
               branch_3, depth(32), [1, 1],
               weights_initializer=trunc_normal(0.1),
               scope='Conv2d_0b_1x1')
-        net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+        net = tf.concat([branch_0, branch_1, branch_2, branch_3], concat_dim)
         end_points[end_point] = net
         if end_point == final_endpoint: return net, end_points
       # 28 x 28 x 256
@@ -176,7 +205,7 @@ def inception_v2_base(inputs,
               branch_3, depth(64), [1, 1],
               weights_initializer=trunc_normal(0.1),
               scope='Conv2d_0b_1x1')
-        net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+        net = tf.concat([branch_0, branch_1, branch_2, branch_3], concat_dim)
         end_points[end_point] = net
         if end_point == final_endpoint: return net, end_points
       # 28 x 28 x 320
@@ -201,7 +230,7 @@ def inception_v2_base(inputs,
         with tf.variable_scope('Branch_2'):
           branch_2 = slim.max_pool2d(
               net, [3, 3], stride=2, scope='MaxPool_1a_3x3')
-        net = tf.concat([branch_0, branch_1, branch_2], 3)
+        net = tf.concat([branch_0, branch_1, branch_2], concat_dim)
         end_points[end_point] = net
         if end_point == final_endpoint: return net, end_points
       # 14 x 14 x 576
@@ -231,7 +260,7 @@ def inception_v2_base(inputs,
               branch_3, depth(128), [1, 1],
               weights_initializer=trunc_normal(0.1),
               scope='Conv2d_0b_1x1')
-        net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+        net = tf.concat([branch_0, branch_1, branch_2, branch_3], concat_dim)
         end_points[end_point] = net
         if end_point == final_endpoint: return net, end_points
       # 14 x 14 x 576
@@ -261,7 +290,7 @@ def inception_v2_base(inputs,
               branch_3, depth(128), [1, 1],
               weights_initializer=trunc_normal(0.1),
               scope='Conv2d_0b_1x1')
-        net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+        net = tf.concat([branch_0, branch_1, branch_2, branch_3], concat_dim)
         end_points[end_point] = net
         if end_point == final_endpoint: return net, end_points
       # 14 x 14 x 576
@@ -291,10 +320,9 @@ def inception_v2_base(inputs,
               branch_3, depth(96), [1, 1],
               weights_initializer=trunc_normal(0.1),
               scope='Conv2d_0b_1x1')
-        net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+        net = tf.concat([branch_0, branch_1, branch_2, branch_3], concat_dim)
         end_points[end_point] = net
         if end_point == final_endpoint: return net, end_points
-
       # 14 x 14 x 576
       end_point = 'Mixed_4e'
       with tf.variable_scope(end_point):
@@ -322,7 +350,7 @@ def inception_v2_base(inputs,
               branch_3, depth(96), [1, 1],
               weights_initializer=trunc_normal(0.1),
               scope='Conv2d_0b_1x1')
-        net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+        net = tf.concat([branch_0, branch_1, branch_2, branch_3], concat_dim)
         end_points[end_point] = net
         if end_point == final_endpoint: return net, end_points
       # 14 x 14 x 576
@@ -347,7 +375,7 @@ def inception_v2_base(inputs,
         with tf.variable_scope('Branch_2'):
           branch_2 = slim.max_pool2d(net, [3, 3], stride=2,
                                      scope='MaxPool_1a_3x3')
-        net = tf.concat([branch_0, branch_1, branch_2], 3)
+        net = tf.concat([branch_0, branch_1, branch_2], concat_dim)
         end_points[end_point] = net
         if end_point == final_endpoint: return net, end_points
       # 7 x 7 x 1024
@@ -377,10 +405,9 @@ def inception_v2_base(inputs,
               branch_3, depth(128), [1, 1],
               weights_initializer=trunc_normal(0.1),
               scope='Conv2d_0b_1x1')
-        net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+        net = tf.concat([branch_0, branch_1, branch_2, branch_3], concat_dim)
         end_points[end_point] = net
         if end_point == final_endpoint: return net, end_points
-
       # 7 x 7 x 1024
       end_point = 'Mixed_5c'
       with tf.variable_scope(end_point):
@@ -408,7 +435,7 @@ def inception_v2_base(inputs,
               branch_3, depth(128), [1, 1],
               weights_initializer=trunc_normal(0.1),
               scope='Conv2d_0b_1x1')
-        net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+        net = tf.concat([branch_0, branch_1, branch_2, branch_3], concat_dim)
         end_points[end_point] = net
         if end_point == final_endpoint: return net, end_points
     raise ValueError('Unknown final endpoint %s' % final_endpoint)
