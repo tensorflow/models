3f49dedf869922ca4c9f8df6ec8cf57ec183f03f
Yuxian Meng <mengyuxian@pku.edu.cn>
2017-07-11 11:50:12 +0800

    Update exporter.py

--- object_detection/exporter.py
+++ object_detection/exporter.py
@@ -181,7 +181,10 @@ def _add_output_tensor_nodes(postprocessed_tensors):
   label_id_offset = 1
   boxes = postprocessed_tensors.get('detection_boxes')
   scores = postprocessed_tensors.get('detection_scores')
-  classes = postprocessed_tensors.get('detection_classes') + label_id_offset
+  if classes:
+      classes += label_id_offset
+  else:
+      classes = tf.constant(1, tf.int32, scores.shape)
   masks = postprocessed_tensors.get('detection_masks')
   num_detections = postprocessed_tensors.get('num_detections')
   outputs = {}
