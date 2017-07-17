a537bb5bbd006cc4483f0dc3e2cf8575eba55c56
xiaoerlageid <xiaoerlaigeid@users.noreply.github.com>
2017-07-17 10:39:19 +0800

    Update label_map_util.py

--- object_detection/utils/label_map_util.py
+++ object_detection/utils/label_map_util.py
@@ -19,7 +19,7 @@ import logging
 
 import tensorflow as tf
 from google.protobuf import text_format
-from object_detection.protos import string_int_label_map_pb2
+from protos import string_int_label_map_pb2
 
 
 def create_category_index(categories):
