aa5f27abeced1cb49e11014c174ecc0b3308e7e3
Minhyeok Jeong <petermnhk@gmail.com>
2017-07-22 01:51:23 +0900

    Fix warning 'Summary name /clone_loss is illegal'

--- slim/deployment/model_deploy.py
+++ slim/deployment/model_deploy.py
@@ -230,7 +230,7 @@ def _gather_clone_loss(clone, num_clones, regularization_losses):
       sum_loss = tf.add_n(all_losses)
   # Add the summaries out of the clone device block.
   if clone_loss is not None:
-    tf.summary.scalar(clone.scope + '/clone_loss', clone_loss)
+    tf.summary.scalar(clone.scope + 'clone_loss', clone_loss)
   if regularization_loss is not None:
     tf.summary.scalar('regularization_loss', regularization_loss)
   return sum_loss
