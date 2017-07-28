d3cae18d8f6ea219c35e8a727ce8e3e7252fb742
BearRed <20673813+BearRed@users.noreply.github.com>
2017-07-28 09:52:40 +0800

    Update ptb_word_lm.py

--- tutorials/rnn/ptb/ptb_word_lm.py
+++ tutorials/rnn/ptb/ptb_word_lm.py
@@ -90,7 +90,7 @@ class PTBInput(object):
   def __init__(self, config, data, name=None):
     self.batch_size = batch_size = config.batch_size
     self.num_steps = num_steps = config.num_steps
-    self.epoch_size = ((len(data) // batch_size) - 1) // num_steps
+    self.epoch_size = ((len(data) // batch_size) - 1) // num_steps # Q: why minus 1?
     self.input_data, self.targets = reader.ptb_producer(
         data, batch_size, num_steps, name=name)
 
