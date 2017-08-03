65605be59056a689a45687ab434a3397fb3037f1
Marianne Linhares Monteiro <monteirom@google.com>
2017-08-02 20:19:12 -0400

    Fixing style

--- tutorials/image/cifar10_estimator/cifar10_model.py
+++ tutorials/image/cifar10_estimator/cifar10_model.py
@@ -17,9 +17,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
-
 import model_base
+import tensorflow as tf
 
 
 class ResNetCifar10(model_base.ResNet):
