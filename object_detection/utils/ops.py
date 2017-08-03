77f497636aae768431e54a956f54437df3459786
cclauss <cclauss@bluewin.ch>
2017-08-03 11:04:26 +0200

    isinstance(i, six.integer_types)

--- object_detection/utils/ops.py
+++ object_detection/utils/ops.py
@@ -15,8 +15,7 @@
 
 """A module for helper tensorflow ops."""
 import math
-from past.builtins import long
-
+import six
 import tensorflow as tf
 
 from object_detection.core import box_list
@@ -198,9 +197,9 @@ def padded_one_hot_encoding(indices, depth, left_pad):
 
   TODO: add runtime checks for depth and indices.
   """
-  if depth < 0 or not isinstance(depth, (int, long)):
+  if depth < 0 or not isinstance(depth,  six.integer_types):
     raise ValueError('`depth` must be a non-negative integer.')
-  if left_pad < 0 or not isinstance(left_pad, (int, long)):
+  if left_pad < 0 or not isinstance(left_pad, six.integer_types):
     raise ValueError('`left_pad` must be a non-negative integer.')
   if depth == 0:
     return None
