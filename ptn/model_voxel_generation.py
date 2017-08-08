df9e50326b7aa9489e000b3dc6c4f41b857eddd3
Oier Mees <oier.mees@gmail.com>
2017-08-08 15:23:23 +0200

    fixes bug #2157 that didn't load the pretrained decoder for the trainind of the volumetric decoder.

--- ptn/model_voxel_generation.py
+++ ptn/model_voxel_generation.py
@@ -129,8 +129,9 @@ class Im2Vox(object):
       var_list.extend(
           filter(is_trainable, tf.contrib.framework.get_model_variables(scope)))
 
+    model_path = tf.train.latest_checkpoint(self._params.init_model)
     init_assign_op, init_feed_dict = slim.assign_from_checkpoint(
-        self._params.init_model, var_list)
+        model_path, var_list)
 
     def init_assign_function(sess):
       sess.run(init_assign_op, init_feed_dict)
