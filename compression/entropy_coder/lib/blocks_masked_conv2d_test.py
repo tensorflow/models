1d7be57ec458c3064ec589358ecb7d6952d0e7e3
cclauss <cclauss@bluewin.ch>
2017-08-10 12:31:50 +0200

    from six.moves import range for Python 3

--- compression/entropy_coder/lib/blocks_masked_conv2d_test.py
+++ compression/entropy_coder/lib/blocks_masked_conv2d_test.py
@@ -17,6 +17,7 @@
 
 from __future__ import division
 from __future__ import unicode_literals
+from six.moves import range
 
 import numpy as np
 import tensorflow as tf
@@ -179,8 +180,8 @@ class MaskedConv2DTest(tf.test.TestCase):
 
     x_feed = - np.ones(input_shape, dtype=float)
     y_expected = np.ones(output_shape, dtype=float)
-    for i in xrange(input_shape[1]):
-      for j in xrange(input_shape[2]):
+    for i in range(input_shape[1]):
+      for j in range(input_shape[2]):
         x_feed[0, i, j, 0] = 10 * (j + 1) + i
         v = 0
         ki_start = max(i - kernel_size // 2, 0)
