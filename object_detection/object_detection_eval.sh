03ff0a977805e921afd2308a5ef4eeaf3c5a1722
CasiaFan <fanzongshaoxing@gmail.com>
2017-08-16 10:57:31 +0800

    random vertical flip images

--- object_detection/object_detection_eval.sh
+++
@@ -1,4 +0,0 @@
-python eval.py --logtostderr \
-               --checkpoint_dir=models/hda_cam_person_fisheye/train \
-               --eval_dir=models/hda_cam_person_fisheye/eval \
-               --pipeline_config_path=models/hda_cam_person_fisheye/faster_rcnn_resnet101_person.config
--- object_detection/object_detection_train.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-python train.py --logtostderr \
-                --train_dir=models/hda_cam_person_fisheye/train \
-                --pipeline_config_path=models/hda_cam_person_fisheye/faster_rcnn_resnet101_person.config
-#                --pipeline_config_path=models/head_detector/faster_rcnn_inception_resnet_v2_atrous_head.config
