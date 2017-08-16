70a4958184106b9ce848da34be34bdf0dbf36450
CasiaFan <fanzongshaoxing@gmail.com>
2017-08-16 09:58:55 +0800

    step by step running

--- object_detection/object_detection_eval.sh
+++ object_detection/object_detection_eval.sh
@@ -1,4 +1,4 @@
 python eval.py --logtostderr \
-               --checkpoint_dir=models/startdt_pedestrian/train \
-               --eval_dir=models/startdt_pedestrian/eval \
-               --pipeline_config_path=models/startdt_pedestrian/faster_rcnn_resnet101_person.config
+               --checkpoint_dir=models/hda_cam_person_fisheye/train \
+               --eval_dir=models/hda_cam_person_fisheye/eval \
+               --pipeline_config_path=models/hda_cam_person_fisheye/faster_rcnn_resnet101_person.config
--- object_detection/object_detection_train.sh
+++ object_detection/object_detection_train.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 python train.py --logtostderr \
-                --train_dir=models/startdt_pedestrian/train \
-                --pipeline_config_path=models/startdt_pedestrian/faster_rcnn_resnet101_person.config
+                --train_dir=models/hda_cam_person_fisheye/train \
+                --pipeline_config_path=models/hda_cam_person_fisheye/faster_rcnn_resnet101_person.config
 #                --pipeline_config_path=models/head_detector/faster_rcnn_inception_resnet_v2_atrous_head.config
