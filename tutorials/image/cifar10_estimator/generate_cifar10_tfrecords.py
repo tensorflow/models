a7531875f9506f7dece6e62bc28ab0c52068d953
Toby Boyd <tobyboyd@google.com>
2017-08-20 12:52:57 -0700

    Updated readme and cleaned up formatting

--- tutorials/image/cifar10_estimator/generate_cifar10_tfrecords.py
+++ tutorials/image/cifar10_estimator/generate_cifar10_tfrecords.py
@@ -12,10 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Read CIFAR-10 data from pickled numpy arrays and write TFExamples.
+"""Read CIFAR-10 data from pickled numpy arrays and writes TFRecords.
 
-Generates TFRecord files from the python version of the CIFAR-10 dataset
-downloaded from https://www.cs.toronto.edu/~kriz/cifar.html.
+Generates tf.train.Example protos and writes them to TFRecord files from the
+python version of the CIFAR-10 dataset downloaded from
+https://www.cs.toronto.edu/~kriz/cifar.html.
 """
 
 from __future__ import absolute_import
@@ -30,14 +31,18 @@ import tarfile
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
-DATA_URL = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
-CIFAR_10_FILE_NAME = 'cifar-10-python.tar.gz'
+CIFAR_FILENAME = 'cifar-10-python.tar.gz'
+CIFAR_DOWNLOAD_URL = 'https://www.cs.toronto.edu/~kriz/' + CIFAR_FILENAME
 CIFAR_LOCAL_FOLDER = 'cifar-10-batches-py'
+
 
 def download_and_extract(data_dir):
   # download CIFAR-10 if not already downloaded.
-  tf.contrib.learn.datasets.base.maybe_download(CIFAR_10_FILE_NAME, data_dir, DATA_URL)
-  tarfile.open(os.path.join(data_dir,CIFAR_10_FILE_NAME), 'r:gz').extractall(data_dir)
+  tf.contrib.learn.datasets.base.maybe_download(CIFAR_FILENAME, data_dir,
+                                                CIFAR_DOWNLOAD_URL)
+  tarfile.open(os.path.join(data_dir, CIFAR_FILENAME),
+               'r:gz').extractall(data_dir)
+
 
 def _int64_feature(value):
   return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
@@ -63,19 +68,17 @@ def read_pickle_from_file(filename):
 
 
 def convert_to_tfrecord(input_files, output_file):
-  """Converts a file to tfrecords."""
+  """Converts a file to TFRecords."""
   print('Generating %s' % output_file)
   with tf.python_io.TFRecordWriter(output_file) as record_writer:
     for input_file in input_files:
-      print(input_file)
       data_dict = read_pickle_from_file(input_file)
       data = data_dict['data']
       labels = data_dict['labels']
-
       num_entries_in_batch = len(labels)
       for i in range(num_entries_in_batch):
-        example = tf.train.Example(
-            features=tf.train.Features(feature={
+        example = tf.train.Example(features=tf.train.Features(
+            feature={
                 'image': _bytes_feature(data[i].tobytes()),
                 'label': _int64_feature(labels[i])
             }))
@@ -83,18 +86,18 @@ def convert_to_tfrecord(input_files, output_file):
 
 
 def main(data_dir):
+  print('Download from {} and extract.'.format(CIFAR_DOWNLOAD_URL))
   download_and_extract(data_dir)
   file_names = _get_file_names()
   input_dir = os.path.join(data_dir, CIFAR_LOCAL_FOLDER)
   for mode, files in file_names.items():
-    input_files = [
-        os.path.join(input_dir, f) for f in files]
+    input_files = [os.path.join(input_dir, f) for f in files]
     output_file = os.path.join(data_dir, mode + '.tfrecords')
     try:
       os.remove(output_file)
     except OSError:
       pass
-    # Convert to Examples and write the result to TFRecords.
+    # Convert to tf.train.Example and write the to TFRecords.
     convert_to_tfrecord(input_files, output_file)
   print('Done!')
 
@@ -105,8 +108,7 @@ if __name__ == '__main__':
       '--data-dir',
       type=str,
       default='',
-      help='Directory to download and extract CIFAR-10 to.'
-  )
+      help='Directory to download and extract CIFAR-10 to.')
 
   args = parser.parse_args()
   main(args.data_dir)
