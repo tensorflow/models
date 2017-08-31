7c30caf11f5e7a0243baeb1340817eb7a706e5f2
derekjchow <derekjchow@gmail.com>
2017-08-31 16:20:56 -0700

    Update slim/ (#2307)

--- slim/nets/nets_factory_test.py
+++ slim/nets/nets_factory_test.py
@@ -19,20 +19,19 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+
 import tensorflow as tf
 
 from nets import nets_factory
 
-slim = tf.contrib.slim
-
 
 class NetworksTest(tf.test.TestCase):
 
-  def testGetNetworkFn(self):
+  def testGetNetworkFnFirstHalf(self):
     batch_size = 5
     num_classes = 1000
-    for net in nets_factory.networks_map:
-      with self.test_session():
+    for net in nets_factory.networks_map.keys()[:10]:
+      with tf.Graph().as_default() as g, self.test_session(g):
         net_fn = nets_factory.get_network_fn(net, num_classes)
         # Most networks use 224 as their default_image_size
         image_size = getattr(net_fn, 'default_image_size', 224)
@@ -43,19 +42,20 @@ class NetworksTest(tf.test.TestCase):
         self.assertEqual(logits.get_shape().as_list()[0], batch_size)
         self.assertEqual(logits.get_shape().as_list()[-1], num_classes)
 
-  def testGetNetworkFnArgScope(self):
+  def testGetNetworkFnSecondHalf(self):
     batch_size = 5
-    num_classes = 10
-    net = 'cifarnet'
-    with self.test_session(use_gpu=True):
-      net_fn = nets_factory.get_network_fn(net, num_classes)
-      image_size = getattr(net_fn, 'default_image_size', 224)
-      with slim.arg_scope([slim.model_variable, slim.variable],
-                          device='/CPU:0'):
+    num_classes = 1000
+    for net in nets_factory.networks_map.keys()[10:]:
+      with tf.Graph().as_default() as g, self.test_session(g):
+        net_fn = nets_factory.get_network_fn(net, num_classes)
+        # Most networks use 224 as their default_image_size
+        image_size = getattr(net_fn, 'default_image_size', 224)
         inputs = tf.random_uniform((batch_size, image_size, image_size, 3))
-        net_fn(inputs)
-      weights = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'CifarNet/conv1')[0]
-      self.assertDeviceEqual('/CPU:0', weights.device)
+        logits, end_points = net_fn(inputs)
+        self.assertTrue(isinstance(logits, tf.Tensor))
+        self.assertTrue(isinstance(end_points, dict))
+        self.assertEqual(logits.get_shape().as_list()[0], batch_size)
+        self.assertEqual(logits.get_shape().as_list()[-1], num_classes)
 
 if __name__ == '__main__':
   tf.test.main()
