8a7908eb2d25f939fa43629923580cd7b042e91c
Steve Austin <trans.medium@gmail.com>
2017-08-31 03:26:26 -0700

    object detection from cam

---
+++ object_detection/cam-mon.py
@@ -0,0 +1,125 @@
+# coding: utf-8
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import tarfile
+import tensorflow as tf
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+import cv2
+cap = cv2.VideoCapture(0)
+
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+# ## Download Model
+if not os.path.isfile(MODEL_FILE):
+    print('download model')
+    opener = urllib.request.URLopener()
+    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+    tar_file = tarfile.open(MODEL_FILE)
+    for file in tar_file.getmembers():
+      file_name = os.path.basename(file.name)
+      if 'frozen_inference_graph.pb' in file_name:
+        tar_file.extract(file, os.getcwd())
+else:
+    print('using cached model')
+#download_model()
+
+# ## Load a (frozen) Tensorflow model into memory.
+print('loading model')
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    print('importing graph')
+    tf.import_graph_def(od_graph_def, name='')
+    print('done')
+
+# ## Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+print(category_index)
+
+# ## Helper code
+
+# In[42]:
+
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+frame = 0
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    while True:
+      print('frame {}'.format(frame))
+      frame += 1
+      ret, image_np = cap.read()
+
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      # Each box represents a part of the image where a particular object was detected.
+      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      # Each score represent how level of confidence for each of the objects.
+      # Score is shown on the result image, together with the class label.
+      scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+      # Actual detection.
+      (boxes, scores, classes, num_detections) = sess.run(
+          [boxes, scores, classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      # Visualization of the results of a detection.
+      #print(type(classes))
+      #print(classes.shape)
+      vis_util.visualize_boxes_and_labels_on_image_array(
+          image_np,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=8)
+      min_score_thresh=.5
+      labels = []
+      #print(scores.shape)
+      #print('n', boxes.shape[0])
+      #for i in range(boxes.shape[0]):
+      #  print('score', i, scores[i])
+      #  boxscore = scores[i]
+      #  if scores is None or scores[i] > min_score_thresh:
+      #      #box = tuple(boxes[i].tolist())
+      #      labels.append(category_index[classes[i]]['name'])
+      #print(labels)
+      #plt.figure(figsize=IMAGE_SIZE)
+      #plt.imshow(image_np)
+      # 800, 600
+      cv2.imshow('R2K9', cv2.resize(image_np, (160, 120)))
+      if cv2.waitKey(25) & 0xFF == ord('q'):
+          cv2.destroyAllWindows()
+          break
+
