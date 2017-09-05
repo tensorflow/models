952bd146bebc85d5a975b25cd746718e1311c7be
Max Ferguson <maxkferg@gmail.com>
2017-09-04 17:00:28 -0700

    Added notebook to plot loss

--- simple_defects/python/model.py
+++ simple_defects/python/model.py
@@ -8,7 +8,8 @@ def build_model(input_shape,flatten):
 	# input: 100x100 images with 3 channels -> (100, 100, 3) tensors.
 	# this applies 32 convolution filters of size 3x3 each.
 	model.add(Conv2D(32, (7, 7), input_shape=input_shape))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Conv2D(32, (7, 7), input_shape=input_shape))
+	#model.add(MaxPooling2D(pool_size=(2, 2)))
 	model.add(Dropout(0.25))
 
 	model.add(Conv2D(64, (5, 5), padding='same'))
