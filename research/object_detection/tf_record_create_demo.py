b99479fbb79a95ed8ec124ef47673ed98dd27305
孙洪志 <sunhongzhi@baofeng.com>
2017-10-19 18:48:17 +0800

    备份

--- research/object_detection/tf_record_create_demo.py
+++ research/object_detection/tf_record_create_demo.py
@@ -3,23 +3,110 @@ import tensorflow as tf
 from PIL import Image  # 注意Image,后面会用到
 import matplotlib.pyplot as plt
 import numpy as np
+# from scipy.misc import imread,imresize
+import time
+# cwd = 'E:\\data_mining\\temp'
+# classes = {'husky', 'chihuahua'}  # 人为 设定 2 类
+# writer = tf.python_io.TFRecordWriter("E:\\data_mining\\temp\\dog_train.tfrecords")  # 要生成的文件
+#
+# for index, name in enumerate(classes):
+#     class_path = os.path.join(cwd, name)
+#     for img_name in os.listdir(class_path):
+#         img_path = os.path.join(class_path, img_name)
+#
+#         img = Image.open(img_path)
+#         img = img.resize((128, 128))
+#         img_raw = img.tobytes()  # 将图片转化为二进制格式
+#         example = tf.train.Example(features=tf.train.Features(feature={
+#             "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
+#             'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+#         }))  # example对象对label和image数据进行封装
+#         writer.write(example.SerializeToString())  # 序列化为字符串
+#
+# writer.close()
 
-cwd = 'E:\\data_mining\\temp'
-classes = {'husky', 'chihuahua'}  # 人为 设定 2 类
-writer = tf.python_io.TFRecordWriter("E:\\data_mining\\temp\\dog_train.tfrecords")  # 要生成的文件
 
-for index, name in enumerate(classes):
-    class_path = os.path.join(cwd, name)
-    for img_name in os.listdir(class_path):
-        img_path = os.path.join(class_path, img_name)
+#图片存放位置
+DATA_DIR = 'data/'
 
-        img = Image.open(img_path)
-        img = img.resize((128, 128))
-        img_raw = img.tobytes()  # 将图片转化为二进制格式
+#图片信息
+IMG_HEIGHT = 227
+IMG_WIDTH = 227
+IMG_CHANNELS = 3
+NUM_TRAIN = 7000
+NUM_VALIDARION = 1144
+
+#读取图片
+def read_images(path):
+    filenames = next(os.walk(path))[2]
+    num_files = len(filenames)
+    images = np.zeros((num_files,IMG_HEIGHT,IMG_WIDTH,IMG_CHANNELS),dtype=np.uint8)
+    labels = np.zeros((num_files, ), dtype=np.uint8)
+    f = open('label.txt')
+    lines = f.readlines()
+    #遍历所有的图片和label，将图片resize到[227,227,3]
+    for i,filename in enumerate(filenames):
+        # img = imread(os.join(path,filename))
+        # img = imresize(img,(IMG_HEIGHT,IMG_WIDTH))
+        img = Image.open(os.join(path,filename))
+        img = img.resize((IMG_HEIGHT,IMG_WIDTH))
+
+        images[i] = img
+        labels[i] = int(lines[i])
+    f.close()
+    return images,labels
+
+#生成整数型的属性
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+#生成字符串型的属性
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def convert(images,labels,name):
+    #获取要转换为TFRecord文件的图片数目
+    num = images.shape[0]
+    #输出TFRecord文件的文件名
+    filename = name+'.tfrecords'
+    print('Writting',filename)
+    #创建一个writer来写TFRecord文件
+    writer = tf.python_io.TFRecordWriter(filename)
+    for i in range(num):
+        #将图像矩阵转化为一个字符串
+        img_raw = images[i].tostring()
+        #将一个样例转化为Example Protocol Buffer，并将所有需要的信息写入数据结构
         example = tf.train.Example(features=tf.train.Features(feature={
-            "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
-            'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
-        }))  # example对象对label和image数据进行封装
-        writer.write(example.SerializeToString())  # 序列化为字符串
+            'label': _int64_feature(int(labels[i])),
+            'image_raw': _bytes_feature(img_raw)}))
+        #将example写入TFRecord文件
+        writer.write(example.SerializeToString())
+    writer.close()
+    print('Writting End')
 
-writer.close()
+def main(argv):
+
+    path = r'E:\data_mining\temp\train'
+    print("name = "+os.path.basename(path))
+    # print('reading images begin')
+    # start_time = time.time()
+    # train_images,train_labels = read_images(DATA_DIR)
+    # duration = time.time() - start_time
+    # print("reading images end , cost %d sec" %duration)
+    #
+    # #get validation
+    # validation_images = train_images[:NUM_VALIDARION,:,:,:]
+    # validation_labels = train_labels[:NUM_VALIDARION]
+    # train_images = train_images[NUM_VALIDARION:,:,:,:]
+    # train_labels = train_labels[NUM_VALIDARION:]
+    #
+    # #convert to tfrecords
+    # print('convert to tfrecords begin')
+    # start_time = time.time()
+    # convert(train_images,train_labels,'train')
+    # convert(validation_images,validation_labels,'validation')
+    # duration = time.time() - start_time
+    # print('convert to tfrecords end , cost %d sec' %duration)
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ research/object_detection/tf_record_data.py
@@ -0,0 +1,183 @@
+import tensorflow as tf
+
+from object_detection.utils import dataset_util
+import os
+from PIL import Image
+from object_detection.utils import label_map_util
+import numpy as np
+
+r"""Convert the Oxford pet dataset to TFRecord for object_detection.
+
+See: O. M. Parkhi, A. Vedaldi, A. Zisserman, C. V. Jawahar
+     Cats and Dogs
+     IEEE Conference on Computer Vision and Pattern Recognition, 2012
+     http://www.robots.ox.ac.uk/~vgg/data/pets/
+
+Example usage:
+    ./create_pet_tf_record --data_dir=/home/user/pet \
+        --output_path=/home/user/pet/output
+"""
+flags = tf.app.flags
+flags.DEFINE_string('output_path', r'E:\data_mining\temp\train\train.record', 'Path to output TFRecord')
+flags.DEFINE_string('input_path', r'E:\data_mining\temp\train', 'Path to output TFRecord')
+flags.DEFINE_string('label_map_path', 'data/logo_label_map.pbtxt', 'Path to label map proto')
+FLAGS = flags.FLAGS
+
+
+def create_tf_example(dir, filename, label_map_dict):
+    """Convert XML derived dict to tf.Example proto.
+
+    Notice that this function normalizes the bounding box coordinates provided
+    by the raw data.
+
+    Args:
+      data: dict holding PASCAL XML fields for a single image (obtained by
+        running dataset_util.recursive_parse_xml_to_dict)
+      label_map_dict: A map from string label names to integers ids.
+      image_subdirectory: String specifying subdirectory within the
+        Pascal dataset directory holding the actual image data.
+      ignore_difficult_instances: Whether to skip difficult instances in the
+        dataset  (default: False).
+
+    Returns:
+      example: The converted tf.Example.
+
+    Raises:
+      ValueError: if the image pointed to by data['filename'] is not a valid JPEG
+    """
+    class_name = os.path.basename(dir)
+    img_path = os.path.join(dir, filename)
+
+    img = np.array(Image.open(img_path))
+    img_raw = img.tostring()
+    # print("width = ",img.width,",height = ",img.height)
+
+    height = img.shape[0]
+    width = img.shape[1]
+
+    xmins = [100 / width]  # List of normalized left x coordinates in bounding box (1 per box)
+    xmaxs = [100 / width]  # List of normalized right x coordinates in bounding box
+    # (1 per box)
+    ymins = [100 / height]  # List of normalized top y coordinates in bounding box (1 per box)
+    ymaxs = [100 / height]  # List of normalized bottom y coordinates in bounding box
+    # (1 per box)
+    classes_text = []  # List of string class name of bounding box (1 per box)
+    classes = []  # List of integer class id of bounding box (1 per box)
+
+    classes_text.append(class_name.encode('utf8'))
+    classes.append(label_map_dict[class_name])
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename.encode('utf8')),
+        'image/source_id': dataset_util.bytes_feature(filename.encode('utf8')),
+        'image/encoded': dataset_util.bytes_feature(img_raw),
+        'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+
+
+def create_tfrecord():
+    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    label_map_dict = label_map_util.get_label_map_dict(FLAGS.label_map_path)
+    for class_dir_name in os.listdir(FLAGS.input_path):
+        class_dir_path = os.path.join(FLAGS.input_path, class_dir_name)
+        if os.path.isdir(class_dir_path):
+            print("class_dir_path = " + class_dir_path)
+            for img_name in os.listdir(class_dir_path):
+                tf_example = create_tf_example(class_dir_path, img_name, label_map_dict)
+                writer.write(tf_example.SerializeToString())
+
+    writer.close()
+
+
+def read_and_decode(filename_queue):
+    # 创建一个reader来读取TFRecord文件中的样例
+    reader = tf.TFRecordReader()
+    # 从文件中读出一个样例
+    _, serialized_example = reader.read(filename_queue)
+    # 解析读入的一个样例
+    features = tf.parse_single_example(serialized_example, features={
+        'image/height': tf.FixedLenFeature([], tf.int64),
+        'image/width': tf.FixedLenFeature([], tf.int64),
+        'image/filename': tf.FixedLenFeature([], tf.string),
+        'image/source_id': tf.FixedLenFeature([], tf.string),
+        'image/encoded': tf.FixedLenFeature([], tf.string),
+        'image/format': tf.FixedLenFeature([], tf.string),
+        'image/object/bbox/xmin': tf.FixedLenFeature([], tf.float32),
+        'image/object/bbox/xmax': tf.FixedLenFeature([], tf.float32),
+        'image/object/bbox/ymin': tf.FixedLenFeature([], tf.float32),
+        'image/object/bbox/ymax': tf.FixedLenFeature([], tf.float32),
+        'image/object/class/text': tf.FixedLenFeature([], tf.float32),
+        'image/object/class/label': tf.FixedLenFeature([], tf.float32),
+    })
+    # 将字符串解析成图像对应的像素数组
+    image = tf.decode_raw(features['image/encoded'], tf.uint8)
+    width = tf.cast(features['image/width'], tf.int32)
+    height = tf.cast(features['image/height'], tf.int32)
+    label = tf.cast(features['image/object/class/text'], tf.string)
+
+    image = tf.reshape(image, [height, width, 3])
+    image = tf.cast(image, tf.float32) * (1. / 255) - 0.5  # ????
+
+    return image, label
+
+
+# # 用于获取一个batch_size的图像和label
+# def inputs(data_set, batch_size, num_epochs):
+#     if not num_epochs:
+#         num_epochs = None
+#     if data_set == 'train':
+#         file = FLAGS.output_path
+#     # else:
+#     #     file = VALIDATION_FILE
+#
+#     filename_queue = tf.train.string_input_producer(["E:\\data_mining\\temp\\dog_train.tfrecords"])  # 读入流中
+#     image, label = read_and_decode(filename_queue)
+#     with tf.Session() as sess:  # 开始一个会话
+#         sess.run(tf.initialize_all_variables())
+#         coord = tf.train.Coordinator()
+#         threads = tf.train.start_queue_runners(coord=coord)
+#         for i in range(20):
+#             example, l = sess.run([image, label])  # 在会话中取出image和label
+#             img = Image.fromarray(example, 'RGB')  # 这里Image是之前提到的
+#             save_path = os.path.join(cwd, str(i) + '_''Label_' + str(l) + '.jpg')
+#             img.save(save_path)  # 存下图片
+#             print(example, l)
+#         coord.request_stop()
+#         coord.join(threads)
+#
+#     return images, labels
+
+
+def main(_):
+    # iamges, labels = input("train", 10, 0)
+    file = FLAGS.output_path
+    with tf.name_scope('input') as scope:
+        filename_queue = tf.train.string_input_producer([file], num_epochs=1)
+        image, label = read_and_decode(filename_queue)
+
+    with tf.Session() as sess:  # 开始一个会话
+        init_op = tf.initialize_all_variables()
+        sess.run(init_op)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        for i in range(20):
+            example, l = sess.run([image, label])  # 在会话中取出image和label
+            img = Image.fromarray(example, 'RGB')  # 这里Image是之前提到的
+            save_path = os.path.join(FLAGS.input_path, str(i) + '_''Label_' + l + '.jpg')
+            img.save(save_path)  # 存下图片
+            print(example, l)
+        coord.request_stop()
+        coord.join(threads)
+
+
+if __name__ == '__main__':
+    tf.app.run()
