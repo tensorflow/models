9226f20b8279a9e06947df7012a860346ffcc9db
JJ Ben-Joseph <jj@phrostbyte.com>
2017-10-20 12:18:59 -0400

    Add the ability to automatically generate protos from setup.py

--- research/setup.py
+++ research/setup.py
@@ -2,15 +2,27 @@
 
 from setuptools import find_packages
 from setuptools import setup
-
+import os
+import sys
 
 REQUIRED_PACKAGES = ['Pillow>=1.0']
 
-setup(
-    name='object_detection',
-    version='0.1',
-    install_requires=REQUIRED_PACKAGES,
-    include_package_data=True,
-    packages=[p for p in find_packages() if p.startswith('object_detection')],
-    description='Tensorflow Object Detection Library',
-)
+def compile_protos():
+    absolute_path = os.path.abspath(__file__)
+    script_dir = os.path.dirname(absolute_path)
+    os.chdir(script_dir)
+
+    cmd = 'protoc object_detection/protos/*.proto --python_out=.'
+    if os.system(cmd) != 0:
+        sys.exit(-1)
+
+if __name__ == "__main__":
+    compile_protos()
+    setup(
+        name='object_detection',
+        version='0.1',
+        install_requires=REQUIRED_PACKAGES,
+        include_package_data=True,
+        packages=[p for p in find_packages() if p.startswith('object_detection')],
+        description='Tensorflow Object Detection Library',
+    )
