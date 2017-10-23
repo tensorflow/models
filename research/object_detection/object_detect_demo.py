814c869becaed63946e74b519301a792e928d8d6
孙洪志 <sunhongzhi@baofeng.com>
2017-10-23 14:47:58 +0800

    所有流程完毕，可以预测logo

--- research/object_detection/object_detect_demo.py
+++ research/object_detection/object_detect_demo.py
@@ -18,28 +18,28 @@ from utils import label_map_util
 from utils import visualization_utils as vis_util
 
 # What model to download.
-MODEL_NAME = 'faster_rcnn_resnet101_coco_11_06_2017'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+# MODEL_NAME = 'faster_rcnn_resnet101_coco_11_06_2017'
+# MODEL_FILE = MODEL_NAME + '.tar.gz'
+# DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_CKPT = r'east_ic_graph_model\frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS = r"D:\WorkSpace\models\research\object_detection\data\logo_label_map.pbtxt"
 
-NUM_CLASSES = 90
+NUM_CLASSES = 2
 
 # download model
-if not os.path.exists(MODEL_FILE):
-    print("downloading model")
-    opener = urllib.request.URLopener()
-    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-tar_file = tarfile.open(MODEL_FILE)
-for file in tar_file.getmembers():
-    file_name = os.path.basename(file.name)
-    if 'frozen_inference_graph.pb' in file_name:
-        tar_file.extract(file, os.getcwd())
+# if not os.path.exists(MODEL_FILE):
+#     print("downloading model")
+#     opener = urllib.request.URLopener()
+#     opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+# tar_file = tarfile.open(MODEL_FILE)
+# for file in tar_file.getmembers():
+#     file_name = os.path.basename(file.name)
+#     if 'frozen_inference_graph.pb' in file_name:
+#         tar_file.extract(file, os.getcwd())
 
 print("download model and extract over.")
 
