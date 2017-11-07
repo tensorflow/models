89d3ec49fd3a6f23f397dc01dc7ecf5a2d179e76
Mark Daoust <markdaoust@google.com>
2017-11-07 11:42:55 -0500

    Factor out iris_data

--- samples/core/get_started/estimator_test.py
+++ samples/core/get_started/estimator_test.py
@@ -25,6 +25,7 @@ from six.moves import StringIO
 
 import custom_estimator
 import premade_estimator
+import iris_data
 
 FOUR_LINES = "\n".join([
     "1,52.40, 2823,152,2",
@@ -32,24 +33,24 @@ FOUR_LINES = "\n".join([
     "176,2824, 136,3.19,0",
     "2,177.30,66.30, 53.10,1",])
 
-def four_lines_data():
+def four_lines_data(y_name="Species"):
   text = StringIO(FOUR_LINES)
 
-  df = pd.read_csv(text, names=premade_estimator.COLUMNS)
+  df = pd.read_csv(text, names=iris_data.COLUMNS)
 
-  xy = (df, df.pop("Species"))
+  xy = (df, df.pop(y_name))
   return xy, xy
 
 
 class RegressionTest(tf.test.TestCase):
   """Test the regression examples in this directory."""
 
-  @tf.test.mock.patch.dict(premade_estimator.__dict__,
+  @tf.test.mock.patch.dict(iris_data.__dict__,
                            {"load_data": four_lines_data})
   def test_premade_estimator(self):
     premade_estimator.main([None, "--train_steps=1"])
 
-  @tf.test.mock.patch.dict(custom_estimator.__dict__,
+  @tf.test.mock.patch.dict(iris_data.__dict__,
                            {"load_data": four_lines_data})
   def test_custom_estimator(self):
     custom_estimator.main([None, "--train_steps=1"])
