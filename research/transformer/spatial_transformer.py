52adc91a2ae665f55cc3983f6f374ef35353862a
Neal Wu <neal@nealwu.com>
2017-11-21 15:08:33 -0800

    Restore blank line

--- research/transformer/spatial_transformer.py
+++ research/transformer/spatial_transformer.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
 from six.moves import xrange
 import tensorflow as tf
 
