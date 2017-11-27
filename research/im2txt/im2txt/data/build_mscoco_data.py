3b8c1f5ab82f93539d82bb75267627a12440fed7
孙洪志 <sunhongzhi@baofeng.com>
2017-11-27 15:37:17 +0800

    流程跑通，还差数据库存储

--- research/im2txt/im2txt/data/build_mscoco_data.py
+++ research/im2txt/im2txt/data/build_mscoco_data.py
@@ -263,7 +263,7 @@ def _process_image_files(thread_index, ranges, name, images, decoder, vocab,
   num_images_in_thread = ranges[thread_index][1] - ranges[thread_index][0]
 
   counter = 0
-  for s in xrange(num_shards_per_batch):
+  for s in range(num_shards_per_batch):
     # Generate a sharded version of the file name, e.g. 'train-00002-of-00010'
     shard = thread_index * num_shards_per_batch + s
     output_filename = "%s-%.5d-of-%.5d" % (name, shard, num_shards)
@@ -319,7 +319,7 @@ def _process_dataset(name, images, vocab, num_shards):
   spacing = np.linspace(0, len(images), num_threads + 1).astype(np.int)
   ranges = []
   threads = []
-  for i in xrange(len(spacing) - 1):
+  for i in range(len(spacing) - 1):
     ranges.append([spacing[i], spacing[i + 1]])
 
   # Create a mechanism for monitoring when all threads are finished.
@@ -330,7 +330,7 @@ def _process_dataset(name, images, vocab, num_shards):
 
   # Launch a thread for each batch.
   print("Launching %d threads for spacings: %s" % (num_threads, ranges))
-  for thread_index in xrange(len(ranges)):
+  for thread_index in range(len(ranges)):
     args = (thread_index, ranges, name, images, decoder, vocab, num_shards)
     t = threading.Thread(target=_process_image_files, args=args)
     t.start()
--- research/im2txt/im2txt/run_inference.py
+++ research/im2txt/im2txt/run_inference.py
@@ -21,13 +21,26 @@ from __future__ import print_function
 import math
 import os
 
-
 import tensorflow as tf
 
 from im2txt import configuration
 from im2txt import inference_wrapper
 from im2txt.inference_utils import caption_generator
 from im2txt.inference_utils import vocabulary
+import pymysql
+import re
+import time
+import socket
+from urllib import request
+from io import BytesIO
+from PIL import Image
+import numpy as np
+
+mysql_host = '119.18.193.226'
+mysql_user = 'bfsportsdt'
+mysql_pass = '85iwx|qttHsrlxPeyldb'
+user_set_last_deal_id = 500000
+news_image_table = 'news_image'
 
 FLAGS = tf.flags.FLAGS
 
@@ -35,51 +48,107 @@ tf.flags.DEFINE_string("checkpoint_path", "",
                        "Model checkpoint file or directory containing a "
                        "model checkpoint file.")
 tf.flags.DEFINE_string("vocab_file", "", "Text file containing the vocabulary.")
-tf.flags.DEFINE_string("input_files", "",
-                       "File pattern or comma-separated list of file patterns "
-                       "of image files.")
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
+def _gain_image_info(conn):
+    cur = conn.cursor()
+    sql = 'select max(original_id) from ai_image.{}'.format('news_image_im2text')
+    cur.execute(sql)
+    data = cur.fetchone()
+    if data is not None and len(data) != 0 and data[0] is not None:
+        last_max_id = data[0]
+    else:
+        last_max_id = user_set_last_deal_id
+
+    sql = 'select id, content from sports.news where id > {} order by id limit 1000'.format(int(last_max_id))
+    cur.execute(sql)
+    all_data = cur.fetchall()
+    cur.close()
+    if len(all_data) == 0:
+        return all_data
+
+    res = []
+    for original_id, content in all_data:
+        imgs = re.findall(pattern='http://image.sports.baofeng.com/[\w]+', string=content)
+        res.append((original_id, imgs))
+    return res
+
+
+def get_mysql_conn():
+    return pymysql.connect(
+        host=mysql_host,
+        user=mysql_user,
+        passwd=mysql_pass,
+        charset='utf8'
+    )
+
+
+# help code
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+
 def main(_):
-  # Build the inference graph.
-  g = tf.Graph()
-  with g.as_default():
-    model = inference_wrapper.InferenceWrapper()
-    restore_fn = model.build_graph_from_config(configuration.ModelConfig(),
-                                               FLAGS.checkpoint_path)
-  g.finalize()
+    # Build the inference graph.
+    g = tf.Graph()
+    with g.as_default():
+        model = inference_wrapper.InferenceWrapper()
+        restore_fn = model.build_graph_from_config(configuration.ModelConfig(),
+                                                   FLAGS.checkpoint_path)
+    g.finalize()
 
-  # Create the vocabulary.
-  vocab = vocabulary.Vocabulary(FLAGS.vocab_file)
+    # Create the vocabulary.
+    vocab = vocabulary.Vocabulary(FLAGS.vocab_file)
 
-  filenames = []
-  for file_pattern in FLAGS.input_files.split(","):
-    filenames.extend(tf.gfile.Glob(file_pattern))
-  tf.logging.info("Running caption generation on %d files matching %s",
-                  len(filenames), FLAGS.input_files)
+    # filenames = []
+    # for file_pattern in FLAGS.input_files.split(","):
+    #     filenames.extend(tf.gfile.Glob(file_pattern))
+    # tf.logging.info("Running caption generation on %d files matching %s", len(filenames), FLAGS.input_files)
 
-  with tf.Session(graph=g) as sess:
-    # Load the model from checkpoint.
-    restore_fn(sess)
+    with tf.Session(graph=g) as sess:
+        # Load the model from checkpoint.
+        restore_fn(sess)
 
-    # Prepare the caption generator. Here we are implicitly using the default
-    # beam search parameters. See caption_generator.py for a description of the
-    # available beam search parameters.
-    generator = caption_generator.CaptionGenerator(model, vocab)
+        # Prepare the caption generator. Here we are implicitly using the default
+        # beam search parameters. See caption_generator.py for a description of the
+        # available beam search parameters.
+        generator = caption_generator.CaptionGenerator(model, vocab)
 
-    for filename in filenames:
-      with tf.gfile.GFile(filename, "r") as f:
-        image = f.read()
-      captions = generator.beam_search(sess, image)
-      print("Captions for image %s:" % os.path.basename(filename))
-      for i, caption in enumerate(captions):
-        # Ignore begin and end words.
-        sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
-        sentence = " ".join(sentence)
-        print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
+        count = 0
+        if True:
+            conn = get_mysql_conn()
+            info_list = _gain_image_info(conn)
+            conn.close()
+            total_start_time = time.time()
+            save_info = []
+            # info_list = [["im2txt/data/COCO_val2014_000000224477.jpg"]]
+            print("info_list size = ", len(info_list))
+            for original_id, image_list in info_list:
+                if image_list is None or len(image_list) == 0:
+                    continue
+                for image_path in image_list:
+                    print("open =>>>>>>>>>>>> ", image_path)
+                    if image_path.startswith('http://'):
+                        socket.setdefaulttimeout(2)
+                        r = request.urlopen(image_path)
+                        image_data = r.read()  # 采用StringIO直接图片文件写到内存，省去写入硬盘
+                    else:
+                        count = count + 1
+                        with tf.gfile.FastGFile(image_path, "rb") as f:
+                            image_data = f.read()
+
+                    captions = generator.beam_search(sess, image_data)
+                    print("Captions for image %s:" % os.path.basename(image_path))
+                    for i, caption in enumerate(captions):
+                        # Ignore begin and end words.
+                        sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
+                        sentence = " ".join(sentence)
+                        print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
 
 
 if __name__ == "__main__":
-  tf.app.run()
+    tf.app.run()
