514afd2baa112ed08ff2377b9d193a16b04d8867
Karmel Allison <karmel@google.com>
2017-12-19 13:41:21 -0800

    Image provisioning is a pain...

--- official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
+++ official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
@@ -4,9 +4,24 @@ from __future__ import print_function
 
 import tensorflow as tf
 
+
+tf.app.flags.DEFINE_integer("task_index", None, "Task index, should be >= 0.")
+tf.app.flags.DEFINE_string("job_name", None, "job name: worker or ps")
+tf.app.flags.DEFINE_string("ps_hosts", None, "Comma-separated list of hostname:port pairs")
+tf.app.flags.DEFINE_string("worker_hosts", None, "Comma-separated list of hostname:port pairs")
+
+
 def main():
   s = tf.constant("Hello, Tensorflow!")
   print(tf.Session().run(s))
+  import os
+  print('git repo', os.listdir('/opt/tf-models'))
+  print('cuda', os.listdir('/usr/lib/cuda'))
+  print('nvidia', os.listdir('/usr/lib/nvidia'))
+  print('gnu', os.listdir('/usr/lib/x86_64-linux-gnu'))
+  print('ld lib path', os.environ['LD_LIBRARY_PATH'])
+  import time
+  time.sleep(300)
 
 
 if __name__ == '__main__':
