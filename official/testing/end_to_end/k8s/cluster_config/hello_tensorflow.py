b9ea161602073e6eec454f2ed2c126bb6a92c52d
Karmel Allison <karmel@google.com>
2017-12-23 21:30:24 -0800

    Keep server alive

--- official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
+++ official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
@@ -6,7 +6,7 @@ from time import gmtime, sleep, strftime
 
 import tensorflow as tf
 
-#from models.official.resnet.imagenet_test import *
+from models.official.resnet.imagenet_test import *
 
 tf.app.flags.DEFINE_integer("task_index", None, "Task index, should be >= 0.")
 tf.app.flags.DEFINE_string("job_name", None, "job name: worker or ps")
@@ -17,11 +17,11 @@ tf.app.flags.DEFINE_string("worker_hosts", None, "Comma-separated list of hostna
 def main():
   import sys
   print('System path', sys.path)
-  #print(tf.test.main())
+  print(tf.test.main())
 
-  while True:
-    print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-    sleep(30)
+  #while True:
+  #  print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+  #  sleep(30)
 
 
 if __name__ == '__main__':
