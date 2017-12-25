251ed26c263139905e62501f05cb2bfbe1937751
Karmel Allison <karmel@google.com>
2017-12-25 14:33:51 -0800

    Try testing cifar

--- official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
+++ official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
@@ -3,22 +3,29 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import argparse
 
 import tensorflow as tf
 
-#from models.official.resnet.imagenet_test import *
+import models
+
 
 tf.app.flags.DEFINE_integer("task_index", None, "Task index, should be >= 0.")
 tf.app.flags.DEFINE_string("job_name", None, "job name: worker or ps")
 tf.app.flags.DEFINE_string("ps_hosts", None, "Comma-separated list of hostname:port pairs")
 tf.app.flags.DEFINE_string("worker_hosts", None, "Comma-separated list of hostname:port pairs")
+tf.app.flags.DEFINE_string("data_dir", None, "Data dir to store data for testing.")
 
 
-def main():
+def main(args):
   import sys
   print('System path', sys.path)
-  print(os.listdir('/opt/')
+  print(os.listdir(FLAGS.data_dir))
+  models.official.resnet.cifar10_download_and_extract(FLAGS.data_dir)
+  models.official.resnet.cifar10_main.main()
 
 
 if __name__ == '__main__':
-  main()
+  tf.logging.set_verbosity(tf.logging.INFO)
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(argv=[sys.argv[0]])
