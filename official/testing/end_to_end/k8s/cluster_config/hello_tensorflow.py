058d6e1f3b08c2f02afaed7f0b15b9e0555e7017
Karmel Allison <karmel@google.com>
2017-12-26 13:04:03 -0800

    Running as scripts instead

--- official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
+++ official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
@@ -3,12 +3,16 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import sys
 import argparse
 
 import tensorflow as tf
 
-import models
+import cifar10_download_and_extract, cifar10_main
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', type=str, default='/tmp/cifar10_data',
+  help='The path to the CIFAR-10 data directory.')
 
 tf.app.flags.DEFINE_integer("task_index", None, "Task index, should be >= 0.")
 tf.app.flags.DEFINE_string("job_name", None, "job name: worker or ps")
@@ -19,13 +23,12 @@ tf.app.flags.DEFINE_string("data_dir", None, "Data dir to store data for testing
 
 def main(args):
   import sys
-  print('System path', sys.path)
+  #cifar10_download_and_extract.main(args)
   print(os.listdir(FLAGS.data_dir))
-  models.official.resnet.cifar10_download_and_extract(FLAGS.data_dir)
-  models.official.resnet.cifar10_main.main()
+  #cifar10_main.main()
 
 
 if __name__ == '__main__':
   tf.logging.set_verbosity(tf.logging.INFO)
   FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(argv=[sys.argv[0]])
+  tf.app.run(argv=[sys.argv[0]] + unparsed)
