a8514d6b971a5314d393acc9c2a2fc8c4637e0d1
Karmel Allison <karmel@google.com>
2017-12-30 14:30:23 -0800

    Debugging libcuda errors

--- official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
+++ official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
@@ -6,13 +6,7 @@ import os
 import sys
 import argparse
 
-import tensorflow as tf
-
-import cifar10_download_and_extract, cifar10_main
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', type=str, default='/tmp/cifar10_data',
-  help='The path to the CIFAR-10 data directory.')
+#import tensorflow as tf
 
 tf.app.flags.DEFINE_integer("task_index", None, "Task index, should be >= 0.")
 tf.app.flags.DEFINE_string("job_name", None, "job name: worker or ps")
@@ -22,13 +16,9 @@ tf.app.flags.DEFINE_string("data_dir", None, "Data dir to store data for testing
 
 
 def main(args):
-  import sys
-  #cifar10_download_and_extract.main(args)
-  print(os.listdir(FLAGS.data_dir))
-  #cifar10_main.main()
+  print(os.environ['LD_LIBRARY_PATH'])
+  import tensorflow as tf
 
 
 if __name__ == '__main__':
-  tf.logging.set_verbosity(tf.logging.INFO)
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(argv=[sys.argv[0]] + unparsed)
+  main()
