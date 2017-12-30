241b068a6dda62e2fc57a463a81f1da6eee09e10
Karmel Allison <karmel@google.com>
2017-12-30 14:50:33 -0800

    Debugging libcuda.so error

--- official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
+++ official/testing/end_to_end/k8s/cluster_config/hello_tensorflow.py
@@ -8,16 +8,16 @@ import argparse
 
 #import tensorflow as tf
 
-tf.app.flags.DEFINE_integer("task_index", None, "Task index, should be >= 0.")
-tf.app.flags.DEFINE_string("job_name", None, "job name: worker or ps")
-tf.app.flags.DEFINE_string("ps_hosts", None, "Comma-separated list of hostname:port pairs")
-tf.app.flags.DEFINE_string("worker_hosts", None, "Comma-separated list of hostname:port pairs")
-tf.app.flags.DEFINE_string("data_dir", None, "Data dir to store data for testing.")
+#tf.app.flags.DEFINE_integer("task_index", None, "Task index, should be >= 0.")
+#tf.app.flags.DEFINE_string("job_name", None, "job name: worker or ps")
+#tf.app.flags.DEFINE_string("ps_hosts", None, "Comma-separated list of hostname:port pairs")
+#tf.app.flags.DEFINE_string("worker_hosts", None, "Comma-separated list of hostname:port pairs")
+#tf.app.flags.DEFINE_string("data_dir", None, "Data dir to store data for testing.")
 
 
 def main(args):
   print('LD_LIBRARY_PATH', os.environ['LD_LIBRARY_PATH'])
-  import tensorflow as tf
+  #import tensorflow as tf
 
 
 if __name__ == '__main__':
