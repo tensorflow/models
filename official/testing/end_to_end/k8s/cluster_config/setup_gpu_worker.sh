b2ca6be8854fb2a2d8876562601b23b1c36e9e9c
Karmel Allison <karmel@google.com>
2018-01-01 13:29:25 -0800

    Fixing cuda install

--- official/testing/end_to_end/k8s/cluster_config/setup_gpu_worker.sh
+++ official/testing/end_to_end/k8s/cluster_config/setup_gpu_worker.sh
@@ -12,7 +12,16 @@ if ! dpkg-query -W cuda; then
   curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
   dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
   apt-get update
-  apt-get install cuda -y
+  apt-get install cuda=8.0.61-1 -y
 fi
 apt-get install -y nvidia-cuda-toolkit
+
+# Install corresponding cuDNN
+if ! ls /usr/local/cuda/lib64 | grep cudnn; then
+  wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/cudnn-8.0-linux-x64-v6.0.tgz
+  tar -xvzf cudnn-8.0-linux-x64-v6.0.tgz
+  cp cuda/include/cudnn.h /usr/local/cuda/include
+  cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+  chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+fi
 
