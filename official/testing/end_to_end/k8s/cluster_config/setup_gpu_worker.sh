4a74e18f5fd53c2002c4e032c1cdc3b4379792e3
Karmel Allison <karmel@google.com>
2018-01-01 20:23:02 -0800

    Nvidia-docker

--- official/testing/end_to_end/k8s/cluster_config/setup_gpu_worker.sh
+++ official/testing/end_to_end/k8s/cluster_config/setup_gpu_worker.sh
@@ -25,3 +25,20 @@ if ! ls /usr/local/cuda/lib64 | grep cudnn; then
   chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
 fi
 
+
+# Install nvidia-docker
+# If you have nvidia-docker 1.0 installed: we need to remove it and all existing GPU containers
+docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
+sudo apt-get purge -y nvidia-docker
+
+# Add the package repositories
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+
+# Install nvidia-docker2 and reload the Docker daemon configuration
+sudo apt-get install -y nvidia-docker2
+sudo pkill -SIGHUP dockerd
+
