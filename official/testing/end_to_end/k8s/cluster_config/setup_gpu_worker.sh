dade31eefd748bcf23a05373ab8576071e04d70e
Karmel Allison <karmel@google.com>
2018-01-04 10:18:10 -0800

    nvidia-docker

--- official/testing/end_to_end/k8s/cluster_config/setup_gpu_worker.sh
+++ official/testing/end_to_end/k8s/cluster_config/setup_gpu_worker.sh
@@ -27,10 +27,6 @@ fi
 
 
 # Install nvidia-docker
-# If you have nvidia-docker 1.0 installed: we need to remove it and all existing GPU containers
-docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
-sudo apt-get purge -y nvidia-docker
-
 # Add the package repositories
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
   sudo apt-key add -
@@ -41,4 +37,5 @@ sudo apt-get update
 # Install nvidia-docker2 and reload the Docker daemon configuration
 sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
+
 
