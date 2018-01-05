ec658316c1b018f343d25a6435b13bd4d6d9d2cb
Karmel Allison <karmel@google.com>
2018-01-04 16:02:31 -0800

    Debugging missing file in docker

--- official/testing/end_to_end/cluster_config/run_resnet_cifar10.py
+++ official/testing/end_to_end/cluster_config/run_resnet_cifar10.py
@@ -15,6 +15,9 @@
 """
 Downloads data and runs ResNet model on the CIFAR-10 dataset.
 
+TODO(karmel): This should not be in cluster_config directory, but for some
+reason, I can't access it from outside when building dockerfile.
+
 TODO(karmel): Clean this up and make reusable.
 
 """
