cf24296f0463483006ad8e20008c599a7538b5ab
Karmel Allison <karmel@google.com>
2018-01-05 15:38:25 -0800

    Clean up and renaming

--- official/testing/end_to_end/cluster/cluster_config.sh
+++ official/testing/end_to_end/cluster/cluster_config.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# These variables will be used for Kubernetes cluster set up.
+# For full available set, please see Kubernetes documentation at
+# https://kubernetes.io/docs/getting-started-guides/gce/
 
 KUBE_ROOT=$HOME/kubernetes
 KUBE_GCE_ZONE=europe-west1-d
 NUM_NODES=2
 NODE_SIZE=n1-standard-8
 NODE_DISK_SIZE=200GB
-KUBE_GCE_INSTANCE_PREFIX=tf-models-$$
+KUBE_GCE_INSTANCE_PREFIX=tf-models-cluster
 
 KUBE_OS_DISTRIBUTION=ubuntu
 KUBE_MASTER_OS_DISTRIBUTION=ubuntu
@@ -16,7 +19,7 @@ KUBE_GCE_NODE_PROJECT=ubuntu-os-cloud
 KUBE_GCE_NODE_IMAGE=ubuntu-1604-xenial-v20161130
 KUBE_NODE_EXTRA_METADATA=startup-script=setup_worker.sh
 
-USE_GPU=true
+USE_GPU=${USE_GPU:-true}
 if [ "$USE_GPU" = true ] ; then
   # With GPU
   GPUS_PER_WORKER=8
