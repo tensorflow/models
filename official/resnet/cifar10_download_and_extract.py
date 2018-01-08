73885f3d95e6493dabc71edaaa64bc4b38b43b72
Karmel Allison <karmel@google.com>
2018-01-08 13:29:31 -0800

    Debugging URL problems

--- official/resnet/cifar10_download_and_extract.py
+++ official/resnet/cifar10_download_and_extract.py
@@ -50,14 +50,13 @@ def main(unused_argv):
           filename, 100.0 * count * block_size / total_size))
       sys.stdout.flush()
 
-    print(DATA_URL)
-    print(filepath)
+    import time
+    time.sleep(600)
     filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
     print()
     statinfo = os.stat(filepath)
     print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
-    import time
-    time.sleep(600)
+
   tarfile.open(filepath, 'r:gz').extractall(FLAGS.data_dir)
 
 
