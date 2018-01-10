5105ce71ccba27c4ce4ac831d09d4ba9c75b01ce
ashaw596 <ashaw596@gmail.com>
2018-01-10 12:17:40 -0500

    Fix bug in gym_wrapper.
    Added an 'env' parameter to env_step function since one an unreferenced one is used in the function.

--- research/pcl_rl/gym_wrapper.py
+++ research/pcl_rl/gym_wrapper.py
@@ -92,14 +92,14 @@ class GymWrapper(object):
 
   def step(self, actions):
 
-    def env_step(action):
+    def env_step(env, action):
       action = self.env_spec.convert_action_to_gym(action)
       obs, reward, done, tt = env.step(action)
       obs = self.env_spec.convert_obs_to_list(obs)
       return obs, reward, done, tt
 
     actions = zip(*actions)
-    outputs = [env_step(action)
+    outputs = [env_step(env, action)
                if not done else (self.env_spec.initial_obs(None), 0, True, None)
                for action, env, done in zip(actions, self.envs, self.dones)]
     for i, (_, _, done, _) in enumerate(outputs):
