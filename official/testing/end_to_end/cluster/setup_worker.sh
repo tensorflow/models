ae128f80f57171033220006ac1d3bb870331c9ac
Karmel Allison <karmel@google.com>
2018-01-17 11:22:27 -0800

    Removing CUDA install

--- official/testing/end_to_end/cluster/setup_worker.sh
+++ official/testing/end_to_end/cluster/setup_worker.sh
@@ -3,30 +3,14 @@
 set -e
 set -x
 
-# TODO(karmel): Installing CUDA shouldn't be necessary if using nvidia-docker;
-# just need the drivers apt-get install nvidia 387 384
 # Install cuda if a CUDA-capable GPU is detected
 if lspci | grep -i nvidia; then
-  echo "Checking for CUDA and installing."
-  # Check for CUDA and try to install.
-  if ! dpkg-query -W cuda; then
-    # The 16.04 installer works with 16.10.
-    curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-    dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-    apt-get update
-    apt-get install cuda=8.0.61-1 -y
-  fi
-  apt-get install -y nvidia-cuda-toolkit
+  echo "Installing NVIDIA drivers"
 
-  # Install corresponding cuDNN
-  if ! ls /usr/local/cuda/lib64 | grep cudnn; then
-    wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/cudnn-8.0-linux-x64-v6.0.tgz
-    tar -xvzf cudnn-8.0-linux-x64-v6.0.tgz
-    cp cuda/include/cudnn.h /usr/local/cuda/include
-    cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
-    chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
-  fi
-
+  apt-get purge nvidia-*
+  add-apt-repository ppa:graphics-drivers -y
+  apt-get update
+  apt-get install nvidia-384 -y
 
   # Install nvidia-docker
   # Add the package repositories
--- official/testing/end_to_end/workload/launch_jobs.py
+++ official/testing/end_to_end/workload/launch_jobs.py
@@ -32,9 +32,6 @@ import yaml
 _DOCKER_IMAGE_PATTERN = 'gcr.io/%s/tf-models-cluster/'
 _OUTPUT_FILE_ENV_VAR = 'TF_DIST_BENCHMARK_RESULTS_FILE'
 _TEST_NAME_ENV_VAR = 'TF_DIST_BENCHMARK_NAME'
-# TODO(karmel): With nvidia-docker, these should be unnecessary.
-_LD_LIBRARY_PATH = '/usr/local/cuda:/usr/lib/nvidia:/usr/lib/x86_64-linux-gnu'
-_CUDA_HOME = '/usr/local/cuda'
 _PORT = 5000
 
 
@@ -211,7 +208,7 @@ def main():
       name_to_docker_image[name] = docker_image
     else:
       docker_image = _GetMostRecentDockerImageFromGcloud(
-          docker_image_pattern % name)
+          FLAGS.docker_image_pattern % name)
       if not docker_image:
         raise NoImageFoundError('No tags found for image %s.' % docker_image)
 
@@ -226,8 +223,6 @@ def main():
     if gpu_count > 0:
       # TODO(karmel): What are volume mounts used for? Should they be kept?
       volumes = get_gpu_volume_mounts()
-      env_vars['LD_LIBRARY_PATH'] = FLAGS.ld_library_path or _LD_LIBRARY_PATH
-      env_vars['CUDA_HOME'] = FLAGS.cuda_lib_dir or _CUDA_HOME
 
     env_vars.update(config.get('env_vars', {}))
     args = config.get('args', {})
@@ -297,10 +292,6 @@ if __name__ == '__main__':
   parser.add_argument(
       '--nvidia_lib_dir', type=str, default=None, required=False,
       help='Directory where nvidia library files are located on gcloud node.')
-  parser.add_argument(
-      '--ld_library_path', type=str, default=None, required=False,
-      help='LD_LIBRARY_PATH on node to be passed to the pods. Default: '
-        + _LD_LIBRARY_PATH)
   FLAGS, _ = parser.parse_known_args()
   logging.basicConfig(level=logging.DEBUG)
   main()
