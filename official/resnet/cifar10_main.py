c2f0d80a5defccc55ffa5cdecd6e34c8d459b9b9
Karmel Allison <karmel@google.com>
2018-01-25 16:40:24 -0800

    Testing remotely...

--- official/resnet/cifar10_main.py
+++ official/resnet/cifar10_main.py
@@ -174,7 +174,7 @@ class Cifar10Model(resnet_model.Model):
     if resnet_size % 6 != 2:
       raise ValueError('resnet_size must be 6n + 2:', resnet_size)
 
-    num_blocks = (self.resnet_size - 2) // 6
+    num_blocks = (resnet_size - 2) // 6
     return [num_blocks] * 3
 
   def _get_stride_sizes(self):
