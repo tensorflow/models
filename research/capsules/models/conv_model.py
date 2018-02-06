66bdca52e9a0dd9b1b404b99d8a570d30caf38ef
cclauss <cclauss@bluewin.ch>
2018-02-06 10:10:09 +0100

    Capsules: Define xrange() for Python 3
    __xrange()__ was removed In Python 3 in favor of __range()__.

--- research/capsules/models/conv_model.py
+++ research/capsules/models/conv_model.py
@@ -22,6 +22,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from six.moves import xrange
 import tensorflow as tf
 from models import model
 from models.layers import variables
