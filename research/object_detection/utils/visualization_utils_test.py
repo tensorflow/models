cdc1185f4a9d2fe85848567df1781b3b76b25892
cclauss <cclauss@bluewin.ch>
2018-02-16 15:45:23 +0100

    ObjDet: print() is a function in Python 3

--- research/object_detection/utils/visualization_utils_test.py
+++ research/object_detection/utils/visualization_utils_test.py
@@ -20,6 +20,8 @@ https://drive.google.com/a/google.com/file/d/0B5HnKS_hMsNARERpU3MtU3I5RFE/view?u
 
 """
 
+from __future__ import print_function
+
 import os
 
 import numpy as np
@@ -145,7 +147,7 @@ class VisualizationUtilsTest(tf.test.TestCase):
         for i in range(images_with_boxes_np.shape[0]):
           img_name = 'image_' + str(i) + '.png'
           output_file = os.path.join(self.get_temp_dir(), img_name)
-          print 'Writing output image %d to %s' % (i, output_file)
+          print('Writing output image %d to %s' % (i, output_file))
           image_pil = Image.fromarray(images_with_boxes_np[i, ...])
           image_pil.save(output_file)
 
