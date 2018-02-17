93b5a6dc23ff2bfc847851150c5396467a15870d
Joshua Payne <snare117@gmail.com>
2018-02-16 23:12:49 -0800

    Update convert_npy.py

--- official/mnist/convert_npy.py
+++ official/mnist/convert_npy.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+"""Converts an image file to a prediction-ready numpy array."""
 
 import argparse
 import os
