1cd09cbaf4ddf394866e1c06aec572de60a4ddb6
look4pritam <pritam.shete@gmail.com>
2018-03-27 15:15:03 +0530

    Dataset tools are added.

--- research/mtcnn/datasets/BasicDataset.py
+++ research/mtcnn/datasets/BasicDataset.py
@@ -126,6 +126,43 @@ class BasicDataset(AbstractDataset):
                 				f2.write("PNet/negative/%s.jpg" % n_idx + ' 0\n')
                 				cv2.imwrite(save_file, resized_im)
                 				n_idx += 1 
+
+				for i in range(20):
+            				size = npr.randint(int(min(w, h) * 0.8), np.ceil(1.25 * max(w, h)))
+
+            				delta_x = npr.randint(-w * 0.2, w * 0.2)
+            				delta_y = npr.randint(-h * 0.2, h * 0.2)
+
+            				nx1 = int(max(x1 + w / 2 + delta_x - size / 2, 0))
+            				ny1 = int(max(y1 + h / 2 + delta_y - size / 2, 0))
+            				nx2 = nx1 + size
+            				ny2 = ny1 + size
+
+            				if nx2 > width or ny2 > height:
+                				continue 
+            				crop_box = np.array([nx1, ny1, nx2, ny2])
+            				offset_x1 = (x1 - nx1) / float(size)
+					offset_y1 = (y1 - ny1) / float(size)
+            				offset_x2 = (x2 - nx2) / float(size)
+            				offset_y2 = (y2 - ny2) / float(size)
+
+            				cropped_im = img[ny1 : ny2, nx1 : nx2, :]
+            				resized_im = cv2.resize(cropped_im, (12, 12), interpolation=cv2.INTER_LINEAR)
+
+            				box_ = box.reshape(1, -1)
+            				if IoU(crop_box, box_) >= 0.65:
+                				save_file = os.path.join(positive_dir, "%s.jpg"%p_idx)
+                				f1.write("PNet/positive/%s.jpg"%p_idx + ' 1 %.2f %.2f %.2f %.2f\n'%(offset_x1, offset_y1, offset_x2, offset_y2))
+                				cv2.imwrite(save_file, resized_im)
+                				p_idx += 1
+            				elif IoU(crop_box, box_) >= 0.4:
+                				save_file = os.path.join(part_dir, "%s.jpg"%d_idx)
+                				f3.write("PNet/part/%s.jpg"%d_idx + ' -1 %.2f %.2f %.2f %.2f\n'%(offset_x1, offset_y1, offset_x2, offset_y2))
+                				cv2.imwrite(save_file, resized_im)
+                				d_idx += 1
+
+				box_idx += 1
+				print( "%s images done, pos: %s part: %s neg: %s"%(idx, p_idx, d_idx, n_idx))
 		f3.close()
 		f2.close()
 		f1.close()
