d724a08ba34a96024dbf11c9c0a69553f84666ed
Zhichao Lu <lzc@google.com>
2018-03-27 14:51:56 -0700

    Add warning if shuffle=false but num_readers > 1. The latter would cause the input data stream to still be shuffled.
    PiperOrigin-RevId: 190678792

--- research/object_detection/utils/dataset_util.py
+++ research/object_detection/utils/dataset_util.py
@@ -123,6 +123,10 @@ def read_dataset(file_read_func, decode_func, input_files, config):
   if config.shuffle:
     filename_dataset = filename_dataset.shuffle(
         config.filenames_shuffle_buffer_size)
+  elif config.num_readers > 1:
+    tf.logging.warning('`shuffle` is false, but the input data stream is '
+                       'still slightly shuffled since `num_readers` > 1.')
+
   filename_dataset = filename_dataset.repeat(config.num_epochs or None)
 
   records_dataset = filename_dataset.apply(
