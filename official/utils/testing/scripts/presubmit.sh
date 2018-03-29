8c32d18561085edd991f7dc302b1848e2ead4510
Scott Zhu <scottzhu@google.com>
2018-03-29 15:20:02 -0700

    Update the test script to be more modularized.
    1. Check the script file location and cd into repo root dir.
    2. Allow caller to call differnt tests.

--- official/utils/testing/scripts/presubmit.sh
+++ official/utils/testing/scripts/presubmit.sh
@@ -3,41 +3,74 @@
 # Presubmit script that run tests and lint under local environment.
 # Make sure that tensorflow and pylint is installed.
 # usage: models >: ./official/utils/testing/scripts/presubmit.sh
+# usage: models >: ./official/utils/testing/scripts/presubmit.sh lint py2_test py3_test
 set +x
 
-# Assume the pwd is under models.
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR/../../../.."
+MODEL_ROOT="$(pwd)"
+
+export PYTHONPATH="$PYTHONPATH:${MODEL_ROOT}"
+
 cd official
 
-exit_code=0
+lint() {
+  local exit_code=0
 
-# Check lint:
-RC_FILE="utils/testing/pylint.rcfile"
+  RC_FILE="utils/testing/pylint.rcfile"
 
-echo "===========Running lint test============"
-for file in `find . -name '*.py' ! -name '*test.py' -print`
-do
-  echo "Linting ${file}"
-  pylint --rcfile="${RC_FILE}" "${file}" || exit_code=$?
-done
+  echo "===========Running lint test============"
+  for file in `find . -name '*.py' ! -name '*test.py' -print`
+  do
+    echo "Linting ${file}"
+    pylint --rcfile="${RC_FILE}" "${file}" || exit_code=$?
+  done
 
-# More lenient for test files.
-for file in `find . -name '*test.py' -print`
-do
-  echo "Linting ${file}"
-  pylint --rcfile="${RC_FILE}" --disable=missing-docstring,protected-access "${file}" || exit_code=$?
-done
+  # More lenient for test files.
+  for file in `find . -name '*test.py' -print`
+  do
+    echo "Linting ${file}"
+    pylint --rcfile="${RC_FILE}" --disable=missing-docstring,protected-access "${file}" || exit_code=$?
+  done
 
-# Run Python test, the python version depends on the local configured defaults.
-echo "===========Running Python test============"
+  return "${exit_code}"
+}
 
-for test_file in `find . -name '*test.py' -print`
-do
-  echo "Testing ${test_file}"
-  python "${test_file}" || exit_code=$?
-done
+py_test() {
+  local PY_BINARY="$1"
+  local exit_code=0
 
-if [ ${exit_code} = "0" ]; then
-  echo "BUILD FAILED"
+  echo "===========Running Python test============"
+
+  for test_file in `find . -name '*test.py' -print`
+  do
+    echo "Testing ${test_file}"
+    ${PY_BINARY} "${test_file}" || exit_code=$?
+  done
+
+  return "${exit_code}"
+}
+
+py2_test() {
+  local PY_BINARY=$(which python2)
+  return $(py_test "${PY_BINARY}")
+}
+
+py3_test() {
+  local PY_BINARY=$(which python3)
+  return $(py_test "${PY_BINARY}")
+}
+
+test_result=0
+
+if [ "$#" -eq 0 ]; then
+  TESTS="lint py2_test py3_test"
 else
-  echo "BUILD SUCCESS"
+  TESTS="$@"
 fi
+
+for t in "${TESTS}"; do
+  ${t} || test_result=$?
+done
+
+exit "${test_result}"
