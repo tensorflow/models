3197b44c7ecdf443bad9b1e95a221bcd83d5568f
Frank Chen <frankchn@google.com>
2018-04-30 14:09:59 -0700

    Remove parent path

--- official/mnist/mnist_tpu.py
+++ official/mnist/mnist_tpu.py
@@ -29,7 +29,6 @@ import sys
 import tensorflow as tf  # pylint: disable=g-bad-import-order
 
 # For open source environment, add parent and grandparent directories for import
-sys.path.append(os.path.dirname(os.path.abspath(sys.path[0])))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(sys.path[0]))))
 
 from official.mnist import dataset  # pylint: disable=wrong-import-position
