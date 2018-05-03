51a2b441c65e5df4fa7b540c2c1211e3adff53cb
Qianli Scott Zhu <scottzhu@google.com>
2018-05-03 11:03:54 -0700

    Remove unused imports. (#4160)

--- official/mnist/mnist.py
+++ official/mnist/mnist.py
@@ -17,9 +17,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import argparse
-import sys
-
 from absl import app as absl_app
 from absl import flags
 import tensorflow as tf  # pylint: disable=g-bad-import-order
--- official/resnet/resnet_run_loop.py
+++ official/resnet/resnet_run_loop.py
@@ -23,7 +23,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import argparse
 import os
 
 from absl import flags
