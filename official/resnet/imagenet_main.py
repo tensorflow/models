567391a27170bfd0a3f59450363c81485af6f782
Taylor Robie <taylorrobie@google.com>
2018-05-03 18:38:32 -0700

    fix indent

--- official/resnet/imagenet_main.py
+++ official/resnet/imagenet_main.py
@@ -208,7 +208,7 @@ class ImagenetModel(resnet_model.Model):
       num_classes: The number of output classes needed from the model. This
         enables users to extend the same model to their own datasets.
       resnet_version: Integer representing which version of the ResNet network
-      to use. See README for details. Valid values: [1, 2]
+        to use. See README for details. Valid values: [1, 2]
       dtype: The TensorFlow dtype to use for calculations.
     """
 
