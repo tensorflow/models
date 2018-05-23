a6561347cccf9b832bc52861ec0fabe32b2ca8d7
Karmel Allison <karmel@google.com>
2018-05-23 14:11:26 -0700

    Using BenchmarkLogger

--- official/utils/logs/hooks_test.py
+++ official/utils/logs/hooks_test.py
@@ -75,27 +75,27 @@ class ExamplesPerSecondHookTest(tf.test.TestCase):
     mon_sess = monitored_session._HookedSession(sess, [hook])  # pylint: disable=protected-access
     sess.run(tf.global_variables_initializer())
 
-    self.logged_message = ''
+    self.logged_message = ""
     for _ in range(every_n_steps):
       mon_sess.run(self.train_op)
-      self.assertEqual(str(self.logged_message).find('exp/sec'), -1)
+      self.assertEqual(str(self.logged_message).find("exp/sec"), -1)
 
     mon_sess.run(self.train_op)
     global_step_val = sess.run(self.global_step)
     # assertNotRegexpMatches is not supported by python 3.1 and later
     if global_step_val > warm_steps:
-      self.assertRegexpMatches(str(self.logged_message), 'exp/sec')
+      self.assertRegexpMatches(str(self.logged_message), "exp/sec")
     else:
-      self.assertEqual(str(self.logged_message).find('exp/sec'), -1)
+      self.assertEqual(str(self.logged_message).find("exp/sec"), -1)
 
     # Add additional run to verify proper reset when called multiple times.
-    self.logged_message = ''
+    self.logged_message = ""
     mon_sess.run(self.train_op)
     global_step_val = sess.run(self.global_step)
     if every_n_steps == 1 and global_step_val > warm_steps:
-      self.assertRegexpMatches(str(self.logged_message), 'exp/sec')
+      self.assertRegexpMatches(str(self.logged_message), "exp/sec")
     else:
-      self.assertEqual(str(self.logged_message).find('exp/sec'), -1)
+      self.assertEqual(str(self.logged_message).find("exp/sec"), -1)
 
     hook.end(sess)
 
@@ -124,14 +124,14 @@ class ExamplesPerSecondHookTest(tf.test.TestCase):
     mon_sess = monitored_session._HookedSession(sess, [hook])  # pylint: disable=protected-access
     sess.run(tf.global_variables_initializer())
 
-    self.logged_message = ''
+    self.logged_message = ""
     mon_sess.run(self.train_op)
-    self.assertEqual(str(self.logged_message).find('exp/sec'), -1)
+    self.assertEqual(str(self.logged_message).find("exp/sec"), -1)
     time.sleep(every_n_secs)
 
-    self.logged_message = ''
+    self.logged_message = ""
     mon_sess.run(self.train_op)
-    self.assertRegexpMatches(str(self.logged_message), 'exp/sec')
+    self.assertRegexpMatches(str(self.logged_message), "exp/sec")
 
     hook.end(sess)
 
@@ -144,5 +144,5 @@ class ExamplesPerSecondHookTest(tf.test.TestCase):
       self._validate_log_every_n_secs(sess, 5)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   tf.test.main()
