615e9f68c209239321cf66a30bed300b5f591fa4
mbolaris <mike@bolaris.com>
2018-05-29 17:55:48 -0700

    Update xml_to_cvs.py

--- research/object_detection/coin_detector/xml_to_cvs.py
+++ research/object_detection/coin_detector/xml_to_cvs.py
@@ -3,7 +3,6 @@ import glob
 import pandas as pd
 import xml.etree.ElementTree as ET
 
-
 def xml_to_csv(path):
     xml_list = []
     for xml_file in glob.glob(path + '/*.xml'):
@@ -24,13 +23,11 @@ def xml_to_csv(path):
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
 
-
 def main():
     for folder in ['train','test']:
         image_path = os.path.join(os.getcwd(), ('images/' + folder))
         xml_df = xml_to_csv(image_path)
         xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
-        print('Successfully converted xml to csv.')
-
+        print('Successfully converted ' + folder + ' xml to csv.')
 
 main()
