588385034bc945ec3868ab7a6f4463370daa01ac
mbolaris <mike@bolaris.com>
2018-05-29 18:14:23 -0700

    Update generate_tfrecord.py

--- research/object_detection/coin_detector/generate_tfrecord.py
+++ research/object_detection/coin_detector/generate_tfrecord.py
@@ -16,8 +16,6 @@ import io
 import pandas as pd
 import tensorflow as tf
 
-from .. import object_detection
-
 from PIL import Image
 from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
