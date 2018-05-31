8f246eda23a1894a3f664c7370d6d0f946719610
mbolaris <mike@bolaris.com>
2018-05-30 20:48:32 -0700

    Update resizer.py

--- research/object_detection/coin_detector/images/resizer.py
+++ research/object_detection/coin_detector/images/resizer.py
@@ -16,6 +16,7 @@ dir_path = os.getcwd()
 for filename in os.listdir(dir_path):
     # If the images are not .JPG images, change the line below to match the image type.
     if filename.endswith(".jpg"):
+        print(resizing)
         image = cv2.imread(filename)
         resized = cv2.resize(image,None,fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
         cv2.imwrite(filename,resized)
