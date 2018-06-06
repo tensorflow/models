1f310dcee676e0fdf9a0e9ab312d6ac08c793da0
haoliang <haoliang@google.com>
2018-06-06 13:24:40 -0700

    update file comment

--- research/deep_speech/data/download.py
+++ research/deep_speech/data/download.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # ==============================================================================
-"""tf.data.Dataset interface to the LibriSpeech dataset."""
+"""Download and preprocess LibriSpeech dataset for DeepSpeech model."""
 
 from __future__ import absolute_import
 from __future__ import division
