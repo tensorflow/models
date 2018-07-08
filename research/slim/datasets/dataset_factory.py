cfbe651a784e35ec21e2cfd9d78a5054df95daad
PhenixI <zhanzhanlove.hi@gmail.com>
2018-07-08 22:23:37 +0800

    add new datasets

--- research/slim/datasets/dataset_factory.py
+++ research/slim/datasets/dataset_factory.py
@@ -22,12 +22,14 @@ from datasets import cifar10
 from datasets import flowers
 from datasets import imagenet
 from datasets import mnist
+from datasets import satellite
 
 datasets_map = {
     'cifar10': cifar10,
     'flowers': flowers,
     'imagenet': imagenet,
     'mnist': mnist,
+    'satellite':satellite
 }
 
 
