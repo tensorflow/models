4b5e56ba703a07eed0378d6bd2687ba25615f921
Scott Zhu <scottzhu@google.com>
2018-07-13 09:05:03 -0700

    Add shorter timeout for GCP util.

--- official/utils/logs/cloud_lib.py
+++ official/utils/logs/cloud_lib.py
@@ -25,7 +25,8 @@ GCP_METADATA_HEADER = {"Metadata-Flavor": "Google"}
 def on_gcp():
   """Detect whether the current running environment is on GCP."""
   try:
-    response = requests.get(GCP_METADATA_URL, headers=GCP_METADATA_HEADER)
+    response = requests.get(
+        GCP_METADATA_URL, headers=GCP_METADATA_HEADER, timeout=5)
     return response.status_code == 200
   except requests.exceptions.RequestException:
     return False
