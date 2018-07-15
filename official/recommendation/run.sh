6709bf0f619c80583d5203eb7365e202242eb4cd
Taylor Robie <taylorrobie@google.com>
2018-07-15 13:55:21 -0700

    make run.sh a bit nicer

--- official/recommendation/run.sh
+++ official/recommendation/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+DATASET="ml-20m"
+
 ROOT_DIR="/tmp/MLPerf_NCF_$(python3 -c 'import uuid; print(str(uuid.uuid4().hex))')"
 echo "Root directory: ${ROOT_DIR}"
 
@@ -8,28 +10,32 @@ mkdir -p ${ROOT_DIR}
 
 DATA_DIR="${ROOT_DIR}/movielens_data"
 mkdir ${DATA_DIR}
-python ../datasets/movielens.py --data_dir ${DATA_DIR} --dataset ml-20m
+python ../datasets/movielens.py --data_dir ${DATA_DIR} --dataset ${DATASET}
+
+{
 
 for i in `seq 0 4`;
 do
   START_TIME=$(date +%s)
   MODEL_DIR="${ROOT_DIR}/run_${i}"
   RUN_LOG="${ROOT_DIR}/run_${i}.log"
+  echo ""
   echo "Beginning run ${i}"
   python ncf_main.py --model_dir ${MODEL_DIR} --data_dir ${DATA_DIR} \
-                     --dataset ml-20m --hooks "" \
+                     --dataset ${DATASET} --hooks "" \
                      --clean \
                      --train_epochs 20 \
                      --batch_size 16384 \
                      --learning_rate 0.0005 \
                      --layers 256,256,128,64 --num_factors 64 \
-  |& cat > ${RUN_LOG}
+  |& tee ${RUN_LOG} \
+  | grep --line-buffered  -E --regexp="Iteration [0-9]+: HR = [0-9\.]+, NDCG = [0-9\.]+"
 
   END_TIME=$(date +%s)
   echo "Run ${i} complete: $(( $END_TIME - $START_TIME )) seconds."
 
-  grep -E --regexp="Iteration [0-9]+: HR = [0-9\.]+, NDCG = [0-9\.]+" ${RUN_LOG} | cat
-
 done
+
+} |& tee "${ROOT_DIR}/summary.log"
 
 #rm -r ${ROOT_DIR}
