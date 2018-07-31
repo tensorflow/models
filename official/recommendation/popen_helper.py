e4ab5a8608553787154d2e5c4916c00a582c47ba
Reed <reedwm@google.com>
2018-07-31 15:35:13 -0700

    Fix crash when Python interpreter not on PATH. (#4961)
    * Fix crash when Python interpreter not on PATH.
    
    * Fix lint error.

--- official/recommendation/popen_helper.py
+++ official/recommendation/popen_helper.py
@@ -15,10 +15,13 @@
 """Helper file for running the async data generation process in OSS."""
 
 import os
-import six
+import sys
 
 
-_PYTHON = "python3" if six.PY3 else "python2"
+_PYTHON = sys.executable
+if not _PYTHON:
+  raise RuntimeError("Could not find path to Python interpreter in order to "
+                     "spawn subprocesses.")
 
 _ASYNC_GEN_PATH = os.path.join(os.path.dirname(__file__),
                                "data_async_generation.py")
