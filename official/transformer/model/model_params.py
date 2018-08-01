65362d494d7846e9435e397614ce6e8b174812fe
Jiang Yu <theJiangYu@gmail.com>
2018-08-01 12:05:23 -0700

    Change vocab_size from 33708 to 33945 in model_params.py
    Change vocab_size from 33708 to 33945 in models/official/transformer/model/model_params.py
    The original vocab_size causes InvalidArgumentError when training Transformer using CPU.

--- official/transformer/model/model_params.py
+++ official/transformer/model/model_params.py
@@ -27,7 +27,7 @@ BASE_PARAMS = defaultdict(
 
     # Model params
     initializer_gain=1.0,  # Used in trainable variable initialization.
-    vocab_size=33708,  # Number of tokens defined in the vocabulary file.
+    vocab_size=33945,  # Number of tokens defined in the vocabulary file.
     hidden_size=512,  # Model dimension in the hidden layers.
     num_hidden_layers=6,  # Number of layers in the encoder and decoder stacks.
     num_heads=8,  # Number of heads to use in multi-headed attention.
