0b251bdfc46efb744c5500eca1a31474480d0591
Taylor Robie <taylorrobie@google.com>
2018-09-24 16:18:13 -0700

    increase batch size for faster failure

--- official/resnet/run_repro.sh
+++ official/resnet/run_repro.sh
@@ -7,6 +7,11 @@ popd
 
 TIME=$(date +%s)
 
+
+# Batch size of 2400 is in place to fail quickly
+# A batch size of 2048 is most common, with 1024 also
+# reasonably common. 2400 fails immediately, 2048 fails on
+# epoch 22, and 1024 fails on epoch 46.
 python3 imagenet_main.py --clean --data_dir /imn/imagenet/combined/ \
-  --model_dir /tmp/garden_imagenet --train_epochs 30 --batch_size 2048 --resnet_version 1 \
+  --model_dir /tmp/garden_imagenet --train_epochs 30 --batch_size 2400 --resnet_version 1 \
   --resnet_size 50 --dtype fp16 --num_gpus 8 |& tee imagenet_garden_${TIME}.log
