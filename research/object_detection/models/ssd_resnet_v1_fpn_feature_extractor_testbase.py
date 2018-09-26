31dbcdc5729abc53e8c376559381e6fef19c1351
Zhichao Lu <lzc@google.com>
2018-09-26 11:15:31 -0700

    Fix the proprocess() function in Resnet v1 to make it work for any number of input channels.
    Note: If the #channels != 3, this will simply skip the mean subtraction in preprocess() function.
    PiperOrigin-RevId: 214635428

--- research/object_detection/models/ssd_resnet_v1_fpn_feature_extractor_testbase.py
+++ research/object_detection/models/ssd_resnet_v1_fpn_feature_extractor_testbase.py
@@ -82,12 +82,15 @@ class SSDResnetFPNFeatureExtractorTestBase(
     image_width = 128
     depth_multiplier = 1
     pad_to_multiple = 1
-    test_image = np.random.rand(4, image_height, image_width, 3)
+    test_image = tf.constant(np.random.rand(4, image_height, image_width, 3))
     feature_extractor = self._create_feature_extractor(depth_multiplier,
                                                        pad_to_multiple)
     preprocessed_image = feature_extractor.preprocess(test_image)
-    self.assertAllClose(preprocessed_image,
-                        test_image - [[123.68, 116.779, 103.939]])
+    with self.test_session() as sess:
+      test_image_out, preprocessed_image_out = sess.run(
+          [test_image, preprocessed_image])
+      self.assertAllClose(preprocessed_image_out,
+                          test_image_out - [[123.68, 116.779, 103.939]])
 
   def test_variables_only_created_in_scope(self):
     depth_multiplier = 1
@@ -103,5 +106,3 @@ class SSDResnetFPNFeatureExtractorTestBase(
         self.assertTrue(
             variable.name.startswith(self._resnet_scope_name())
             or variable.name.startswith(self._fpn_scope_name()))
-
-
