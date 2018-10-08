259198c28093fe73cb8ff5d6dcd928607d542882
Cameron Newton <newtoncameron@hotmail.com>
2018-10-08 12:37:15 -0700

    Edited github script

--- setup_environment.sh
+++ setup_environment.sh
@@ -11,12 +11,14 @@ declare -a new_shell_config_lines=(
     export PYTHONPATH=$PYTHONPATH:$CURR_DIR/research:$CURR_DIR/research/slim
 )
 
+#Installing python dependencies
 pip install virtualenv
 
 virtualenv subbots_python
 source $CURR_DIR/subbots_python/bin/activate
 pip install -r $CURR_DIR/requirements.txt
 
+#Testing installation
 python $CURR_DIR/research/object_detection/builders/model_builder_test.py
 
 
