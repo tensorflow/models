bd86e960df8bfd5457c32b7768ff5dfbc4ce917d
Toby Boyd <tobyboyd@google.com>
2018-10-12 13:02:14 -0700

    perf_args piped in and add back top_1 and top_5

--- official/resnet/resnet_run_loop.py
+++ official/resnet/resnet_run_loop.py
@@ -431,25 +431,25 @@ def resnet_model_fn(features, labels, mode, model_class,
     train_op = None
 
   accuracy = tf.metrics.accuracy(labels, predictions['classes'])
-  #accuracy_top_5 = tf.metrics.mean(tf.nn.in_top_k(predictions=logits,
-  #                                                targets=labels,
-  #                                                k=5,
-  #                                                name='top_5_op'))
-  metrics = {'accuracy': accuracy}
-          #   'accuracy_top_5': accuracy_top_5}
+  accuracy_top_5 = tf.metrics.mean(tf.nn.in_top_k(predictions=logits,
+                                                  targets=labels,
+                                                  k=5,
+                                                  name='top_5_op'))
+  metrics = {'accuracy': accuracy,
+             'accuracy_top_5': accuracy_top_5}
 
   # Create a tensor named train_accuracy for logging purposes
   tf.identity(accuracy[1], name='train_accuracy')
-  #tf.identity(accuracy_top_5[1], name='train_accuracy_top_5')
+  tf.identity(accuracy_top_5[1], name='train_accuracy_top_5')
   tf.summary.scalar('train_accuracy', accuracy[1])
-  #tf.summary.scalar('train_accuracy_top_5', accuracy_top_5[1])
+  tf.summary.scalar('train_accuracy_top_5', accuracy_top_5[1])
 
   return tf.estimator.EstimatorSpec(
       mode=mode,
       predictions=predictions,
       loss=loss,
-      train_op=train_op)
-      #eval_metric_ops=metrics)
+      train_op=train_op,
+      eval_metric_ops=metrics)
 
 
 def resnet_main(
