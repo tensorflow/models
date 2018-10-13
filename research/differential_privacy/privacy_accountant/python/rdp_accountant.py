fb5ac1c5418a0a9f77b4dca8738c137a3a133296
Ilya Mironov <mironov@google.com>
2018-10-12 17:25:23 -0700

    Streamlining rdp_accountant.

--- research/differential_privacy/privacy_accountant/python/rdp_accountant.py
+++ research/differential_privacy/privacy_accountant/python/rdp_accountant.py
@@ -12,21 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""RDP analysis of the Gaussian-with-sampling mechanism.
+"""RDP analysis of the Sampled Gaussian mechanism.
 
-Functionality for computing Renyi differential privacy of an additive Gaussian
-mechanism with sampling. Its public interface consists of two methods:
-  compute_rdp(q, sigma, T, orders) computes RDP with the sampling rate q,
-                                   noise sigma, T steps at the list of orders.
+Functionality for computing Renyi differential privacy (RDP) of an additive
+Sampled Gaussian mechanism (SGM). Its public interface consists of two methods:
+  compute_rdp(q, stddev_to_sensitivity_ratio, T, orders) computes RDP with for
+                                   SGM iterated T times.
   get_privacy_spent(orders, rdp, target_eps, target_delta) computes delta
                                    (or eps) given RDP at multiple orders and
                                    a target value for eps (or delta).
 
 Example use:
 
-Suppose that we have run an algorithm with parameters, an array of
-(q1, sigma1, T1) ... (qk, sigma_k, Tk), and we wish to compute eps for a given
-delta. The example code would be:
+Suppose that we have run an SGM applied to a function with l2-sensitivity 1.
+Its parameters are given as a list of tuples (q1, sigma1, T1), ...,
+(qk, sigma_k, Tk), and we wish to compute eps for a given delta.
+The example code would be:
 
   max_order = 32
   orders = range(2, max_order + 1)
@@ -43,15 +44,12 @@ import sys
 
 from absl import app
 from absl import flags
+
 import math
+import sys
+
 import numpy as np
 from scipy import special
-
-FLAGS = flags.FLAGS
-flags.DEFINE_boolean("rdp_verbose", False,
-                     "Output intermediate results for RDP computation.")
-FLAGS(sys.argv)  # Load the flags (including on import)
-
 
 ########################
 # LOG-SPACE ARITHMETIC #
@@ -68,10 +66,13 @@ def _log_add(logx, logy):
 
 
 def _log_sub(logx, logy):
-  """Subtract two numbers in the log space. Answer must be positive."""
+  """Subtract two numbers in the log space. Answer must be non-negative."""
+  if logx < logy:
+    raise ValueError("The result of subtraction must be non-negative .")
   if logy == -np.inf:  # subtracting 0
     return logx
-  assert logx > logy
+  if logx == logy:
+    return -np.inf  # 0 is represented as -np.inf in the log space.
 
   try:
     # Use exp(x) - exp(y) = (exp(x - y) - 1) * exp(y).
@@ -89,84 +90,58 @@ def _log_print(logx):
 
 
 def _compute_log_a_int(q, sigma, alpha):
-  """Compute log(A_alpha) for integer alpha."""
+  """Compute log(A_alpha) for integer alpha. 0 < q < 1."""
   assert isinstance(alpha, (int, long))
 
-  # The first and second terms of A_alpha in the log space:
-  log_a1, log_a2 = -np.inf, -np.inf
+  # Initialize with 0 in the log space.
+  log_a = -np.inf
 
   for i in range(alpha + 1):
-    # Compute in the log space. Extra care needed for q = 0 or 1.
-    log_coef_i = math.log(special.binom(alpha, i))
-    if q > 0:
-      log_coef_i += i * math.log(q)
-    elif i > 0:
-      continue  # The term is 0, skip the rest.
+    log_coef_i = (
+        math.log(special.binom(alpha, i)) + i * math.log(q) +
+        (alpha - i) * math.log(1 - q))
 
-    if q < 1.0:
-      log_coef_i += (alpha - i) * math.log(1 - q)
-    elif i < alpha:
-      continue  # The term is 0, skip the rest.
+    s = log_coef_i + (i * i - i) / (2 * (sigma**2))
+    log_a = _log_add(log_a, s)
 
-    s1 = log_coef_i + (i * i - i) / (2.0 * (sigma ** 2))
-    s2 = log_coef_i + (i * i + i) / (2.0 * (sigma ** 2))
-    log_a1 = _log_add(log_a1, s1)
-    log_a2 = _log_add(log_a2, s2)
-
-  log_a = _log_add(math.log(1 - q) + log_a1, math.log(q) + log_a2)
-  if FLAGS.rdp_verbose:
-    print("A: by binomial expansion    {} = {} + {}".format(
-        _log_print(log_a),
-        _log_print(math.log(1 - q) + log_a1), _log_print(math.log(q) + log_a2)))
   return float(log_a)
 
 
 def _compute_log_a_frac(q, sigma, alpha):
-  """Compute log(A_alpha) for fractional alpha."""
-  # The four parts of A_alpha in the log space:
-  log_a11, log_a12 = -np.inf, -np.inf
-  log_a21, log_a22 = -np.inf, -np.inf
+  """Compute log(A_alpha) for fractional alpha. 0 < q < 1."""
+  # The two parts of A_alpha, integrals over (-inf,z0] and (z0, +inf), are
+  # initialized to 0 in the log space:
+  log_a0, log_a1 = -np.inf, -np.inf
   i = 0
 
-  z0, _ = _compute_zs(sigma, q)
+  z0 = sigma**2 * math.log(1 / q - 1) + .5
 
   while True:  # do ... until loop
     coef = special.binom(alpha, i)
     log_coef = math.log(abs(coef))
     j = alpha - i
 
-    log_t1 = log_coef + i * math.log(q) + j * math.log(1 - q)
-    log_t2 = log_coef + j * math.log(q) + i * math.log(1 - q)
+    log_t0 = log_coef + i * math.log(q) + j * math.log(1 - q)
+    log_t1 = log_coef + j * math.log(q) + i * math.log(1 - q)
 
-    log_e11 = math.log(.5) + _log_erfc((i - z0) / (math.sqrt(2) * sigma))
-    log_e12 = math.log(.5) + _log_erfc((z0 - j) / (math.sqrt(2) * sigma))
-    log_e21 = math.log(.5) + _log_erfc((i - (z0 - 1)) / (math.sqrt(2) * sigma))
-    log_e22 = math.log(.5) + _log_erfc((z0 - 1 - j) / (math.sqrt(2) * sigma))
+    log_e0 = math.log(.5) + _log_erfc((i - z0) / (math.sqrt(2) * sigma))
+    log_e1 = math.log(.5) + _log_erfc((z0 - j) / (math.sqrt(2) * sigma))
 
-    log_s11 = log_t1 + (i * i - i) / (2 * (sigma ** 2)) + log_e11
-    log_s12 = log_t2 + (j * j - j) / (2 * (sigma ** 2)) + log_e12
-    log_s21 = log_t1 + (i * i + i) / (2 * (sigma ** 2)) + log_e21
-    log_s22 = log_t2 + (j * j + j) / (2 * (sigma ** 2)) + log_e22
+    log_s0 = log_t0 + (i * i - i) / (2 * (sigma**2)) + log_e0
+    log_s1 = log_t1 + (j * j - j) / (2 * (sigma**2)) + log_e1
 
     if coef > 0:
-      log_a11 = _log_add(log_a11, log_s11)
-      log_a12 = _log_add(log_a12, log_s12)
-      log_a21 = _log_add(log_a21, log_s21)
-      log_a22 = _log_add(log_a22, log_s22)
+      log_a0 = _log_add(log_a0, log_s0)
+      log_a1 = _log_add(log_a1, log_s1)
     else:
-      log_a11 = _log_sub(log_a11, log_s11)
-      log_a12 = _log_sub(log_a12, log_s12)
-      log_a21 = _log_sub(log_a21, log_s21)
-      log_a22 = _log_sub(log_a22, log_s22)
+      log_a0 = _log_sub(log_a0, log_s0)
+      log_a1 = _log_sub(log_a1, log_s1)
 
     i += 1
-    if max(log_s11, log_s21, log_s21, log_s22) < -30:
+    if max(log_s0, log_s1) < -30:
       break
 
-  log_a = _log_add(
-      math.log(1. - q) + _log_add(log_a11, log_a12),
-      math.log(q) + _log_add(log_a21, log_a22))
-  return log_a
+  return _log_add(log_a0, log_a1)
 
 
 def _compute_log_a(q, sigma, alpha):
@@ -178,91 +153,20 @@ def _compute_log_a(q, sigma, alpha):
 
 
 def _log_erfc(x):
-  # Can be replaced with a single call to log_ntdr if available:
-  # return np.log(2.) + special.log_ntdr(-x * 2**.5)
-  r = special.erfc(x)
-  if r == 0.0:
-    # Using the Laurent series at infinity for the tail of the erfc function:
-    #     erfc(x) ~ exp(-x^2-.5/x^2+.625/x^4)/(x*pi^.5)
-    # To verify in Mathematica:
-    #     Series[Log[Erfc[x]] + Log[x] + Log[Pi]/2 + x^2, {x, Infinity, 6}]
-    return (-math.log(math.pi) / 2 - math.log(x) - x ** 2 - .5 * x ** -2 +
-            .625 * x ** -4 - 37. / 24. * x ** -6 + 353. / 64. * x ** -8)
-  else:
-    return math.log(r)
-
-
-def _compute_zs(sigma, q):
-  z0 = sigma ** 2 * math.log(1 / q - 1) + .5
-  z1 = min(z0 - 2, z0 / 2)
-  return z0, z1
-
-
-def _compute_log_b0(sigma, q, alpha, z1):
-  """Return an approximation to log(B0) or None if failed to converge."""
-  z0, _ = _compute_zs(sigma, q)
-  s, log_term, log_b0, k, sign, max_log_term = 0, 1., 0, 0, 1, -np.inf
-  # Keep adding new terms until precision is no longer preserved.
-  # Don't stop on the negative.
-  while (k < alpha or (log_term > max_log_term - 36 and log_term > -30) or
-         sign < 0.):
-    log_b1 = k * (k - 2 * z0) / (2 * sigma ** 2)
-    log_b2 = _log_erfc((k - z1) / (math.sqrt(2) * sigma))
-    log_term = log_b0 + log_b1 + log_b2
-    max_log_term = max(max_log_term, log_term)
-    s += sign * math.exp(log_term)
-    k += 1
-    # Maintain invariant: sign * exp(log_b0) = {-alpha choose k}
-    log_b0 += math.log(abs(-alpha - k + 1)) - math.log(k)
-    sign *= -1
-
-  if s == 0:  # May happen if all terms are < 1e-324.
-    return -np.inf
-  if s < 0 or math.log(s) < max_log_term - 25:  # The series failed to converge.
-    return None
-  c = math.log(.5) - math.log(1 - q) * alpha
-  return c + math.log(s)
-
-
-def _bound_log_b1(sigma, q, alpha, z1):
-  log_c = _log_add(math.log(1 - q),
-                   math.log(q) + (2 * z1 - 1.) / (2 * sigma ** 2))
-  return math.log(.5) - log_c * alpha + _log_erfc(z1 / (math.sqrt(2) * sigma))
-
-
-def _bound_log_b(q, sigma, alpha):
-  """Compute a numerically stable bound on log(B_alpha)."""
-  if q == 1.:  # If the sampling rate is 100%, A and B are symmetric.
-    return _compute_log_a(q, sigma, alpha)
-
-  z0, z1 = _compute_zs(sigma, q)
-  log_b_bound = np.inf
-
-  # Puts a lower bound on B1: it cannot be less than its value at z0.
-  log_lb_b1 = _bound_log_b1(sigma, q, alpha, z0)
-
-  while z0 - z1 > 1e-3:
-    m = (z0 + z1) / 2
-    log_b0 = _compute_log_b0(sigma, q, alpha, m)
-    if log_b0 is None:
-      z0 = m
-      continue
-    log_b1 = _bound_log_b1(sigma, q, alpha, m)
-    log_b_bound = min(log_b_bound, _log_add(log_b0, log_b1))
-    log_b_min_bound = _log_add(log_b0, log_lb_b1)
-    if (log_b_bound < 0 or
-        log_b_min_bound < 0 or
-        log_b_bound > log_b_min_bound + .01):
-      # If the bound is likely to be too loose, move z1 closer to z0 and repeat.
-      z1 = m
+  try:
+    return math.log(2) + special.log_ndtr(-x * 2**.5)
+  except NameError:
+    # If log_ndtr is not available, approximate as follows:
+    r = special.erfc(x)
+    if r == 0.0:
+      # Using the Laurent series at infinity for the tail of the erfc function:
+      #     erfc(x) ~ exp(-x^2-.5/x^2+.625/x^4)/(x*pi^.5)
+      # To verify in Mathematica:
+      #     Series[Log[Erfc[x]] + Log[x] + Log[Pi]/2 + x^2, {x, Infinity, 6}]
+      return (-math.log(math.pi) / 2 - math.log(x) - x**2 - .5 * x**-2 +
+              .625 * x**-4 - 37. / 24. * x**-6 + 353. / 64. * x**-8)
     else:
-      break
-
-  return log_b_bound
-
-
-def _log_bound_b_elementary(q, alpha):
-  return -math.log(1 - q) * alpha
+      return math.log(r)
 
 
 def _compute_delta(orders, rdp, eps):
@@ -319,7 +223,7 @@ def _compute_eps(orders, rdp, delta):
 
 
 def _compute_rdp(q, sigma, alpha):
-  """Compute RDP of the Gaussian mechanism with sampling at order alpha.
+  """Compute RDP of the Sampled Gaussian mechanism at order alpha.
 
   Args:
     q: The sampling rate.
@@ -329,37 +233,25 @@ def _compute_rdp(q, sigma, alpha):
   Returns:
     RDP at alpha, can be np.inf.
   """
+  if q == 0:
+    return 0
+
+  if q == 1.:
+    return alpha / (2 * sigma**2)
+
   if np.isinf(alpha):
     return np.inf
 
-  log_moment_a = _compute_log_a(q, sigma, alpha - 1)
-
-  log_bound_b = _log_bound_b_elementary(q, alpha - 1)  # does not require sigma
-
-  if log_bound_b < log_moment_a:
-    if FLAGS.rdp_verbose:
-      print("Elementary bound suffices   : {} < {}".format(
-          _log_print(log_bound_b), _log_print(log_moment_a)))
-  else:
-    log_bound_b2 = _bound_log_b(q, sigma, alpha - 1)
-    if math.isnan(log_bound_b2):
-      if FLAGS.rdp_verbose:
-        print("B bound failed to converge")
-    else:
-      if FLAGS.rdp_verbose and (log_bound_b2 < log_bound_b):
-        print("Elementary bound is stronger: {} < {}".format(
-            _log_print(log_bound_b2), _log_print(log_bound_b)))
-      log_bound_b = min(log_bound_b, log_bound_b2)
-
-  return max(log_moment_a, log_bound_b) / (alpha - 1)
+  return _compute_log_a(q, sigma, alpha) / (alpha - 1)
 
 
-def compute_rdp(q, sigma, steps, orders):
-  """Compute RDP of Gaussian mechanism with sampling for given parameters.
+def compute_rdp(q, stddev_to_sensitivity_ratio, steps, orders):
+  """Compute RDP of the Sampled Gaussian Mechanism for given parameters.
 
   Args:
     q: The sampling rate.
-    sigma: The std of the additive Gaussian noise.
+    stddev_to_sensitivity_ratio: The ratio of std of the Gaussian noise to the
+      l2-sensitivity of the function to which it is added.
     steps: The number of steps.
     orders: An array (or a scalar) of RDP orders.
 
@@ -368,9 +260,10 @@ def compute_rdp(q, sigma, steps, orders):
   """
 
   if np.isscalar(orders):
-    rdp = _compute_rdp(q, sigma, orders)
+    rdp = _compute_rdp(q, stddev_to_sensitivity_ratio, orders)
   else:
-    rdp = np.array([_compute_rdp(q, sigma, order) for order in orders])
+    rdp = np.array([_compute_rdp(q, stddev_to_sensitivity_ratio, order)
+                    for order in orders])
 
   return rdp * steps
 
@@ -405,11 +298,3 @@ def get_privacy_spent(orders, rdp, target_eps=None, target_delta=None):
   else:
     eps, opt_order = _compute_eps(orders, rdp, target_delta)
     return eps, target_delta, opt_order
-
-
-def main(_):
-  pass
-
-
-if __name__ == "__main__":
-  app.run(main)
--- research/differential_privacy/privacy_accountant/python/rdp_accountant_test.py
+++ research/differential_privacy/privacy_accountant/python/rdp_accountant_test.py
@@ -19,155 +19,137 @@ from __future__ import division
 from __future__ import print_function
 
 from absl.testing import absltest
+from absl.testing import parameterized
+
 import mpmath as mp
 import numpy as np
+import sys
 
 import rdp_accountant
 
 
-class TestGaussianMoments(absltest.TestCase):
-  ##############################
-  # MULTI-PRECISION ARITHMETIC #
-  ##############################
+class TestGaussianMoments(parameterized.TestCase):
+  #################################
+  # HELPER FUNCTIONS:             #
+  # Exact computations using      #
+  # multi-precision arithmetic.   #
+  #################################
 
-  def _pdf_gauss_mp(self, x, sigma, mean):
-    return 1. / mp.sqrt(2. * sigma ** 2 * mp.pi) * mp.exp(
-        -(x - mean) ** 2 / (2. * sigma ** 2))
+  def _log_float_mp(self, x):
+    # Convert multi-precision input to float log space.
+    if x >= sys.float_info.min:
+      return float(mp.log(x))
+    else:
+      return -np.inf
 
-  def _integral_inf_mp(self, fn):
-    integral, _ = mp.quad(
-        fn, [-mp.inf, mp.inf], error=True,
-        maxdegree=7)  # maxdegree = 6 is not enough
-    return integral
-
-  def _integral_bounded_mp(self, fn, lb, ub):
-    integral, _ = mp.quad(fn, [lb, ub], error=True)
+  def _integral_mp(self, fn, bounds=(-mp.inf, mp.inf)):
+    integral, _ = mp.quad(fn, bounds, error=True, maxdegree=8)
     return integral
 
   def _distributions_mp(self, sigma, q):
-    mu0 = lambda y: self._pdf_gauss_mp(y, sigma=sigma, mean=0)
-    mu1 = lambda y: self._pdf_gauss_mp(y, sigma=sigma, mean=1.)
-    mu = lambda y: (1 - q) * mu0(y) + q * mu1(y)
-    return mu0, mu1, mu
 
-  def compute_a_mp(self, sigma, q, order, verbose=False):
-    """Compute A_lambda for arbitrary lambda by numerical integration."""
-    mp.dps = 100
-    mu0, mu1, mu = self._distributions_mp(sigma, q)
-    a_lambda_fn = lambda z: mu(z) * (mu(z) / mu0(z)) ** order
-    a_lambda = self._integral_inf_mp(a_lambda_fn)
+    def _mu0(x):
+      return mp.npdf(x, mu=0, sigma=sigma)
 
-    if verbose:
-      a_lambda_first_term_fn = lambda z: mu0(z) * (mu(z) / mu0(z)) ** order
-      a_lambda_second_term_fn = lambda z: mu1(z) * (mu(z) / mu0(z)) ** order
+    def _mu1(x):
+      return mp.npdf(x, mu=1, sigma=sigma)
 
-      a_lambda_first_term = self._integral_inf_mp(a_lambda_first_term_fn)
-      a_lambda_second_term = self._integral_inf_mp(a_lambda_second_term_fn)
+    def _mu(x):
+      return (1 - q) * _mu0(x) + q * _mu1(x)
 
-      print("A: by numerical integration {} = {} + {}".format(
-          a_lambda, (1 - q) * a_lambda_first_term, q * a_lambda_second_term))
+    return _mu0, _mu  # Closure!
 
-    return a_lambda
+  def _mu1_over_mu0(self, x, sigma):
+    # Closed-form expression for N(1, sigma^2) / N(0, sigma^2) at x.
+    return mp.exp((2 * x - 1) / (2 * sigma**2))
 
-  def compute_b_mp(self, sigma, q, order, verbose=False):
-    """Compute B_lambda for arbitrary lambda by numerical integration."""
-    mu0, _, mu = self._distributions_mp(sigma, q)
-    b_lambda_fn = lambda z: mu0(z) * (mu0(z) / mu(z)) ** order
-    b_numeric = self._integral_inf_mp(b_lambda_fn)
+  def _mu_over_mu0(self, x, q, sigma):
+    return (1 - q) + q * self._mu1_over_mu0(x, sigma)
 
-    if verbose:
-      _, z1 = rdp_accountant._compute_zs(sigma, q)
-      print("z1 = ", z1)
-      print("x in the Taylor series = ", q / (1 - q) * np.exp(
-          (2 * z1 - 1) / (2 * sigma ** 2)))
-
-      b0_numeric = self._integral_bounded_mp(b_lambda_fn, -np.inf, z1)
-      b1_numeric = self._integral_bounded_mp(b_lambda_fn, z1, +np.inf)
-
-      print("B: numerically {} = {} + {}".format(b_numeric, b0_numeric,
-                                                 b1_numeric))
-    return float(b_numeric)
-
-  def _compute_rdp_mp(self, q, sigma, order):
-    log_a_mp = float(mp.log(self.compute_a_mp(sigma, q, order)))
-    log_b_mp = float(mp.log(self.compute_b_mp(sigma, q, order)))
-    return log_a_mp, log_b_mp
+  def _compute_a_mp(self, sigma, q, alpha):
+    """Compute A_alpha for arbitrary alpha by numerical integration."""
+    mu0, _ = self._distributions_mp(sigma, q)
+    a_alpha_fn = lambda z: mu0(z) * self._mu_over_mu0(z, q, sigma)**alpha
+    a_alpha = self._integral_mp(a_alpha_fn)
+    return a_alpha
 
   # TEST ROUTINES
-  def _almost_equal(self, a, b, rtol, atol=0.):
-    # Analogue of np.testing.assert_allclose(a, b, rtol, atol).
-    self.assertBetween(a, b * (1 - rtol) - atol, b * (1 + rtol) + atol)
+  def test_compute_rdp_no_data(self):
+    # q = 0
+    self.assertEqual(rdp_accountant.compute_rdp(0, 10, 1, 20), 0)
 
-  def _compare_bounds(self, q, sigma, order):
-    log_a_mp, log_b_mp = self._compute_rdp_mp(q, sigma, order)
-    log_a = rdp_accountant._compute_log_a(q, sigma, order)
-    log_bound_b = rdp_accountant._bound_log_b(q, sigma, order)
+  def test_compute_rdp_no_sampling(self):
+    # q = 1, RDP = alpha/2 * sigma^2
+    self.assertEqual(rdp_accountant.compute_rdp(1, 10, 1, 20), 0.1)
 
-    if log_a_mp < 1000 and log_a_mp > 1e-6:
-      self._almost_equal(log_a, log_a_mp, rtol=1e-6)
-    else:  # be more tolerant for _very_ large or small logarithms
-      self._almost_equal(log_a, log_a_mp, rtol=1e-3, atol=1e-14)
-
-    if np.isfinite(log_bound_b):
-      # Ignore divergence between the bound and exact value of B if
-      # they don't matter anyway (bound on A is larger) or q > .5
-      if log_bound_b > log_a and q <= .5:
-        self._almost_equal(log_b_mp, log_bound_b, rtol=1e-6, atol=1e-14)
-
-    if np.isfinite(log_a_mp) and np.isfinite(log_b_mp):
-      # We hypothesize that this assertion is always true; no proof yet.
-      self.assertLessEqual(log_b_mp, log_a_mp + 1e-6)
-
-  def test_compute_rdp(self):
+  def test_compute_rdp_scalar(self):
     rdp_scalar = rdp_accountant.compute_rdp(0.1, 2, 10, 5)
     self.assertAlmostEqual(rdp_scalar, 0.07737, places=5)
 
-    rdp_vec = rdp_accountant.compute_rdp(0.01, 2.5, 50, [1.5, 2.5, 5, 50, 100,
-                                                         np.inf])
+  def test_compute_rdp_sequence(self):
+    rdp_vec = rdp_accountant.compute_rdp(0.01, 2.5, 50,
+                                         [1.5, 2.5, 5, 50, 100, np.inf])
+    self.assertSequenceAlmostEqual(
+        rdp_vec, [0.00065, 0.001085, 0.00218075, 0.023846, 167.416307, np.inf],
+        delta=1e-5)
 
-    correct = [0.00065, 0.001085, 0.00218075, 0.023846, 167.416307, np.inf]
-    for i in range(len(rdp_vec)):
-      self.assertAlmostEqual(rdp_vec[i], correct[i], places=5)
+  params = ({'q': 1e-7, 'sigma': .1, 'order': 1.01},
+            {'q': 1e-6, 'sigma': .1, 'order': 256},
+            {'q': 1e-5, 'sigma': .1, 'order': 256.1},
+            {'q': 1e-6, 'sigma': 1, 'order': 27},
+            {'q': 1e-4, 'sigma': 1., 'order': 1.5},
+            {'q': 1e-3, 'sigma': 1., 'order': 2},
+            {'q': .01, 'sigma': 10, 'order': 20},
+            {'q': .1, 'sigma': 100, 'order': 20.5},
+            {'q': .99, 'sigma': .1, 'order': 256},
+            {'q': .999, 'sigma': 100, 'order': 256.1})
 
-  def test_compare_with_mp(self):
-    # Compare the cheap computation with an expensive, multi-precision
-    # computation for a few parameters. Takes a few seconds.
+  # pylint:disable=undefined-variable
+  @parameterized.parameters(p for p in params)
+  def test_compute_log_a_equals_mp(self, q, sigma, order):
+    # Compare the cheap computation of log(A) with an expensive, multi-precision
+    # computation.
+    log_a = rdp_accountant._compute_log_a(q, sigma, order)
+    log_a_mp = self._log_float_mp(self._compute_a_mp(sigma, q, order))
+    np.testing.assert_allclose(log_a, log_a_mp, rtol=1e-4)
 
-    self._compare_bounds(q=.01, sigma=.1, order=.5)
-    self._compare_bounds(q=.1, sigma=1., order=5)
-    self._compare_bounds(q=.5, sigma=2., order=32.5)
-
-    for q in (1e-6, .1, .999):
-      for sigma in (.1, 10., 100.):
-        for order in (1.01, 2, 255.9, 256):
-          self._compare_bounds(q, sigma, order)
-
-  def test_get_privacy_spent(self):
+  def test_get_privacy_spent_check_target_delta(self):
     orders = range(2, 33)
     rdp = rdp_accountant.compute_rdp(0.01, 4, 10000, orders)
-    eps, delta, opt_order = rdp_accountant.get_privacy_spent(orders, rdp,
-                                                             target_delta=1e-5)
+    eps, _, opt_order = rdp_accountant.get_privacy_spent(
+        orders, rdp, target_delta=1e-5)
     self.assertAlmostEqual(eps, 1.258575, places=5)
     self.assertEqual(opt_order, 20)
 
-    eps, delta, _ = rdp_accountant.get_privacy_spent(orders, rdp,
-                                                     target_eps=1.258575)
+  def test_get_privacy_spent_check_target_eps(self):
+    orders = range(2, 33)
+    rdp = rdp_accountant.compute_rdp(0.01, 4, 10000, orders)
+    _, delta, opt_order = rdp_accountant.get_privacy_spent(
+        orders, rdp, target_eps=1.258575)
     self.assertAlmostEqual(delta, 1e-5)
+    self.assertEqual(opt_order, 20)
 
-  def test_compute_privacy_loss(self):
-    parameters = [(0.01, 4, 10000), (0.1, 2, 100)]
-    delta = 1e-5
+  def test_check_composition(self):
     orders = (1.25, 1.5, 1.75, 2., 2.5, 3., 4., 5., 6., 7., 8., 10., 12., 14.,
               16., 20., 24., 28., 32., 64., 256.)
 
-    rdp = np.zeros_like(orders, dtype=float)
-    for q, sigma, steps in parameters:
-      rdp += rdp_accountant.compute_rdp(q, sigma, steps, orders)
-    eps, delta, opt_order = rdp_accountant.get_privacy_spent(orders, rdp,
-                                                             target_delta=delta)
-    self.assertAlmostEqual(eps, 3.276237, places=5)
-    self.assertEqual(opt_order, 8)
+    rdp = rdp_accountant.compute_rdp(q=1e-4,
+                                     stddev_to_sensitivity_ratio=.4,
+                                     steps=40000,
+                                     orders=orders)
+
+    eps, _, opt_order = rdp_accountant.get_privacy_spent(orders, rdp,
+                                                         target_delta=1e-6)
+
+    rdp += rdp_accountant.compute_rdp(q=0.1,
+                                      stddev_to_sensitivity_ratio=2,
+                                      steps=100,
+                                      orders=orders)
+    eps, _, opt_order = rdp_accountant.get_privacy_spent(orders, rdp,
+                                                         target_delta=1e-5)
+    self.assertAlmostEqual(eps, 8.509656, places=5)
+    self.assertEqual(opt_order, 2.5)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   absltest.main()
