a35c5542d6093b8cea03e809cfdef9b70d7d2ae4
cclauss <cclauss@bluewin.ch>
2018-11-28 00:34:19 +0100

    Update object_detection_evaluation.py

--- research/object_detection/utils/object_detection_evaluation.py
+++ research/object_detection/utils/object_detection_evaluation.py
@@ -33,7 +33,6 @@ import collections
 import logging
 import unicodedata
 import numpy as np
-import six
 
 from object_detection.core import standard_fields
 from object_detection.utils import label_map_util
@@ -305,8 +304,8 @@ class ObjectDetectionEvaluator(DetectionEvaluator):
       if idx + self._label_id_offset in category_index:
         category_name = category_index[idx + self._label_id_offset]['name']
         try:
-          category_name = six.text_type(category_name).encode('utf-8')
-        except TypeError:
+          category_name = unicode(category_name, 'utf-8')
+        except (NameError, TypeError):
           pass
         category_name = unicodedata.normalize(
             'NFKD', category_name).encode('ascii', 'ignore')
