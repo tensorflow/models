7b1e3705eaf50dbd77f52888838f57f3286c0235
Shining Sun <shiningsun@google.com>
2018-12-14 12:39:56 -0800

    Revert "first attempt to extract common parts of two main files"
    This reverts commit 3fd9c7fe366af712b11bfb660fcd3db40c9e80d1.

--- official/resnet/keras/keras_main.py
+++ official/resnet/keras/keras_main.py
@@ -33,6 +33,9 @@ from official.utils.logs import logger
 from official.utils.misc import distribution_utils
 from tensorflow.python.keras.optimizer_v2 import gradient_descent as gradient_descent_v2
 
+IMAGENET_DATASET = "imagenet"
+CIFAR_DATASET = "cifar"
+
 
 class TimeHistory(tf.keras.callbacks.Callback):
   """Callback for Keras models."""
@@ -85,6 +88,7 @@ class TimeHistory(tf.keras.callbacks.Callback):
 LR_SCHEDULE = [  # (multiplier, epoch to start) tuples
     (0.1, 91), (0.01, 136), (0.001, 182)
 ]
+
 
 BASE_LEARNING_RATE = 0.1
 
@@ -162,7 +166,6 @@ class LearningRateBatchScheduler(tf.keras.callbacks.Callback):
                    'learning rate to %s.', self.epochs, batch, lr)
 
 
-
 def parse_record_keras(raw_record, is_training, dtype):
   """Parses a record containing a training example of an image.
 
@@ -178,12 +181,28 @@ def parse_record_keras(raw_record, is_training, dtype):
   Returns:
     Tuple with processed image tensor and one-hot-encoded label tensor.
   """
-  image, label = cifar_main.parse_record(raw_record, is_training, dtype)
-  label = tf.sparse_to_dense(label, (cifar_main._NUM_CLASSES,), 1)
+  if flags_obj.dataset == IMAGENET_DATASET:
+    image_buffer, label, bbox = imagenet_main._parse_example_proto(raw_record)
+
+    image = imagenet_preprocessing.preprocess_image(
+        image_buffer=image_buffer,
+        bbox=bbox,
+        output_height=imagenet_main._DEFAULT_IMAGE_SIZE,
+        output_width=imagenet_main._DEFAULT_IMAGE_SIZE,
+        num_channels=imagenet_main._NUM_CHANNELS,
+        is_training=is_training)
+    image = tf.cast(image, dtype)
+    label = tf.sparse_to_dense(label, (imagenet_main._NUM_CLASSES,), 1)
+
+  elif flags_obj.dataset == CIFAR_DATASET:
+    image, label = cifar_main.parse_record(raw_record, is_training, dtype)
+    label = tf.sparse_to_dense(label, (cifar_main._NUM_CLASSES,), 1)
+  else:
+    raise ValueError("Unknown dataset: {%s}".format(flags_obj.dataset))
+
   return image, label
 
-
-def run_cifar_with_keras(flags_obj):
+def run_imagenet_with_keras(flags_obj):
   """Run ResNet ImageNet training and eval loop using native Keras APIs.
 
   Args:
@@ -203,46 +222,8 @@ def run_cifar_with_keras(flags_obj):
   per_device_batch_size = distribution_utils.per_device_batch_size(
       flags_obj.batch_size, flags_core.get_num_gpus(flags_obj))
 
-  # pylint: disable=protected-access
-  if flags_obj.use_synthetic_data:
-    synth_input_fn = resnet_run_loop.get_synth_input_fn(
-        cifar_main._HEIGHT, cifar_main._WIDTH,
-        cifar_main._NUM_CHANNELS, cifar_main._NUM_CLASSES,
-        dtype=flags_core.get_tf_dtype(flags_obj))
-    train_input_dataset = synth_input_fn(
-        True,
-        flags_obj.data_dir,
-        batch_size=per_device_batch_size,
-        height=cifar_main._HEIGHT,
-        width=cifar_main._WIDTH,
-        num_channels=cifar_main._NUM_CHANNELS,
-        num_classes=cifar_main._NUM_CLASSES,
-        dtype=dtype)
-    eval_input_dataset = synth_input_fn(
-        False,
-        flags_obj.data_dir,
-        batch_size=per_device_batch_size,
-        height=cifar_main._HEIGHT,
-        width=cifar_main._WIDTH,
-        num_channels=cifar_main._NUM_CHANNELS,
-        num_classes=cifar_main._NUM_CLASSES,
-        dtype=dtype)
-  # pylint: enable=protected-access
-
-  else:
-    train_input_dataset = cifar_main.input_fn(
-        True,
-        flags_obj.data_dir,
-        batch_size=per_device_batch_size,
-        num_epochs=flags_obj.train_epochs,
-        parse_record_fn=parse_record_keras)
-
-    eval_input_dataset = cifar_main.input_fn(
-        False,
-        flags_obj.data_dir,
-        batch_size=per_device_batch_size,
-        num_epochs=flags_obj.train_epochs,
-        parse_record_fn=parse_record_keras)
+  train_input_dataset, eval_input_dataset = get_data(
+      flags_obj.dataset, flags_obj.use_synthetic_data)
 
   # Use Keras ResNet50 applications model and native keras APIs
   # initialize RMSprop optimizer
@@ -253,15 +234,40 @@ def run_cifar_with_keras(flags_obj):
   opt = gradient_descent_v2.SGD(learning_rate=0.1, momentum=0.9)
 
   # TF Optimizer:
-  # opt = tf.train.MomentumOptimizer(learning_rate=0.1, momentum=0.9)
+  # learning_rate = BASE_LEARNING_RATE * flags_obj.batch_size / 256
+  # opt = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9)
 
   strategy = distribution_utils.get_distribution_strategy(
       num_gpus=flags_obj.num_gpus)
 
-  model = keras_resnet_model.ResNet56(input_shape=(32, 32, 3),
-                                      include_top=True,
-                                      classes=cifar_main._NUM_CLASSES,
-                                      weights=None)
+  if flags_obj.dataset == IMAGENET_DATASET:
+    model = resnet_model_tpu.ResNet50(num_classes=imagenet_main._NUM_CLASSES)
+    steps_per_epoch = imagenet_main._NUM_IMAGES['train'] // flags_obj.batch_size
+
+    lr_callback = LearningRateBatchScheduler(
+      learning_rate_schedule,
+      batch_size=flags_obj.batch_size,
+      num_images=imagenet_main._NUM_IMAGES['train'])
+
+    num_eval_steps = (imagenet_main._NUM_IMAGES['validation'] //
+                    flags_obj.batch_size)
+  elif flags_obj.dataset = CIFAR_DATASET:
+    model = keras_resnet_model.ResNet56(input_shape=(32, 32, 3),
+                                        include_top=True,
+                                        classes=cifar_main._NUM_CLASSES,
+                                        weights=None)
+
+    steps_per_epoch = cifar_main._NUM_IMAGES['train'] // flags_obj.batch_size
+
+    lr_callback = LearningRateBatchScheduler(
+        learning_rate_schedule,
+        batch_size=flags_obj.batch_size,
+        num_images=cifar_main._NUM_IMAGES['train'])
+
+    num_eval_steps = (cifar_main._NUM_IMAGES['validation'] //
+                      flags_obj.batch_size)
+  else:
+    raise ValueError("Unknown dataset: {%s}".format(flags_obj.dataset))
 
   loss = 'categorical_crossentropy'
   accuracy = 'categorical_accuracy'
@@ -277,21 +283,12 @@ def run_cifar_with_keras(flags_obj):
                   metrics=[accuracy],
                   distribute=strategy)
 
-  steps_per_epoch = cifar_main._NUM_IMAGES['train'] // flags_obj.batch_size
-
   time_callback = TimeHistory(flags_obj.batch_size)
 
   tesorboard_callback = tf.keras.callbacks.TensorBoard(
       log_dir=flags_obj.model_dir)
     #  update_freq="batch")  # Add this if want per batch logging.
 
-  lr_callback = LearningRateBatchScheduler(
-      learning_rate_schedule,
-      batch_size=flags_obj.batch_size,
-      num_images=cifar_main._NUM_IMAGES['train'])
-
-  num_eval_steps = (cifar_main._NUM_IMAGES['validation'] //
-                    flags_obj.batch_size)
 
   print('Executing eagerly?:', tf.executing_eagerly())
   history = model.fit(train_input_dataset,
@@ -310,6 +307,8 @@ def run_cifar_with_keras(flags_obj):
                                steps=num_eval_steps,
                                verbose=1)
 
+  print('Test loss:', eval_output[0])
+
   stats = {}
   stats['accuracy_top_1'] = eval_output[1]
   stats['eval_loss'] = eval_output[0]
@@ -318,20 +317,110 @@ def run_cifar_with_keras(flags_obj):
 
   print('top_1 accuracy:{}'.format(stats['accuracy_top_1']))
   print('top_1_training_accuracy:{}'.format(stats['training_accuracy_top_1']))
+
   return stats
 
+def get_data(dataset, use_synthetic_data):
 
-def define_keras_cifar_flags():
+  if dataset == IMAGENET_DATASET:
+    if use_synthetic_data:
+      synth_input_fn = resnet_run_loop.get_synth_input_fn(
+          imagenet_main._DEFAULT_IMAGE_SIZE, imagenet_main._DEFAULT_IMAGE_SIZE,
+          imagenet_main._NUM_CHANNELS, imagenet_main._NUM_CLASSES,
+          dtype=flags_core.get_tf_dtype(flags_obj))
+      train_input_dataset = synth_input_fn(
+          batch_size=per_device_batch_size,
+          height=imagenet_main._DEFAULT_IMAGE_SIZE,
+          width=imagenet_main._DEFAULT_IMAGE_SIZE,
+          num_channels=imagenet_main._NUM_CHANNELS,
+          num_classes=imagenet_main._NUM_CLASSES,
+          dtype=dtype)
+      eval_input_dataset = synth_input_fn(
+          batch_size=per_device_batch_size,
+          height=imagenet_main._DEFAULT_IMAGE_SIZE,
+          width=imagenet_main._DEFAULT_IMAGE_SIZE,
+          num_channels=imagenet_main._NUM_CHANNELS,
+          num_classes=imagenet_main._NUM_CLASSES,
+          dtype=dtype)
+    # pylint: enable=protected-access
+
+    else:
+      train_input_dataset = imagenet_main.input_fn(
+            True,
+            flags_obj.data_dir,
+            batch_size=per_device_batch_size,
+            num_epochs=flags_obj.train_epochs,
+            parse_record_fn=parse_record_keras)
+
+      eval_input_dataset = imagenet_main.input_fn(
+            False,
+            flags_obj.data_dir,
+            batch_size=per_device_batch_size,
+            num_epochs=flags_obj.train_epochs,
+            parse_record_fn=parse_record_keras)
+  elif dataset == CIFAR_DATASET:
+    if use_synthetic_data:
+      if flags_obj.use_synthetic_data:
+        synth_input_fn = resnet_run_loop.get_synth_input_fn(
+            cifar_main._HEIGHT, cifar_main._WIDTH,
+            cifar_main._NUM_CHANNELS, cifar_main._NUM_CLASSES,
+            dtype=flags_core.get_tf_dtype(flags_obj))
+        train_input_dataset = synth_input_fn(
+            True,
+            flags_obj.data_dir,
+            batch_size=per_device_batch_size,
+            height=cifar_main._HEIGHT,
+            width=cifar_main._WIDTH,
+            num_channels=cifar_main._NUM_CHANNELS,
+            num_classes=cifar_main._NUM_CLASSES,
+            dtype=dtype)
+        eval_input_dataset = synth_input_fn(
+            False,
+            flags_obj.data_dir,
+            batch_size=per_device_batch_size,
+            height=cifar_main._HEIGHT,
+            width=cifar_main._WIDTH,
+            num_channels=cifar_main._NUM_CHANNELS,
+            num_classes=cifar_main._NUM_CLASSES,
+            dtype=dtype)
+      # pylint: enable=protected-access
+
+      else:
+        train_input_dataset = cifar_main.input_fn(
+            True,
+            flags_obj.data_dir,
+            batch_size=per_device_batch_size,
+            num_epochs=flags_obj.train_epochs,
+            parse_record_fn=parse_record_keras)
+
+        eval_input_dataset = cifar_main.input_fn(
+            False,
+            flags_obj.data_dir,
+            batch_size=per_device_batch_size,
+            num_epochs=flags_obj.train_epochs,
+            parse_record_fn=parse_record_keras)
+
+  return train_input_dataset, eval_input_dataset
+
+
+def define_keras_flags():
   flags.DEFINE_boolean(name='enable_eager', default=False, help='Enable eager?')
+  flags.DEFINE_string(name='dataset', default=IMAGENET_DATASET,
+      help='Which dataset, ImageNet or Cifar?')
 
 
 def main(_):
   with logger.benchmark_context(flags.FLAGS):
-    run_cifar_with_keras(flags.FLAGS)
+    run_imagenet_with_keras(flags.FLAGS)
 
 
 if __name__ == '__main__':
-  tf.logging.set_verbosity(tf.logging.DEBUG)
-  define_keras_cifar_flags()
-  cifar_main.define_cifar_flags()
+  tf.logging.set_verbosity(tf.logging.INFO)
+  define_keras_flags()
+
+  if flags_obj.dataset == IMAGENET_DATASET:
+    imagenet_main.define_imagenet_flags()
+  elif flags_obj.dataset == CIFAR_DATASET:
+    cifar_main.define_cifar_flags()
+
   absl_app.run(main)
