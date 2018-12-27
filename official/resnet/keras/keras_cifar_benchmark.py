f762945a3973a2be2946add40c049ba27028788b
Shining Sun <shiningsun@google.com>
2018-12-27 23:26:00 +0000

    Fix benchmark imports and add license stmt

--- official/resnet/keras/keras_cifar_benchmark.py
+++ official/resnet/keras/keras_cifar_benchmark.py
@@ -1,3 +1,17 @@
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 """Executes Keras benchmarks and accuracy tests."""
 from __future__ import print_function
 
@@ -8,8 +22,9 @@ from absl.testing import flagsaver
 import tensorflow as tf  # pylint: disable=g-bad-import-order
 
 from official.resnet import cifar10_main as cifar_main
-import official.resnet.keras.keras_cifar_main as keras_cifar_main
-import official.resnet.keras.keras_common as keras_common
+from official.resnet.keras import keras_cifar_main
+from official.resnet.keras import keras_common
+
 
 DATA_DIR = '/data/cifar10_data/'
 
