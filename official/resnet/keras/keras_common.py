2fd16716336d4168d52f84ffb0fff7ee5ab2e730
Shining Sun <shiningsun@google.com>
2019-01-04 00:12:35 +0000

    fix lint errors

--- official/resnet/keras/keras_common.py
+++ official/resnet/keras/keras_common.py
@@ -216,12 +216,13 @@ def get_strategy_scope(strategy):
   if strategy:
     strategy_scope = strategy.scope()
   else:
-    strategy_scope = keras_common.DummyContextManager()
+    strategy_scope = DummyContextManager()
 
   return strategy_scope
 
 
 class DummyContextManager(object):
+
   def __enter__(self):
     pass
 
