c812f6f445a176ce56b873054f5b76db2ec14b44
morincl <clement-morin+github@protonmail.ch>
2019-02-05 17:56:54 +0100

    Fix invalid dict access
    Due to changes in research/object_detection/utils/config_util.py
    introduced by 99256cf.

--- research/object_detection/metrics/offline_eval_map_corloc.py
+++ research/object_detection/metrics/offline_eval_map_corloc.py
@@ -159,7 +159,7 @@ def main(argv):
       eval_config_path=FLAGS.eval_config_path)
 
   eval_config = configs['eval_config']
-  input_config = configs['eval_input_config']
+  input_config = configs['eval_input_configs'][0]
 
   metrics = read_data_and_evaluate(input_config, eval_config)
 
