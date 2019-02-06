66249d0f419c5e3b4ae96eea942f2c86a0e6a71b
Goldie Gadde <ggadde@google.com>
2019-02-06 11:19:23 -0800

    Revert "tf_upgrade_v2 on resnet and utils folders. (#6154)"
    This reverts commit d6b2b83c0eb668085ecf4045178d210e7bc69892.

--- official/resnet/keras/keras_common_test.py
+++ official/resnet/keras/keras_common_test.py
@@ -22,7 +22,7 @@ import tensorflow as tf  # pylint: disable=g-bad-import-order
 
 from official.resnet.keras import keras_common
 
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 
 class KerasCommonTests(tf.test.TestCase):
