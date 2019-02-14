b365e7075a32f044c9dfc27cdd2c23822ec12503
Xunkai Zhang <xunkai@google.com>
2019-02-14 11:56:45 +0000

    cleanup code

--- official/keras_application_models/v2/train_resnet50.py
+++ official/keras_application_models/v2/train_resnet50.py
@@ -1,4 +1,4 @@
-# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,111 +20,98 @@ from __future__ import print_function
 # pylint: disable=g-bad-import-order
 import os
 import numpy as np
+import absl.logging
 from absl import app as absl_app
 from absl import flags
 import tensorflow as tf
 # pylint: enable=g-bad-import-order
 
-from official.keras_application_models import dataset
-from official.utils.flags import core as flags_core
-from official.utils.logs import logger
-from official.utils.misc import distribution_utils
+from official.keras_application_models.v2 import dataset
+from official.keras_application_models.v2 import utils
 
 
-class FakeScope():
-  def __enter__(self):
-    pass
-  def __exit__(self, ext_type, ext_val, tb):
-    pass
-
-
-class NotReallyADistributionStrategy():
-
-  def scope(self):
-    return FakeScope()
-
-
-def train_resnet50(_):
-  """Train Resnet50 on CIFAR from the scratch."""
-  # Ensure a valid model name was supplied via command line argument
-
-  # Check if eager execution is enabled
-  if FLAGS.eager:
-    tf.logging.info("Eager execution is enabled...")
-    tf.enable_eager_execution()
-
-  strategy = None
-  if FLAGS.dist_strat:
-    strategy = distribution_utils.get_distribution_strategy(
-        num_gpus=FLAGS.num_gpus)
+def get_model(input_shape, classes, no_pretrained_weights):
+  if no_pretrained_weights:
+    return tf.keras.applications.ResNet50(
+        weights=None,
+        input_shape=input_shape,
+        include_top=True,
+        classes=classes)
   else:
-    strategy = NotReallyADistributionStrategy()
-
-  with strategy.scope():
-
-    tf.logging.info("Using CIFAR-10 dataset...")
-    dataset_name = "CIFAR-10"
-    ds = dataset.Cifar10Dataset(FLAGS.batch_size)
-    x_train, y_train = ds.x_train, ds.y_train
-    x_test, y_test = ds.x_test, ds.y_test
-    # Subtract pixel mean from data
-    x_train_mean = np.mean(x_train, axis=0)
-    x_train -= x_train_mean
-    x_test -= x_train_mean
-    # Apply data augmentation
-    datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-        # randomly shift images horizontally
-        width_shift_range=0.1,
-        # randomly shift images vertically
-        height_shift_range=0.1,
-        # set mode for filling points outside the input boundaries
-        fill_mode='constant',
-        # value used for fill_mode = "constant"
-        cval=0.,
-        # randomly flip images
-        horizontal_flip=True)
-
     base_model = tf.keras.applications.ResNet50(
         # Use imagenet pretrained weights require input_shape and pooling.
         weights='imagenet',
-        input_shape=ds.input_shape,
+        input_shape=input_shape,
         pooling='avg',
         # When include_top is False, we need manually add FC layers.
         include_top=False)
     # Manually add FC layer
     x = base_model.output
     x = tf.keras.layers.Dense(10, activation='softmax', name='fc10')(x)
-    model = tf.keras.Model(inputs=base_model.inputs, outputs=x)
+    return tf.keras.Model(inputs=base_model.inputs, outputs=x)
 
-    # Adam optimizer and some other optimizers doesn't work well with
-    # distribution strategy (b/113076709)
-    # Use keras.SGD (SGD + Momentum) according to ResNet paper.
+
+def get_cifar_model(input_shape=(32, 32, 3),
+                    classes=10,
+                    no_pretrained_weights=False,
+                    up_sampling=1):
+  if up_sampling == 1:
+    input_tensor = tf.keras.layers.Input(shape=input_shape)
+    x = input_tensor
+  else:
+    input_tensor = tf.keras.layers.Input(shape=input_shape)
+    x = tf.keras.layers.UpSampling2D(
+        size=up_sampling, interpolation="bilinear")(input_tensor)
+    input_shape = (
+        input_shape[0] * up_sampling,
+        input_shape[1] * up_sampling,
+        input_shape[2])
+  base_model = tf.keras.applications.ResNet50(
+      weights=(None if no_pretrained_weights else 'imagenet'),
+      input_shape=input_shape,
+      input_tensor=x,
+      include_top=False,
+      pooling='avg')
+  x = base_model.output
+  x = tf.keras.layers.Dense(classes, activation='softmax', name='fc10')(x)
+  return tf.keras.Model(inputs=input_tensor, outputs=x)
+
+
+def train_resnet50(_):
+  """Train Resnet50 on CIFAR from the scratch."""
+
+  # Enable/Disable eager based on flags. It's enabled by default.
+  utils.init_eager_execution(FLAGS.no_eager)
+
+  # Initialize distribution strategy.
+  # TODO(xunkai55): make it work when FLAGS.dist_strat == True.
+  # If FLAGS.dist_strat == False, a fake placeholder will be used.
+  strategy = utils.get_distribution_strategy(
+    FLAGS.num_gpus, no_distribution_strategy=not FLAGS.dist_strat)
+
+  with strategy.scope():
+    ds = dataset.Cifar10Dataset()
+    train_dataset, test_dataset = ds.to_dataset(FLAGS.batch_size)
+    model = get_cifar_model(input_shape=ds.input_shape,
+                            classes=ds.num_classes,
+                            no_pretrained_weights=FLAGS.no_pretrained_weights,
+                            up_sampling=4)
     optimizer = tf.keras.optimizers.SGD(
         learning_rate=tf.keras.backend.variable(1e-3), momentum=0.9)
-    lr_scheduler = tf.keras.callbacks.LearningRateScheduler(
-        lambda x: 1e-3 if x <= 10 else 1e-4 if x <= 30 else 1e-5,
-        verbose=1)
+    if FLAGS.no_pretrained_weights:
+      lr_scheduler = tf.keras.callbacks.LearningRateScheduler(
+          lambda x: 1e-2 if x < 50 else 1e-3 if x < 70 else 1e-4,
+          verbose=1)
+    else:
+      lr_scheduler = tf.keras.callbacks.LearningRateScheduler(
+          lambda x: 1e-3 if x < 10 else 1e-4 if x < 30 else 1e-5,
+          verbose=1)
 
     # Add L1L2 regularization to avoid overfitting
-    l1 = 0.
-    l2 = 0.0001
-    for layer in model.layers:
-      if hasattr(layer, "kernel_regularizer"):
-        layer.kerner_regularizer = tf.keras.regularizers.l1_l2(l1, l2)
+    utils.add_global_regularization(model, l2=0.0001)
 
-    # Prepare model model saving directory.
-    save_dir = os.path.join(os.getcwd(), 'saved_models')
-    model_name = 'cifar10_resnet50_model.{epoch:03d}.h5'
-    if not os.path.isdir(save_dir):
-      os.makedirs(save_dir)
-    filepath = os.path.join(save_dir, model_name)
-
-    # Prepare callbacks for model saving and for learning rate adjustment.
-    checkpoint = tf.keras.callbacks.ModelCheckpoint(
-        filepath=filepath, monitor='val_acc', verbose=1, save_best_only=True)
-
+    checkpoint = utils.prepare_model_saving("resnet50")
     callbacks = [lr_scheduler, checkpoint]
-    datagen.fit(x_train)
 
     model.compile(loss="categorical_crossentropy",
                   optimizer=optimizer,
@@ -132,72 +119,24 @@ def train_resnet50(_):
 
     # Train and evaluate the model
     history = model.fit(
-        datagen.flow(x_train, y_train, batch_size=FLAGS.batch_size),
+        train_dataset,
         epochs=FLAGS.train_epochs,
         callbacks=callbacks,
-        validation_data=(x_test, y_test),
-        steps_per_epoch=int(np.ceil(FLAGS.num_train_images / FLAGS.batch_size)),
+        validation_data=test_dataset,
+        steps_per_epoch=int(np.ceil(ds.num_train_examples / FLAGS.batch_size)),
+        validation_steps=int(np.ceil(ds.num_test_examples / FLAGS.batch_size))
     )
 
   # Clear the session explicitly to avoid session delete error
   tf.keras.backend.clear_session()
 
 
-def define_keras_benchmark_flags():
-  """Add flags for keras built-in application models."""
-  flags_core.define_base(hooks=False)
-  flags_core.define_performance()
-  flags_core.define_image()
-  flags_core.define_benchmark()
-  flags.adopt_module_key_flags(flags_core)
-
-  flags_core.set_defaults(
-      data_format="channels_last",
-      batch_size=32,
-      train_epochs=2)
-
-  flags.DEFINE_integer(
-      name="num_train_images", default=50000,
-      help=flags_core.help_wrap(
-          "The number of images for training. The default value is 50000."))
-
-  flags.DEFINE_integer(
-      name="num_eval_images", default=10000,
-      help=flags_core.help_wrap(
-          "The number of images for evaluation. The default value is 10000."))
-
-  flags.DEFINE_boolean(
-      name="eager", default=False, help=flags_core.help_wrap(
-          "To enable eager execution. Note that if eager execution is enabled, "
-          "only one GPU is utilized even if multiple GPUs are provided and "
-          "multi_gpu_model is used."))
-
-  flags.DEFINE_boolean(
-      name="dist_strat", default=False, help=flags_core.help_wrap(
-          "To enable distribution strategy for model training and evaluation. "
-          "Number of GPUs used for distribution strategy can be set by the "
-          "argument --num_gpus."))
-
-  flags.DEFINE_list(
-      name="callbacks",
-      default=[],
-      help=flags_core.help_wrap(
-          "A list of (case insensitive) strings to specify the names of "
-          "callbacks. For example: `--callbacks ExamplesPerSecondCallback,"
-          "LoggingMetricCallback`"))
-
-  # pylint: disable=unused-variable
-  def _check_eager_dist_strat(flag_dict):
-    return not(flag_dict["eager"] and flag_dict["dist_strat"])
-
-
 def main(_):
-  with logger.benchmark_context(FLAGS):
-    train_resnet50(FLAGS)
+  train_resnet50(FLAGS)
 
 
 if __name__ == "__main__":
-  tf.logging.set_verbosity(tf.logging.INFO)
-  define_keras_benchmark_flags()
+  absl.logging.set_verbosity(absl.logging.INFO)
+  utils.define_flags()
   FLAGS = flags.FLAGS
   absl_app.run(main)
