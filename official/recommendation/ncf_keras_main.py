f2600a9b79cc44b1bdca149da8893b0a839651e2
Shining Sun <shiningsun@google.com>
2019-02-15 22:14:51 +0000

    converged

--- official/recommendation/ncf_keras_main.py
+++ official/recommendation/ncf_keras_main.py
@@ -22,92 +22,31 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import contextlib
-import heapq
-import json
-import logging
-import math
-import multiprocessing
 import os
-import signal
-import typing
 
 # pylint: disable=g-bad-import-order
-import numpy as np
 from absl import app as absl_app
 from absl import flags
 import tensorflow as tf
 # pylint: enable=g-bad-import-order
 
-from tensorflow.contrib.compiler import xla
 from official.datasets import movielens
 from official.recommendation import constants as rconst
-from official.recommendation import data_pipeline
-from official.recommendation import data_preprocessing
 from official.recommendation import ncf_common
 from official.recommendation import neumf_model
-from official.utils.flags import core as flags_core
-from official.utils.logs import hooks_helper
 from official.utils.logs import logger
 from official.utils.logs import mlperf_helper
-from official.utils.misc import distribution_utils
 from official.utils.misc import model_helpers
 
 
 FLAGS = flags.FLAGS
 
 
-def main(_):
-  with logger.benchmark_context(FLAGS), \
-      mlperf_helper.LOGGER(FLAGS.output_ml_perf_compliance_logging):
-    mlperf_helper.set_ncf_root(os.path.split(os.path.abspath(__file__))[0])
-    if FLAGS.tpu:
-      raise ValueError("NCF in Keras does not support TPU for now")
-    run_ncf(FLAGS)
-
-
-def run_ncf(_):
-  """Run NCF training and eval with Keras."""
-  params = ncf_common.parse_flags(FLAGS)
-
-  distribution = ncf_common.get_distribution_strategy(params)
-
-  num_users, num_items, num_train_steps, num_eval_steps, producer = (
-      ncf_common.get_inputs(params))
-
-  params["num_users"], params["num_items"] = num_users, num_items
-  producer.start()
-  model_helpers.apply_clean(flags.FLAGS)
-
-  # TODO(shiningsun): Both MirroredStrategy and OneDeviceStrategy error out.
-  # Find out why and change the distribute to distribution
-  with distribution_utils.MaybeDistributionScope(None):
-    keras_model = _get_keras_model(params)
-
-    optimizer = ncf_common.get_optimizer(params)
-    keras_model.compile(
-        loss="sparse_categorical_crossentropy",
-        metrics=[_get_metric_fn(params)],
-        optimizer=optimizer)
-
-    train_input_dataset, eval_input_dataset = _get_train_and_eval_data(producer, params)
-
-    keras_model.fit(train_input_dataset,
-        epochs=FLAGS.train_epochs,
-        steps_per_epoch=num_train_steps,
-        callbacks=[IncrementEpochCallback(producer)],
-        verbose=2)
-
-    tf.logging.info("Training done. Start evaluating")
-
-    eval_results = keras_model.evaluate(
-        eval_input_dataset,
-        steps=num_eval_steps,
-        verbose=2)
-
-    tf.logging.info("Keras evaluation is done.")
-
-  return eval_results
+def _keras_loss(y_true, y_pred):
+  loss = tf.losses.sparse_softmax_cross_entropy(
+      labels=tf.cast(y_true, tf.int32),
+      logits=y_pred)
+  return loss
 
 
 def _get_metric_fn(params):
@@ -128,34 +67,6 @@ def _get_metric_fn(params):
         params["use_xla_for_gpu"])
 
   return metric_fn
-
-
-def _get_keras_model(params):
-  batch_size = params['batch_size']
-
-  user_input = tf.keras.layers.Input(
-      shape=(), batch_size=batch_size, name=movielens.USER_COLUMN, dtype=tf.int32)
-  item_input = tf.keras.layers.Input(
-      shape=(), batch_size=batch_size, name=movielens.ITEM_COLUMN, dtype=tf.int32)
-
-  base_model = neumf_model.construct_model(user_input, item_input, params)
-  base_model_output = base_model.output
-
-  zeros = tf.keras.layers.Lambda(
-      lambda x: x * 0)(base_model_output)
-
-  softmax_logits = tf.keras.layers.concatenate(
-      [zeros, base_model_output],
-      axis=-1)
-
-  keras_model = tf.keras.Model(
-      inputs=[user_input, item_input],
-      outputs=softmax_logits)
-
-  keras_model.summary()
-
-
-  return keras_model
 
 
 def _get_hit_rate_metric(
@@ -183,32 +94,21 @@ def _get_hit_rate_metric(
   return in_top_k
 
 
-
 def _get_train_and_eval_data(producer, params):
 
-  def preprocess_training_input(features, labels):
-    # Add a dummy dup_mask to the input dataset, because it is part of the
-    # model's input layres, which is because it is part of the eval input
-    # data
-    weights = features.pop(rconst.VALID_POINT_MASK)
-    return features, labels, weights
-
-
   train_input_fn = producer.make_input_fn(is_training=True)
-  train_input_dataset = train_input_fn(params).map(
-      lambda features, labels: preprocess_training_input(features, labels))
-  train_input_dataset = train_input_dataset.repeat(FLAGS.train_epochs)
+  train_input_dataset = train_input_fn(params)
 
   def preprocess_eval_input(features):
     labels = tf.zeros_like(features[movielens.USER_COLUMN])
     return features, labels
 
   eval_input_fn = producer.make_input_fn(is_training=False)
-  eval_input_dataset = eval_input_fn(params)
-  eval_input_dataset = eval_input_dataset.map(
+  eval_input_dataset = eval_input_fn(params).map(
       lambda features : preprocess_eval_input(features))
 
   return train_input_dataset, eval_input_dataset
+
 
 class IncrementEpochCallback(tf.keras.callbacks.Callback):
   """A callback to increase the requested epoch for the data producer.
@@ -223,6 +123,80 @@ class IncrementEpochCallback(tf.keras.callbacks.Callback):
 
   def on_epoch_begin(self, epoch, logs=None):
     self._producer.increment_request_epoch()
+
+
+def _get_keras_model(params):
+  batch_size = params['batch_size']
+
+  user_input = tf.keras.layers.Input(
+      shape=(), batch_size=batch_size, name=movielens.USER_COLUMN, dtype=tf.int32)
+  item_input = tf.keras.layers.Input(
+      shape=(), batch_size=batch_size, name=movielens.ITEM_COLUMN, dtype=tf.int32)
+
+  base_model = neumf_model.construct_model(user_input, item_input, params)
+  base_model_output = base_model.output
+
+  zeros = tf.keras.layers.Lambda(
+      lambda x: x * 0)(base_model_output)
+
+  softmax_logits = tf.keras.layers.concatenate(
+      [zeros, base_model_output],
+      axis=-1)
+
+  keras_model = tf.keras.Model(
+      inputs=[user_input, item_input],
+      outputs=softmax_logits)
+
+  keras_model.summary()
+  return keras_model
+
+
+def run_ncf(_):
+  """Run NCF training and eval with Keras."""
+  params = ncf_common.parse_flags(FLAGS)
+
+  num_users, num_items, num_train_steps, num_eval_steps, producer = (
+      ncf_common.get_inputs(params))
+
+  params["num_users"], params["num_items"] = num_users, num_items
+  producer.start()
+  model_helpers.apply_clean(flags.FLAGS)
+
+  keras_model = _get_keras_model(params)
+  optimizer = ncf_common.get_optimizer(params)
+
+  keras_model.compile(
+      loss=_keras_loss,
+      metrics=[_get_metric_fn(params)],
+      optimizer=optimizer)
+
+  train_input_dataset, eval_input_dataset = _get_train_and_eval_data(producer, params)
+  keras_model.fit(train_input_dataset,
+      epochs=FLAGS.train_epochs,
+      callbacks=[IncrementEpochCallback(producer)],
+      verbose=2)
+
+  tf.logging.info("Training done. Start evaluating")
+
+  eval_results = keras_model.evaluate(
+      eval_input_dataset,
+      steps=num_eval_steps,
+      verbose=2)
+
+  tf.logging.info("Keras evaluation is done.")
+  return eval_results
+
+
+def main(_):
+  with logger.benchmark_context(FLAGS), \
+      mlperf_helper.LOGGER(FLAGS.output_ml_perf_compliance_logging):
+    mlperf_helper.set_ncf_root(os.path.split(os.path.abspath(__file__))[0])
+    if FLAGS.tpu:
+      raise ValueError("NCF in Keras does not support TPU for now")
+    if FLAGS.num_gpus > 1:
+      raise ValueError("NCF in Keras does not support distribution strategies. "
+          "Please set num_gpus to 1")
+    run_ncf(FLAGS)
 
 
 if __name__ == "__main__":
