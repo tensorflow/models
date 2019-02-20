7aa7df3a3c87d466a77bd36813a1654696828cf2
Xunkai Zhang <xunkai@google.com>
2019-02-20 07:00:51 +0000

    minor changes

--- official/keras_application_models/v2/train_resnet50_on_cifar10.py
+++ official/keras_application_models/v2/train_resnet50_on_cifar10.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 """Retrain the keras built-in ResNet50 on CIFAR-10."""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
