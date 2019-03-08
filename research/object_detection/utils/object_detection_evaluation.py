9f53fadce33c6280644aea2b1b695908c74d5db6
Dragos Bobolea <boboleadragos@gmail.com>
2019-03-08 13:55:48 +0200

    fix eval.py for python3

--- research/object_detection/utils/object_detection_evaluation.py
+++ research/object_detection/utils/object_detection_evaluation.py
@@ -210,7 +210,7 @@ class ObjectDetectionEvaluator(DetectionEvaluator):
       if idx + self._label_id_offset in category_index:
         category_name = category_index[idx + self._label_id_offset]['name']
         try:
-          category_name = unicode(category_name, 'utf-8')
+          category_name = str(category_name, 'utf-8')
         except TypeError:
           pass
         category_name = unicodedata.normalize('NFKD', category_name).encode(
@@ -348,7 +348,7 @@ class ObjectDetectionEvaluator(DetectionEvaluator):
       if idx + self._label_id_offset in category_index:
         category_name = category_index[idx + self._label_id_offset]['name']
         try:
-          category_name = unicode(category_name, 'utf-8')
+          category_name = str(category_name, 'utf-8')
         except TypeError:
           pass
         category_name = unicodedata.normalize(
