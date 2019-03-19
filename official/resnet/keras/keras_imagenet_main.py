daa327d34b9d775bb62b76ff85b2f46ff62ac90b
Soroush Radpour <ssh.rdp@gmail.com>
2019-03-19 13:51:13 -0700

    Add the option to run Keras resnet model on multiple workers. (#6368)

--- official/resnet/keras/keras_imagenet_main.py
+++ official/resnet/keras/keras_imagenet_main.py
@@ -144,7 +144,8 @@ def run(flags_obj):
 
   strategy = distribution_utils.get_distribution_strategy(
       distribution_strategy=flags_obj.distribution_strategy,
-      num_gpus=flags_obj.num_gpus)
+      num_gpus=flags_obj.num_gpus,
+      num_workers=distribution_utils.configure_cluster())
 
   strategy_scope = keras_common.get_strategy_scope(strategy)
 
