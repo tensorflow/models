94d17aea1a92d0a222fd10f8a192beedc97c54cc
Yuefeng Zhou <yuefengz@google.com>
2019-03-28 16:29:00 -0700

    Scale up learning rate according to num workers in Estimator imagenet models. (#6472)
    * Move distribution strategy creation before creating any ops, which is
    required by multi-node collective ops in eager mode.
    
    * Scale up learning rate according to num workers in ResNet50 w/
    Estimator.
    
    * Scale up LR in cifar.
    
    * Fix a typo.
    
    * Add num_workers to run param as well. Make num_worker optional in
    params.

--- official/resnet/cifar10_main.py
+++ official/resnet/cifar10_main.py
@@ -212,9 +212,9 @@ def cifar10_model_fn(features, labels, mode, params):
   features = tf.reshape(features, [-1, HEIGHT, WIDTH, NUM_CHANNELS])
   # Learning rate schedule follows arXiv:1512.03385 for ResNet-56 and under.
   learning_rate_fn = resnet_run_loop.learning_rate_with_decay(
-      batch_size=params['batch_size'], batch_denom=128,
-      num_images=NUM_IMAGES['train'], boundary_epochs=[91, 136, 182],
-      decay_rates=[1, 0.1, 0.01, 0.001])
+      batch_size=params['batch_size'] * params.get('num_workers', 1),
+      batch_denom=128, num_images=NUM_IMAGES['train'],
+      boundary_epochs=[91, 136, 182], decay_rates=[1, 0.1, 0.01, 0.001])
 
   # Weight decay of 2e-4 diverges from 1e-4 decay used in the ResNet paper
   # and seems more stable in testing. The difference was nominal for ResNet-56.
