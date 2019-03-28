1166770e5fae46681b35da2fe8f5c254d52ea3be
Shining Sun <shiningsun@google.com>
2019-03-28 20:05:10 +0000

    remove debug prints

--- official/recommendation/ncf_keras_benchmark.py
+++ official/recommendation/ncf_keras_benchmark.py
@@ -56,13 +56,6 @@ class KerasNCFBenchmarkBase(tf.test.Benchmark):
       # Loads flags to get defaults to then override. List cannot be empty.
       flags.FLAGS(['foo'])
       core.set_defaults(**self.default_flags)
-      print(">>>>>>>>> zhenzheng default_flags: ", self.default_flags)
-      print(">>>>>>>>> zhenzheng flags.FLAGS: ", flags.FLAGS)
-      '''
-      # Overrides flag values with defaults for the class of tests.
-      for k, v in self.default_flags.items():
-        setattr(FLAGS, k, v)
-      '''
       saved_flag_values = flagsaver.save_flag_values()
       KerasNCFBenchmarkBase.local_flags = saved_flag_values
     else:
