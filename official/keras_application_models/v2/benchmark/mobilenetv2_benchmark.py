08f732a05f1938a61b87f3f51633db942d3ffb4d
Xunkai Zhang <xunkai@google.com>
2019-04-03 13:35:08 +0000

    fix report format

--- official/keras_application_models/v2/benchmark/mobilenetv2_benchmark.py
+++ official/keras_application_models/v2/benchmark/mobilenetv2_benchmark.py
@@ -31,12 +31,16 @@ from official.keras_application_models.v2 import utils
 FLAGS = flags.FLAGS
 
 
-class MobileNetV2Benchmark(tf.test.Benchmark):
+class MobileNetV2BenchmarkBase(tf.test.Benchmark):
   """Benchmarks tf.keras.application.MobileNetV2."""
 
   local_flags = None
 
-  def __init__(self, output_dir=None, root_data_dir=None, **kwargs):
+  def __init__(self,
+               output_dir=None,
+               root_data_dir=None,
+               dataset_name=None,
+               **kwargs):
     if output_dir is None:
       self._output_dir = FLAGS.benchmark_output_dir
     else:
@@ -45,21 +49,21 @@ class MobileNetV2Benchmark(tf.test.Benchmark):
     if root_data_dir is None:
       self._data_dir = FLAGS.benchmark_data_dir
     else:
-      self._data_dir = os.path.join(root_data_dir, "imagenet")
+      self._data_dir = os.path.join(root_data_dir, dataset_name)
 
   def _prepare_dataset_builder(self, data_spec):
     if data_spec is None:
       return benchmark_datasets.ImageNetDatasetBuilder(self._data_dir)
 
   def _setup(self):
-    if MobileNetV2Benchmark.local_flags is None:
+    if MobileNetV2BenchmarkBase.local_flags is None:
       utils.define_flags()
       # Loads flags to get defaults.
       flags.FLAGS(["foo"])
       saved_flag_values = flagsaver.save_flag_values()
-      MobileNetV2Benchmark.local_flags = saved_flag_values
+      MobileNetV2BenchmarkBase.local_flags = saved_flag_values
     else:
-      flagsaver.restore_flag_values(MobileNetV2Benchmark.local_flags)
+      flagsaver.restore_flag_values(MobileNetV2BenchmarkBase.local_flags)
 
   def _run_and_report(self, data_spec=None):
     start_time_sec = time.time()
@@ -79,10 +83,16 @@ class MobileNetV2Benchmark(tf.test.Benchmark):
   def _json_description(self, value):
     return json.dumps({"value": value})
 
+
+class MobileNetV2ImagenetSanityTest(MobileNetV2BenchmarkBase):
+
+  def __init__(self, output_dir=None, root_data_dir=None, **kwargs):
+    super().__init__(output_dir, root_data_dir, "imagenet")
+
   def benchmark_no_dist_strat_sanity(self):
     self._setup()
     FLAGS.no_pretrained_weights=True
-    FLAGS.batch_size=32
+    FLAGS.batch_size=96
     FLAGS.train_epochs=2
     FLAGS.limit_train_num=960
     self._run_and_report()
@@ -90,7 +100,7 @@ class MobileNetV2Benchmark(tf.test.Benchmark):
   def benchmark_1_gpu_sanity(self):
     self._setup()
     FLAGS.no_pretrained_weights=True
-    FLAGS.batch_size=32
+    FLAGS.batch_size=96
     FLAGS.train_epochs=2
     FLAGS.limit_train_num=960
     FLAGS.dist_strat=True
@@ -100,7 +110,31 @@ class MobileNetV2Benchmark(tf.test.Benchmark):
   def benchmark_2_gpus_sanity(self):
     self._setup()
     FLAGS.no_pretrained_weights=True
-    FLAGS.batch_size=32
+    FLAGS.batch_size=96
+    FLAGS.train_epochs=2
+    FLAGS.limit_train_num=960
+    FLAGS.dist_strat=True
+    FLAGS.num_gpus=2
+    self._run_and_report()
+
+
+class MobileNetV2FinetuneBenchmark(MobileNetV2BenchmarkBase):
+
+  def __init__(self, output_dir=None, root_data_dir=None, **kwargs):
+    super().__init__(output_dir, root_data_dir, "cifar-10-batches-bin")
+
+  def benchmark_1_gpu_finetune(self):
+    self._setup()
+    FLAGS.batch_size=96
+    FLAGS.train_epochs=2
+    FLAGS.limit_train_num=960
+    FLAGS.dist_strat=True
+    FLAGS.num_gpus=1
+    self._run_and_report()
+
+  def benchmark_2_gpus_finetune(self):
+    self._setup()
+    FLAGS.batch_size=96
     FLAGS.train_epochs=2
     FLAGS.limit_train_num=960
     FLAGS.dist_strat=True
