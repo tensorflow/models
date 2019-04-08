8ffb731f3f305c4ea06efba79b4d0cc41c079ed6
Shining Sun <shiningsun@google.com>
2019-04-08 18:25:39 +0000

    Added main function to the two keras resent benchmarks

--- official/resnet/keras/keras_imagenet_benchmark.py
+++ official/resnet/keras/keras_imagenet_benchmark.py
@@ -666,3 +666,8 @@ class TrivialKerasBenchmarkReal(keras_benchmark.KerasBenchmark):
         stats,
         total_batch_size=FLAGS.batch_size,
         log_steps=FLAGS.log_steps)
+
+
+if __name__ == '__main__':
+  assert tf.__version__.startswith('2')
+  tf.test.main()
