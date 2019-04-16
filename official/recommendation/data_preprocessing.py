b5b680667406f1f4213b7180299a33fcdac72ad4
Shining Sun <shiningsun@google.com>
2019-04-16 22:51:53 +0000

    Fixing an import bug

--- official/recommendation/data_preprocessing.py
+++ official/recommendation/data_preprocessing.py
@@ -28,7 +28,7 @@ import typing
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import absl as logging
+from absl import logging
 # pylint: enable=wrong-import-order
 
 from official.datasets import movielens
