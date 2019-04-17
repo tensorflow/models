2ae6d37adb42767fc1633372d5f76891d34f9a01
Shining Sun <seemuch@users.noreply.github.com>
2019-04-17 16:11:23 -0700

    Added unit tests keras cifar and imagenet (#6535)
    * before moving test cases to the base class
    
    * Added tests for keras cifar and keras imagenet
    
    * fix cifar10_test
    
    * add blank lines
    
    * fix lint errors
    
    * fix lint
    
    * Resolve comments
    
    * Modified two resnet keras tests
    
    * Tests passed
    
    * Remove keras_test_base
    
    * Remove gpu from the no-dist tests

--- official/resnet/keras/keras_imagenet_benchmark.py
+++ official/resnet/keras/keras_imagenet_benchmark.py
@@ -368,7 +368,7 @@ class Resnet50KerasBenchmarkBase(keras_benchmark.KerasBenchmark):
     FLAGS.dtype = 'fp16'
     FLAGS.enable_eager = True
     FLAGS.distribution_strategy = 'default'
-    FLAGS.model_dir = self._get_model_dir('benchmark_8_gpu_fp16')
+    FLAGS.model_dir = self._get_model_dir('benchmark_8_gpu_fp16_tweaked')
     FLAGS.batch_size = 256 * 8  # 8 GPUs
     FLAGS.tf_gpu_thread_mode = 'gpu_private'
     self._run_and_report_benchmark()
