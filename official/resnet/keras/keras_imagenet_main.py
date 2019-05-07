57c7e67fda99f733439ee1f479e1323fc1486999
Haoyu Zhang <haoyuzhang@google.com>
2019-05-07 10:48:26 -0700

    Use flags to define collective ops when initializing MirroredStrategy

--- official/resnet/keras/keras_imagenet_main.py
+++ official/resnet/keras/keras_imagenet_main.py
@@ -125,7 +125,9 @@ def run(flags_obj):
   strategy = distribution_utils.get_distribution_strategy(
       distribution_strategy=flags_obj.distribution_strategy,
       num_gpus=flags_obj.num_gpus,
-      num_workers=distribution_utils.configure_cluster())
+      num_workers=distribution_utils.configure_cluster(),
+      all_reduce_alg=flags_obj.all_reduce_alg,
+      num_packs=flags_obj.num_packs)
 
   strategy_scope = distribution_utils.get_strategy_scope(strategy)
 
