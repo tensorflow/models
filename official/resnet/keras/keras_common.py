52ba4c951752c26e070c23dce3de0e07b19fe410
Igor Saprykin <isaprykin@google.com>
2019-05-14 18:58:37 -0700

    Fix the PiecewiseConstantDecayWithWarmup.

--- official/resnet/keras/keras_common.py
+++ official/resnet/keras/keras_common.py
@@ -101,7 +101,7 @@ class PiecewiseConstantDecayWithWarmup(
     self.compute_lr_on_cpu = compute_lr_on_cpu
     self.name = name
 
-    self.cached_learning_rate_op = None
+    self.cached_learning_rate_op_cache = {}
 
   def __call__(self, step):
     if tf.executing_eagerly():
@@ -110,13 +110,16 @@ class PiecewiseConstantDecayWithWarmup(
     # In an eager function or graph, the current implementation of optimizer
     # repeatedly call and thus create ops for the learning rate schedule. To
     # avoid this, we cache the ops if not executing eagerly.
-    if self.cached_learning_rate_op is None:
+    graph = tf.compat.v1.get_default_graph()
+    if graph not in self.cached_learning_rate_op_cache:
       if self.compute_lr_on_cpu:
         with tf.device('/device:CPU:0'):
-          self.cached_learning_rate_op = self._get_learning_rate(step)
+          self.cached_learning_rate_op_cache[graph] = self._get_learning_rate(
+              step)
       else:
-        self.cached_learning_rate_op = self._get_learning_rate(step)
-    return self.cached_learning_rate_op
+        self.cached_learning_rate_op_cache[graph] = self._get_learning_rate(
+            step)
+    return self.cached_learning_rate_op_cache[graph]
 
   def _get_learning_rate(self, step):
     """Compute learning rate at given step."""
