ae58841a3da6ab7d6250bc050cf0b66df58f0c5e
Xiao Ma <xima@google.com>
2019-05-15 21:47:37 -0700

    Fix estimator tests for Cifar10 in Keras.

--- official/resnet/estimator_benchmark.py
+++ official/resnet/estimator_benchmark.py
@@ -382,9 +382,7 @@ class Resnet56EstimatorAccuracy(EstimatorBenchmark):
     flags.FLAGS.batch_size = 128
     flags.FLAGS.train_epochs = 182
     flags.FLAGS.model_dir = self._get_model_dir('benchmark_graph_1_gpu')
-    flags.FLAGS.resnet_size = 56
     flags.FLAGS.dtype = 'fp32'
-    flags.FLAGS.hooks = ['ExamplesPerSecondHook']
     self._run_and_report_benchmark_keras()
 
   def benchmark_graph_fp16_1_gpu(self):
