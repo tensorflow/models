f7bd9654c74f649558e155186926b3303b68aed9
Igor Saprykin <isaprykin@google.com>
2019-05-21 16:59:26 -0700

    Add the transformer model after the oss conversion.

--- official/transformer/model/model_utils.py
+++ official/transformer/model/model_utils.py
@@ -42,13 +42,13 @@ def get_position_encoding(
   Returns:
     Tensor with shape [length, hidden_size]
   """
-  position = tf.to_float(tf.range(length))
+  position = tf.cast(tf.range(length), tf.float32)
   num_timescales = hidden_size // 2
   log_timescale_increment = (
       math.log(float(max_timescale) / float(min_timescale)) /
-      (tf.to_float(num_timescales) - 1))
+      (tf.cast(num_timescales, tf.float32) - 1))
   inv_timescales = min_timescale * tf.exp(
-      tf.to_float(tf.range(num_timescales)) * -log_timescale_increment)
+      tf.cast(tf.range(num_timescales), tf.float32) * -log_timescale_increment)
   scaled_time = tf.expand_dims(position, 1) * tf.expand_dims(inv_timescales, 0)
   signal = tf.concat([tf.sin(scaled_time), tf.cos(scaled_time)], axis=1)
   return signal
@@ -68,7 +68,7 @@ def get_decoder_self_attention_bias(length):
     float tensor of shape [1, 1, length, length]
   """
   with tf.name_scope("decoder_self_attention_bias"):
-    valid_locs = tf.matrix_band_part(tf.ones([length, length]), -1, 0)
+    valid_locs = tf.linalg.band_part(tf.ones([length, length]), -1, 0)
     valid_locs = tf.reshape(valid_locs, [1, 1, length, length])
     decoder_bias = _NEG_INF * (1.0 - valid_locs)
   return decoder_bias
@@ -86,7 +86,7 @@ def get_padding(x, padding_value=0):
       0 -> non-padding, 1 -> padding
   """
   with tf.name_scope("padding"):
-    return tf.to_float(tf.equal(x, padding_value))
+    return tf.cast(tf.equal(x, padding_value), tf.float32)
 
 
 def get_padding_bias(x):
---
+++ official/transformer/v2/transformer_main.py
@@ -0,0 +1,241 @@
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Train and evaluate the Transformer model.
+
+See README for description of setting the training schedule and evaluating the
+BLEU score.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import datetime
+import os
+import tempfile
+
+# pylint: disable=g-bad-import-order
+from absl import flags
+import tensorflow as tf
+# pylint: enable=g-bad-import-order
+
+from official.transformer import compute_bleu
+from official.transformer.utils import tokenizer
+from official.transformer.v2 import data_pipeline
+from official.transformer.v2 import misc
+from official.transformer.v2 import optimizer
+from official.transformer.v2 import transformer
+from official.transformer.v2 import translate
+from official.utils.flags import core as flags_core
+from official.utils.logs import logger
+
+
+INF = int(1e9)
+BLEU_DIR = "bleu"
+_SINGLE_SAMPLE = 1
+
+
+def translate_and_compute_bleu(model, subtokenizer, bleu_source, bleu_ref):
+  """Translate file and report the cased and uncased bleu scores."""
+  # Create temporary file to store translation.
+  tmp = tempfile.NamedTemporaryFile(delete=False)
+  tmp_filename = tmp.name
+
+  translate.translate_file(
+      model,
+      subtokenizer,
+      bleu_source,
+      output_file=tmp_filename,
+      print_all_translations=False)
+
+  # Compute uncased and cased bleu scores.
+  uncased_score = compute_bleu.bleu_wrapper(bleu_ref, tmp_filename, False)
+  cased_score = compute_bleu.bleu_wrapper(bleu_ref, tmp_filename, True)
+  os.remove(tmp_filename)
+  return uncased_score, cased_score
+
+
+def evaluate_and_log_bleu(model, bleu_source, bleu_ref, vocab_file):
+  """Calculate and record the BLEU score."""
+  subtokenizer = tokenizer.Subtokenizer(vocab_file)
+
+  uncased_score, cased_score = translate_and_compute_bleu(
+      model, subtokenizer, bleu_source, bleu_ref)
+
+  tf.compat.v1.logging.info("Bleu score (uncased): %s", uncased_score)
+  tf.compat.v1.logging.info("Bleu score (cased): %s", cased_score)
+  return uncased_score, cased_score
+
+
+class TransformerTask(object):
+  """Main entry of Transformer model."""
+
+  def __init__(self, flags_obj):
+    """Init function of TransformerMain.
+
+    Args:
+      flags_obj: Object containing parsed flag values, i.e., FLAGS.
+    """
+    self.flags_obj = flags_obj
+
+    # Add flag-defined parameters to params object
+    num_gpus = flags_core.get_num_gpus(flags_obj)
+    self.params = params = misc.get_model_params(flags_obj.param_set, num_gpus)
+
+    params["data_dir"] = flags_obj.data_dir
+    params["model_dir"] = flags_obj.model_dir
+    params["num_parallel_calls"] = (
+        flags_obj.num_parallel_calls or tf.data.experimental.AUTOTUNE)
+
+    params["use_synthetic_data"] = flags_obj.use_synthetic_data
+    params["batch_size"] = flags_obj.batch_size or params["default_batch_size"]
+    params["repeat_dataset"] = None
+
+  def train(self):
+    """Trains the model."""
+    params, flags_obj, is_train = self.params, self.flags_obj, True
+    model = transformer.create_model(params, is_train)
+    opt = self._create_optimizer()
+
+    model.compile(opt, target_tensors=[])
+    model.summary()
+    self._load_weights_if_possible(model, flags_obj.init_weight_path)
+
+    cur_log_dir = _get_log_dir_or_default(flags_obj)
+    _ensure_dir(cur_log_dir)
+
+    map_data_fn = data_pipeline.map_data_for_transformer_fn
+    train_ds = data_pipeline.train_input_fn(params)
+    train_ds = train_ds.map(
+        map_data_fn, num_parallel_calls=params["num_parallel_calls"])
+    valid_ds = data_pipeline.eval_input_fn(params)
+    valid_ds = valid_ds.map(
+        map_data_fn, num_parallel_calls=params["num_parallel_calls"])
+
+    init_epoch = flags_obj.init_epoch or 0
+    init_steps = init_epoch * flags_obj.steps_per_epoch
+    callbacks = self._create_callbacks(cur_log_dir, init_steps, params)
+
+    history = model.fit(
+        train_ds,
+        initial_epoch=init_epoch,
+        epochs=flags_obj.train_epochs,
+        steps_per_epoch=flags_obj.steps_per_epoch,
+        validation_data=valid_ds,
+        validation_steps=flags_obj.validation_steps,
+        callbacks=callbacks)
+    tf.compat.v1.logging.info("\nTrain history: {}".format(history.history))
+
+    save_weight_path = os.path.join(cur_log_dir, "saves-model-weights.hdf5")
+    save_model_path = os.path.join(cur_log_dir, "saves-model.hdf5")
+    model.save_weights(save_weight_path)
+    model.save(save_model_path)
+
+  def eval(self):
+    """Evaluates the model."""
+    params, flags_obj, is_train = self.params, self.flags_obj, False
+    with tf.name_scope("model"):
+      model = transformer.create_model(params, is_train)
+      self._load_weights_if_possible(model, flags_obj.init_weight_path)
+      model.summary()
+    evaluate_and_log_bleu(model, flags_obj.bleu_source, flags_obj.bleu_ref,
+                          flags_obj.vocab_file)
+
+  def predict(self):
+    """Predicts result from the model."""
+    params, flags_obj, is_train = self.params, self.flags_obj, False
+
+    with tf.name_scope("model"):
+      model = transformer.create_model(params, is_train)
+      self._load_weights_if_possible(model, flags_obj.init_weight_path)
+      model.summary()
+    subtokenizer = tokenizer.Subtokenizer(flags_obj.vocab_file)
+
+    ds = data_pipeline.eval_input_fn(params)
+    ds = ds.map(lambda x, y: x).take(_SINGLE_SAMPLE)
+    ret = model.predict(ds)
+    val_outputs, _ = ret
+    length = len(val_outputs)
+    for i in range(length):
+      translate.translate_from_input(val_outputs[i], subtokenizer)
+
+  def _create_callbacks(self, cur_log_dir, init_steps, params):
+    """Creates a list of callbacks."""
+    sfunc = optimizer.LearningRateFn(params["learning_rate"],
+                                     params["hidden_size"],
+                                     params["learning_rate_warmup_steps"])
+    scheduler_callback = optimizer.LearningRateScheduler(sfunc, init_steps)
+
+    tb_logdir = os.path.join(cur_log_dir, "logs")
+    save_path = os.path.join(cur_log_dir,
+                             "weights-epoch-{epoch:02d}-loss-{loss:.4f}.hdf5")
+    csv_path = os.path.join(cur_log_dir, "result.csv")
+    return [
+        scheduler_callback,
+        tf.keras.callbacks.TensorBoard(tb_logdir),
+        tf.keras.callbacks.ModelCheckpoint(save_path, save_weights_only=True),
+        tf.keras.callbacks.CSVLogger(csv_path, append=True),
+    ]
+
+  def _load_weights_if_possible(self, model, init_weight_path=None):
+    """Loads model weights when it is provided."""
+    if init_weight_path:
+      tf.compat.v1.logging.info("Load weights: {}".format(init_weight_path))
+      model.load_weights(init_weight_path, by_name=True)
+
+  def _create_optimizer(self):
+    """Creates optimizer."""
+    params = self.params
+    opt = optimizer.LazyAdam(
+        params["learning_rate"],
+        params["optimizer_adam_beta1"],
+        params["optimizer_adam_beta2"],
+        epsilon=params["optimizer_adam_epsilon"])
+    return opt
+
+
+def _get_log_dir_or_default(flags_obj):
+  """Gets init_logdir_timestamp if it is given, otherwise use current time."""
+  if flags_obj.init_logdir_timestamp is not None:
+    timestamp = flags_obj.init_logdir_timestamp
+  else:
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M")
+  return os.path.join(flags_obj.model_dir, timestamp)
+
+
+def _ensure_dir(log_dir):
+  """Makes log dir if not existed."""
+  if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+
+def main(_):
+  flags_obj = flags.FLAGS
+  with logger.benchmark_context(flags_obj):
+    task = TransformerTask(flags_obj)
+    if flags_obj.mode == "train":
+      task.train()
+    elif flags_obj.mode == "predict":
+      task.predict()
+    elif flags_obj.mode == "eval":
+      task.eval()
+    else:
+      raise ValueError("Invalid mode {}".format(flags_obj.mode))
+
+
+if __name__ == "__main__":
+  tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+  misc.define_transformer_flags()
+  tf.compat.v1.app.run(main)
