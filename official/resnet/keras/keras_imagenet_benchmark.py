625387380fc912b78e0d99ce01c0be823e4bcf31
rxsang <rxsang@google.com>
2019-05-23 14:02:07 -0700

    Refine the benchmark name.

--- official/resnet/keras/keras_imagenet_benchmark.py
+++ official/resnet/keras/keras_imagenet_benchmark.py
@@ -568,7 +568,7 @@ class Resnet50KerasBenchmarkBase(keras_benchmark.KerasBenchmark):
     FLAGS.data_delay_prefetch = True
     self._run_and_report_benchmark()
 
-  def benchmark_xla_8_gpu_fp16_optional_get_next(self):
+  def benchmark_xla_8_gpu_fp16_tweaked_optional_next(self):
     """Test Keras model with manual config tuning, XLA, 8 GPUs, fp16 and
        enabling get_next_as_optional.
     """
@@ -580,7 +580,7 @@ class Resnet50KerasBenchmarkBase(keras_benchmark.KerasBenchmark):
     FLAGS.enable_xla = True
     FLAGS.distribution_strategy = 'default'
     FLAGS.model_dir = self._get_model_dir(
-        'benchmark_xla_8_gpu_fp16_optional_get_next')
+        'benchmark_xla_8_gpu_fp16_tweaked_optional_next')
     FLAGS.batch_size = 256 * 8  # 8 GPUs
     FLAGS.use_tensor_lr = True
     # FLAGS.tf_gpu_thread_mode = 'gpu_private'
