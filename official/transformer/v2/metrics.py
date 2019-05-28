fd3314e2a99f713ce80127ffb6bd754d8c9724c9
guptapriya <14104855+guptapriya@users.noreply.github.com>
2019-05-28 01:27:25 -0700

    Revert metrics change
    Didn't actually work in distributed case..

--- official/transformer/v2/metrics.py
+++ official/transformer/v2/metrics.py
@@ -146,11 +146,12 @@ class MetricLayer(tf.keras.layers.Layer):
     """"Builds metric layer."""
     neg_log_perplexity = functools.partial(
         padded_neg_log_perplexity, vocab_size=self.vocab_size)
-    self.metric_fns = [
-        ("accuracy", padded_accuracy),
-        ("accuracy_top5", padded_accuracy_top5),
-        ("accuracy_per_sequence", padded_sequence_accuracy),
-        ("neg_log_perplexity", neg_log_perplexity),
+    self.metric_mean_fns = [
+        (tf.keras.metrics.Mean("accuracy"), padded_accuracy),
+        (tf.keras.metrics.Mean("accuracy_top5"), padded_accuracy_top5),
+        (tf.keras.metrics.Mean("accuracy_per_sequence"),
+         padded_sequence_accuracy),
+        (tf.keras.metrics.Mean("neg_log_perplexity"), neg_log_perplexity),
     ]
     super(MetricLayer, self).build(input_shape)
 
@@ -159,10 +160,12 @@ class MetricLayer(tf.keras.layers.Layer):
 
   def call(self, inputs):
     logits, targets = inputs[0], inputs[1]
-    for name, fn in self.metric_fns:
-      per_example_metric, weights = fn(logits, targets)
-      m = tf.reduce_mean(per_example_metric * weights)
-      self.add_metric(m, aggregation='mean', name='name')
+    # TODO(guptapriya): Remove this check when underlying issue to create metrics
+    # with dist strat in cross replica context is fixed.
+    if tf.distribute.has_strategy() and not tf.distribute.in_cross_replica_context():
+     for mean, fn in self.metric_mean_fns:
+       m = mean(*fn(logits, targets))
+       self.add_metric(m)
     return logits
 
 
