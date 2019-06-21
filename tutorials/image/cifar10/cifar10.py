aab9bf629382062475bbc8e09de85435a6ea24f9
GeorgeKesaev <kesaev@gmail.com>
2019-06-21 19:31:49 +0300

    change default train with float16 instead of float32 accuracy

--- tutorials/image/cifar10/cifar10.py
+++ tutorials/image/cifar10/cifar10.py
@@ -51,7 +51,7 @@ FLAGS = tf.app.flags.FLAGS
 # Basic model parameters.
 tf.app.flags.DEFINE_integer('batch_size', 128,
                             """Number of images to process in a batch.""")
-tf.app.flags.DEFINE_boolean('use_fp16', False,
+tf.app.flags.DEFINE_boolean('use_fp16', True,
                             """Train the model using fp16.""")
 
 # Global constants describing the CIFAR-10 data set.
