0d2f029f4d5321cb57f4c975db5940a9bf06d45f
vishal sharma <vishalsh533@gmail.com>
2019-06-25 10:17:23 +0530

    help user to create variable file from saved model.ckpt file
    This file will help user to create variable file from saved model.ckpt file

---
+++ research/object_detection/Python script for getting variable file from model.ckpt (1).py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import tensorflow as tf
+from tensorflow.python.training import checkpoint_utils as cp
+
+
+# In[ ]:
+
+
+for i in range(0,51): 
+    variable_name = 'd' + str(i)
+    variable_name = tf.get_variable(cp.list_variables(r'C:\tensorflow1\models\research\object_detection\training\fast-style-model.ckpt-done')[i][0],initializer = tf.constant(cp.list_variables(r'C:\tensorflow1\models\research\object_detection\training\fast-style-model.ckpt-done')[i][1]))
+    
+print(variable_name)
+
+
+# In[ ]:
+
+
+# initialize all of the variables
+init_op = tf.global_variables_initializer()
+
+
+# In[ ]:
+
+
+with tf.Session() as sess:
+    # initialize all of the variables in the session
+    sess.run(init_op)
+   
+
+
+# In[ ]:
+
+
+# create saver object
+saver = tf.train.Saver()
+for i, var in enumerate(saver._var_list):
+    print('Var {}: {}'.format(i, var))
+
+
+# In[ ]:
+
+
+with tf.Session() as sess:
+    # initialize all of the variables in the session
+    sess.run(init_op)
+
+    # save the variable in the disk
+    saved_path = saver.save(sess, r'saved_variable')
+    print('model saved in {}'.format(saved_path))
+
+
+# In[ ]:
+
+
+import os
+os.getcwd()
+
+
+# In[ ]:
+
+
+import os
+for file in os.listdir('.'):
+    if 'saved_variable' in file:
+        print(file)
+
