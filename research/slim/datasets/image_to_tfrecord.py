f87bf44c209a3c2b1eaed823b500a1be6428ecbd
KimHyunJune <35071931+oryondark@users.noreply.github.com>
2019-07-05 20:55:27 +0900

    Update and rename make_tfrecord.py to image_to_tfrecord.py

--- research/slim/datasets/image_to_tfrecord.py
+++ research/slim/datasets/image_to_tfrecord.py
@@ -1,21 +1,42 @@
-'''
-Create by hjkim in bigdata Lab. KOOKMIN Univ.
-'''
+#create by Hyunjune Kim in KOOKMIN University Bigdata Laboratory.
+import tensorflow as tf
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
+
+class ImageReader(object):
+  """Helper class that provides TensorFlow image coding utilities."""
+
+  def __init__(self):
+    # Initializes function that decodes RGB JPEG data.
+    self._decode_jpeg_data = tf.placeholder(dtype=tf.string)
+    self._decode_jpeg = tf.image.decode_jpeg(self._decode_jpeg_data, channels=3)
+
+  def read_image_dims(self, sess, image_data):
+    image = self.decode_jpeg(sess, image_data)
+    return image.shape[0], image.shape[1]
+
+  def decode_jpeg(self, sess, image_data):
+    image = sess.run(self._decode_jpeg,
+                     feed_dict={self._decode_jpeg_data: image_data})
+
+    return image
+
+
 '''
 This function will return a categories of dataset if you serve the dataset root path.
 '''
 class MakeTfRecord():
-    def __init__(self, image_dir):
+    def __init__(self, image_dir, root):
         self.image_dir = image_dir
         self.dirs = []
         self.image_labels = {}
+        self.parent_path = root
         '''
         image_labels = { label_1 : 0
                          label_2 : 1
                          ......
                          }
+        parent_path = "path/to/traindirectory"
         '''
         
     # The following functions can be used to convert a value to a type compatible
@@ -73,12 +94,17 @@ class MakeTfRecord():
                     'image/encoded': self._bytes_feature(image_string),
                 }
         return tf.train.Example(features=tf.train.Features(feature=feature))
-    
 
-    def _writer_fn(self, sess, c_label, total_category_num):
+    def _writer_fn(self, save_path, sess, c_label, total_category_num):
+        """
+        sess : session of tensorflow
+        c_label : candidated label
+        total_category_num : total number of labels
+        """
         image_reader = ImageReader()
         gen = self._generates_data()
-        with tf.python_io.TFRecordWriter('/dockermnt/dataset/training_tfrecord/trainImage_{0}_of_{1}.tfrecord'.format(str(c_label), str(len(total_category_num)))) as writer:
+        tfpath = '/{0}/trainImage_{1}_of_{2}.tfrecord'.format(save_path, str(c_label), str(len(total_category_num)))
+        with tf.python_io.TFRecordWriter() as writer:
             for path, label in gen:
                 if c_label == label :
                     # Read the filename:
@@ -87,25 +113,14 @@ class MakeTfRecord():
 
 
                     example = dataset_utils.image_to_tfexample(
-                    image_data, b'png', height, width, label)
-                    '''
-                    # Why do not get of shape???
-                    image_string = open(path, 'rb').read()
-                    im = imread(path) # so, i had get shape using the plt lib.
-                    tfrecord = self._image_example(im, image_string, label)
-                    '''
+                    image_data, b'png', height, width, label) # i should setting the png format.
                     writer.write(example.SerializeToString())
 
     def build(self):
-        '''
-		tfrecord_mk = MakeTfRecord()
-		tfrecord_mk.build()
-		You can see that the log prints a categories in directory.
-        '''
+        
         self._create_labels() # label key-map
         total_category_num = os.listdir(self.image_dir)
         print(total_category_num)
         with tf.Session('') as sess:            
-            for c_label in range(70): # fixed
-                self._writer_fn(sess, c_label, total_category_num)
-
+            for c_label in range(total_category_num):
+                self._writer_fn(sess, self.parent_path, c_label, total_category_num)
