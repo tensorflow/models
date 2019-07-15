6631f1d7d57ca35dbbda737599ed998740b209e8
Bruce Fontaine <bfontain@google.com>
2019-07-15 15:39:25 -0700

    Fix import order

--- official/staging/shakespeare/shakespeare_main.py
+++ official/staging/shakespeare/shakespeare_main.py
@@ -19,10 +19,11 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import numpy as np
 
 from absl import app as absl_app
 from absl import flags
-import numpy as np
+
 import tensorflow as tf
 
 
