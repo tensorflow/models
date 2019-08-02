917101c0b197ec24890768e83bc960ae9cd2fb63
Hongkun Yu <saberkun@users.noreply.github.com>
2019-08-02 11:38:01 -0700

    Update presubmit.sh
    old lint is no longer used.

--- official/utils/testing/scripts/presubmit.sh
+++ official/utils/testing/scripts/presubmit.sh
@@ -26,33 +26,6 @@ MODEL_ROOT="$(pwd)"
 
 export PYTHONPATH="$PYTHONPATH:${MODEL_ROOT}"
 
-lint() {
-  local exit_code=0
-
-  RC_FILE="official/utils/testing/pylint.rcfile"
-  PROTO_SKIP="DO\sNOT\sEDIT!"
-
-  echo "===========Running lint test============"
-  for file in `find official/ -name '*.py' ! -name '*test.py' -print`
-  do
-    if grep ${PROTO_SKIP} ${file}; then
-      echo "Linting ${file} (Skipped: Machine generated file)"
-    else
-      echo "Linting ${file}"
-      pylint --rcfile="${RC_FILE}" "${file}" || exit_code=$?
-    fi
-  done
-
-  # More lenient for test files.
-  for file in `find official/ -name '*test.py' -print`
-  do
-    echo "Linting ${file}"
-    pylint --rcfile="${RC_FILE}" --disable=missing-docstring,protected-access "${file}" || exit_code=$?
-  done
-
-  return "${exit_code}"
-}
-
 py_test() {
   local PY_BINARY="$1"
   local exit_code=0
