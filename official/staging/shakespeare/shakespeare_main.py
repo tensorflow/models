97a84ac0e16783f6e166f6202135f53678a0f1bd
Taylor Robie <taylorrobie@google.com>
2019-08-06 14:35:28 -0700

    re-add implementation=2

--- official/staging/shakespeare/shakespeare_main.py
+++ official/staging/shakespeare/shakespeare_main.py
@@ -141,7 +141,10 @@ def build_model(vocab_size,
   if use_cudnn and not keras_utils.is_v2_0():
     LSTM = tf.compat.v1.CuDNNLSTM
   else:
-    LSTM = tf.keras.layers.LSTM
+    # The LSTM call was rewritten to be more efficient in 2.0. However because
+    # we want to compare the performance of the two runtimes, we force both
+    # V1 and V2 to use the more efficient implementation.
+    LSTM = functools.partial(tf.keras.layers.LSTM, implementation=2)
 
   # By indirecting the activation through a lambda layer, the logic to dispatch
   # to CuDNN in V2 doesn't trigger and we force the LSTM to run in non-CuDNN
