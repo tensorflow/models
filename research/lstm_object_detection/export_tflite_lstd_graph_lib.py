d113dea99462144251f8ac7a05edc0c5b60e6ad5
richardbrks <richardbrks@gmail.com>
2019-08-08 19:26:05 -0400

    add and correct copyright

--- research/lstm_object_detection/export_tflite_lstd_graph_lib.py
+++ research/lstm_object_detection/export_tflite_lstd_graph_lib.py
@@ -1,4 +1,4 @@
-# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
 r"""Exports detection models to use with tf-lite.
 
 See export_tflite_lstd_graph.py for usage.
