5f2b5a30d6b9695529d684b0d4980f5997545abd
JinFree <ytrqwe12@gmail.com>
2019-08-16 14:09:21 +0900

    installing object detection api

--- install_for_VM.sh
+++ install_for_VM.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt install protobuf-compiler python3-tk -y
 pip3 install virtualenv
-source start_tf_detection && pip install tensorflow==1.12.0
+source start_tf_detection && pip install tensorflow==1.12.0 jupyter notebook Cython contextlib2 matplotlib pillow lxml 
+
