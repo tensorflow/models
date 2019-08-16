e30a0a731ba67002aeacaef05a9e5e09f6fca146
JinFree <ytrqwe12@gmail.com>
2019-08-16 14:14:30 +0900

    modify cocoapi github

--- install_for_VM.sh
+++ install_for_VM.sh
@@ -2,4 +2,9 @@
 sudo apt install protobuf-compiler python3-tk -y
 pip3 install virtualenv
 source start_tf_detection && pip install tensorflow==1.12.0 jupyter notebook Cython contextlib2 matplotlib pillow lxml 
-
+git clone https://github.com/JinFree/cocoapi.git
+cd cocoapi/PythonAPI
+cp -r pycocotools ../../research/
+cd ../../models/research/
+protoc object_detection/protos/*.proto --python_out=.
+python object_detection/builders/model_builder_test.py
