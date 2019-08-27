1241db2090cc6c8f1a9489e655ad8ddcfc929c01
saberkun <solitarylord@gmail.com>
2019-08-27 01:45:41 -0700

    revert reverted changes...

--- research/lstm_object_detection/test_tflite_model.py
+++ research/lstm_object_detection/test_tflite_model.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 # ==============================================================================
 
+"""Test a tflite model using random input data."""
+
+from __future__ import print_function
 from absl import flags
 import numpy as np
 import tensorflow as tf
@@ -23,28 +26,28 @@ FLAGS = flags.FLAGS
 
 def main(_):
 
-    flags.mark_flag_as_required('model_path')
+  flags.mark_flag_as_required('model_path')
 
-    # Load TFLite model and allocate tensors.
-    interpreter = tf.lite.Interpreter(model_path=FLAGS.model_path)
-    interpreter.allocate_tensors()
+  # Load TFLite model and allocate tensors.
+  interpreter = tf.lite.Interpreter(model_path=FLAGS.model_path)
+  interpreter.allocate_tensors()
 
-    # Get input and output tensors.
-    input_details = interpreter.get_input_details()
-    print 'input_details:', input_details
-    output_details = interpreter.get_output_details()
-    print 'output_details:', output_details
+  # Get input and output tensors.
+  input_details = interpreter.get_input_details()
+  print('input_details:', input_details)
+  output_details = interpreter.get_output_details()
+  print('output_details:', output_details)
 
-    # Test model on random input data.
-    input_shape = input_details[0]['shape']
-    # change the following line to feed into your own data.
-    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-    interpreter.set_tensor(input_details[0]['index'], input_data)
+  # Test model on random input data.
+  input_shape = input_details[0]['shape']
+  # change the following line to feed into your own data.
+  input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+  interpreter.set_tensor(input_details[0]['index'], input_data)
 
-    interpreter.invoke()
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    print output_data
+  interpreter.invoke()
+  output_data = interpreter.get_tensor(output_details[0]['index'])
+  print(output_data)
 
 
 if __name__ == '__main__':
-    tf.app.run()
+  tf.app.run()
