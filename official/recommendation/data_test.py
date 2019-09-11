1577ed076cab2d38be4885a1b0678756f7aacd8b
Jiri Simsa <jsimsa@google.com>
2019-09-11 00:37:11 -0700

    Fix TF 2.0 tf.data API usage.
    PiperOrigin-RevId: 268401010

--- official/recommendation/data_test.py
+++ official/recommendation/data_test.py
@@ -127,7 +127,7 @@ class BaseTest(tf.test.TestCase):
     # type: (tf.data.Dataset, tf.Graph) -> list
     with self.session(graph=g) as sess:
       with g.as_default():
-        batch = dataset.make_one_shot_iterator().get_next()
+        batch = tf.compat.v1.data.make_one_shot_iterator(dataset).get_next()
       output = []
       while True:
         try:
