e4ac1b248fa52f19437b984318201104aef45bdd
Anton Khlebka <36956567+AKhlebko@users.noreply.github.com>
2019-09-24 15:04:34 +0300

    Change perlin tensor read place

--- research/deeplab/input_preprocess.py
+++ research/deeplab/input_preprocess.py
@@ -24,13 +24,6 @@ import os
 _PROB_OF_FLIP = 0.5
 _AUG_PROB = 0.25
 
-PERLIN_NOISE_IMAGE_TENSOR = tf.io.decode_image(
-  tf.read_file(os.path.expanduser("~/satelite_seg/perlin_image.png")),
-  channels=3,
-  dtype=tf.dtypes.float32,
-  expand_animations=False
-)
-
 def preprocess_image_and_label(image,
                                label,
                                crop_height,
@@ -142,7 +135,13 @@ def preprocess_image_and_label(image,
       processed_image, label
       )
 
-  if is_training:
+  if is_training and preprocess_utils.get_random_number() < _AUG_PROB:
+    PERLIN_NOISE_IMAGE_TENSOR = tf.io.decode_image(
+      tf.read_file(os.path.expanduser("~/satelite_seg/perlin_image.png")),
+      channels=3,
+      dtype=tf.dtypes.float32,
+      expand_animations=False
+    )
     perlin_image = preprocess_utils.random_crop([PERLIN_NOISE_IMAGE_TENSOR], crop_height, crop_width)[0]
     processed_image, label = preprocess_utils.add_perlin_noise(
       perlin_image, processed_image, label
