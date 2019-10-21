c3bf4a7980d14e47f0a933151a0951f572ba85ff
minoring <minho1350@gmail.com>
2019-10-21 12:14:22 -0700

    Fix typo in utils/flags/_conventions.py
    arparse -> argparse

--- official/utils/flags/_conventions.py
+++ official/utils/flags/_conventions.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Central location for shared arparse convention definitions."""
+"""Central location for shared argparse convention definitions."""
 
 from __future__ import absolute_import
 from __future__ import division
