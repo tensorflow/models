6c486c7d0e525a0b8dab0d2aefc0d1504c9cb82b
JinFree <ytrqwe12@gmail.com>
2019-12-13 09:55:33 +0900

    fix error

--- install_for_TX2_jetpack_4.2.2.sh
+++ install_for_TX2_jetpack_4.2.2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install python3-pip python3-numpy python3-matplotlib python3-pil python3-lxml python3-tk-y
+sudo apt install python3-pip python3-numpy python3-matplotlib python3-pil python3-lxml python3-tk -y
 sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev 
 wget https://developer.download.nvidia.com/compute/redist/jp/v42/tensorflow-gpu/tensorflow_gpu-1.14.0+nv19.7-cp36-cp36m-linux_aarch64.whl
 pip3 install tensorflow_gpu-1.14.0+nv19.7-cp36-cp36m-linux_aarch64.whl
@@ -17,3 +17,8 @@ cd research/
 protoc object_detection/protos/*.proto --python_out=.
 
 python3 object_detection/builders/model_builder_test.py
+
+
+
+
+
