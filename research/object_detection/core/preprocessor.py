9bed652a3189d2a37bbe11aa1e34c5327d979c23
Li-Chi Huang <bill2239@gmail.com>
2020-01-08 14:04:20 -0600

    Revert "fix means to take list in subtract_channel_mean for preprocessor"
    This reverts commit 4596f426742a43211864295c5fccf635fc777605.

--- research/object_detection/core/preprocessor.py
+++ research/object_detection/core/preprocessor.py
@@ -2983,7 +2983,6 @@ def subtract_channel_mean(image, means=None):
       raise ValueError('Input must be of size [height, width, channels]')
     if len(means) != image.get_shape()[-1]:
       raise ValueError('len(means) must match the number of channels')
-    means = list(means)
     return image - [[means]]
 
 
