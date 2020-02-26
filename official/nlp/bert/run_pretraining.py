7c0e458b83b394a26d56085cf933dc2eb46df239
Hongkun Yu <hongkuny@google.com>
2020-02-25 16:02:42 -0800

    Clean up doc string.
    PiperOrigin-RevId: 297226166

--- official/nlp/bert/run_pretraining.py
+++ official/nlp/bert/run_pretraining.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Run masked LM/next sentence masked_lm pre-training for BERT in TF 2.x."""
+"""Run masked LM/next sentence pre-training for BERT in TF 2.x."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
