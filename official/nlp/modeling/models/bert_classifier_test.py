0066ae22e2a9fe03b4c58810c48c04e858f3a6ad
Hongkun Yu <hongkuny@google.com>
2020-03-04 11:57:45 -0800

    Creates a models folder to host all keras.Model end points.
    PiperOrigin-RevId: 298905114

--- official/nlp/modeling/models/bert_classifier_test.py
+++ official/nlp/modeling/models/bert_classifier_test.py
@@ -22,13 +22,13 @@ import tensorflow as tf
 
 from tensorflow.python.keras import keras_parameterized  # pylint: disable=g-direct-tensorflow-import
 from official.nlp.modeling import networks
-from official.nlp.modeling.networks import bert_span_labeler
+from official.nlp.modeling.models import bert_classifier
 
 
 # This decorator runs the test in V1, V2-Eager, and V2-Functional mode. It
 # guarantees forward compatibility of this code for the V2 switchover.
 @keras_parameterized.run_all_keras_modes
-class BertSpanLabelerTest(keras_parameterized.TestCase):
+class BertClassifierTest(keras_parameterized.TestCase):
 
   def test_bert_trainer(self):
     """Validate that the Keras object can be created."""
@@ -39,7 +39,10 @@ class BertSpanLabelerTest(keras_parameterized.TestCase):
         vocab_size=vocab_size, num_layers=2, sequence_length=sequence_length)
 
     # Create a BERT trainer with the created network.
-    bert_trainer_model = bert_span_labeler.BertSpanLabeler(test_network)
+    num_classes = 3
+    bert_trainer_model = bert_classifier.BertClassifier(
+        test_network,
+        num_classes=num_classes)
 
     # Create a set of 2-dimensional inputs (the first dimension is implicit).
     word_ids = tf.keras.Input(shape=(sequence_length,), dtype=tf.int32)
@@ -49,32 +52,9 @@ class BertSpanLabelerTest(keras_parameterized.TestCase):
     # Invoke the trainer model on the inputs. This causes the layer to be built.
     cls_outs = bert_trainer_model([word_ids, mask, type_ids])
 
-    # Validate that there are 2 outputs are of the expected shape.
-    self.assertEqual(2, len(cls_outs))
-    expected_shape = [None, sequence_length]
-    for out in cls_outs:
-      self.assertAllEqual(expected_shape, out.shape.as_list())
-
-  def test_bert_trainer_named_compilation(self):
-    """Validate compilation using explicit output names."""
-    # Build a transformer network to use within the BERT trainer.
-    vocab_size = 100
-    sequence_length = 512
-    test_network = networks.TransformerEncoder(
-        vocab_size=vocab_size, num_layers=2, sequence_length=sequence_length)
-
-    # Create a BERT trainer with the created network.
-    bert_trainer_model = bert_span_labeler.BertSpanLabeler(test_network)
-
-    # Attempt to compile the model using a string-keyed dict of output names to
-    # loss functions. This will validate that the outputs are named as we
-    # expect.
-    bert_trainer_model.compile(
-        optimizer='sgd',
-        loss={
-            'start_positions': 'mse',
-            'end_positions': 'mse'
-        })
+    # Validate that the outputs are of the expected shape.
+    expected_classification_shape = [None, num_classes]
+    self.assertAllEqual(expected_classification_shape, cls_outs.shape.as_list())
 
   def test_bert_trainer_tensor_call(self):
     """Validate that the Keras object can be invoked."""
@@ -84,7 +64,8 @@ class BertSpanLabelerTest(keras_parameterized.TestCase):
         vocab_size=100, num_layers=2, sequence_length=2)
 
     # Create a BERT trainer with the created network.
-    bert_trainer_model = bert_span_labeler.BertSpanLabeler(test_network)
+    bert_trainer_model = bert_classifier.BertClassifier(
+        test_network, num_classes=2)
 
     # Create a set of 2-dimensional data tensors to feed into the model.
     word_ids = tf.constant([[1, 1], [2, 2]], dtype=tf.int32)
@@ -105,12 +86,12 @@ class BertSpanLabelerTest(keras_parameterized.TestCase):
 
     # Create a BERT trainer with the created network. (Note that all the args
     # are different, so we can catch any serialization mismatches.)
-    bert_trainer_model = bert_span_labeler.BertSpanLabeler(test_network)
+    bert_trainer_model = bert_classifier.BertClassifier(
+        test_network, num_classes=4, initializer='zeros', output='predictions')
 
     # Create another BERT trainer via serialization and deserialization.
     config = bert_trainer_model.get_config()
-    new_bert_trainer_model = bert_span_labeler.BertSpanLabeler.from_config(
-        config)
+    new_bert_trainer_model = bert_classifier.BertClassifier.from_config(config)
 
     # Validate that the config can be forced to JSON.
     _ = new_bert_trainer_model.to_json()
