c25e3e0148d8d98bd373bb9b0d0fde118caf003d
Chen Chen <chendouble@google.com>
2020-03-13 11:42:12 -0700

    Internal Change
    PiperOrigin-RevId: 300796559

--- official/pip_package/setup.py
+++ official/pip_package/setup.py
@@ -13,12 +13,28 @@
 # limitations under the License.
 # ==============================================================================
 """Sets up TensorFlow Official Models."""
+import datetime
+import sys
+
 from setuptools import find_packages
 from setuptools import setup
 
+version = '2.1.0.dev2'
+
+project_name = 'tf-models-official'
+
+if '--project_name' in sys.argv:
+  project_name_idx = sys.argv.index('--project_name')
+  project_name = sys.argv[project_name_idx + 1]
+  sys.argv.remove('--project_name')
+  sys.argv.pop(project_name_idx)
+
+if project_name == 'tf-models-nightly':
+  version += '.' + datetime.datetime.now().strftime('%Y%m%d%H%M')
+
 setup(
-    name='tf-models-official',
-    version='0.0.3.dev1',
+    name=project_name,
+    version=version,
     description='TensorFlow Official Models',
     author='Google Inc.',
     author_email='no-reply@google.com',
