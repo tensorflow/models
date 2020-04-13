1025682fd04be63f8f422b655c6646422cc01259
Zongwei Zhou <zongweiz@google.com>
2020-04-13 12:37:55 -0700

    Reduce Resnet50 pure eager FP32 test batch size
    PiperOrigin-RevId: 306287724

--- official/benchmark/resnet_ctl_imagenet_benchmark.py
+++ official/benchmark/resnet_ctl_imagenet_benchmark.py
@@ -288,7 +288,7 @@ class Resnet50CtlBenchmarkBase(CtlBenchmark):
     FLAGS.num_gpus = 1
     FLAGS.distribution_strategy = 'one_device'
     FLAGS.model_dir = self._get_model_dir('benchmark_1_gpu_eager')
-    FLAGS.batch_size = 128
+    FLAGS.batch_size = 120
     FLAGS.use_tf_function = False
     FLAGS.use_tf_while_loop = False
     FLAGS.single_l2_loss_op = True
