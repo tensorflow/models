a174bf5b1db0e2c1e04697ff5aae5182bd1c60e7
Allen Wang <allencwang@google.com>
2020-04-17 16:35:38 -0700

    Move resnet_imagenet_benchmark.py to the benchmarks/models folder.
    PiperOrigin-RevId: 307137297

--- official/benchmark/keras_imagenet_benchmark.py
+++ official/benchmark/keras_imagenet_benchmark.py
@@ -28,8 +28,8 @@ import tensorflow as tf  # pylint: disable=g-bad-import-order
 
 from official.benchmark import benchmark_wrappers
 from official.benchmark import keras_benchmark
+from official.benchmark.models import resnet_imagenet_main
 from official.vision.image_classification import classifier_trainer
-from official.vision.image_classification.resnet import resnet_imagenet_main
 
 MIN_TOP_1_ACCURACY = 0.76
 MAX_TOP_1_ACCURACY = 0.77
