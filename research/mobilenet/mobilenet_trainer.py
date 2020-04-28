ba4709d6e677982083452b5996a9d324516e8b3a
luoshixin <luoshixin@google.com>
2020-04-28 11:41:00 +0800

    set up module structure for mobilenet

---
+++ research/mobilenet/mobilenet_trainer.py
@@ -0,0 +1,35 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
+"""Runs an Image Classification task for MobileNet."""
+
+from absl import app
+from absl import flags
+from absl import logging
+
+
+def train_and_eval():
+  """Runs the train and eval path using compile/fit."""
+  pass
+
+
+def main(_):
+  pass
+
+
+if __name__ == '__main__':
+  logging.set_verbosity(logging.INFO)
+
+  app.run(main)
