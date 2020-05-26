73b5be67f8b9b70b46c5cfb7b6b69b0106b1b94c
Allen Wang <allencwang@google.com>
2020-05-26 13:39:20 -0700

    Modify `augment.transform` to use `tf.rank` rather than `len(shape)`.
    PiperOrigin-RevId: 313259032

--- official/vision/image_classification/augment.py
+++ official/vision/image_classification/augment.py
@@ -169,7 +169,7 @@ def transform(image: tf.Tensor, transforms) -> tf.Tensor:
   """Prepares input data for `image_ops.transform`."""
   original_ndims = tf.rank(image)
   transforms = tf.convert_to_tensor(transforms, dtype=tf.float32)
-  if len(tf.shape(transforms)) == 1:
+  if tf.rank(transforms) == 1:
     transforms = transforms[None]
   image = to_4d(image)
   image = image_ops.transform(
