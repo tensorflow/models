427543022981827958fc5a186f20f1a9fbc90c57
xinliupitt <xinliupitt@gmail.com>
2020-05-29 14:41:57 -0400

    dimention list

--- official/nlp/modeling/layers/position_embedding.py
+++ official/nlp/modeling/layers/position_embedding.py
@@ -167,13 +167,16 @@ class PositionEmbeddingRelative(tf.keras.layers.Layer):
 
   def build(self, input_shape):
     """Implements build() for the layer."""
-    # tf.print('input shape', input_shape)
+    dimension_list = input_shape.as_list()
+    tf.print('input shape', dimension_list)
+    self._length = dimension_list[1]
     super(PositionEmbeddingRelative, self).build(input_shape)
 
   def call(self, inputs):
     """Implements call() for the layer."""
-    tf.print('tf shape', tf.shpae(inputs))
-    self._length = tf.shpae(inputs)[1]
+
+    # tf.print('tf shape', tf.shpae(inputs))
+    # self._length = tf.shpae(inputs)[1]
     position = tf.cast(tf.range(self._length), tf.float32)
     num_timescales = self._hidden_size // 2
     min_timescale, max_timescale = self._min_timescale, self._max_timescale
