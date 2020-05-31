0691aeae87b609383d5c1120f55cc0efb0e8fac0
xinliupitt <xinliupitt@gmail.com>
2020-05-31 16:34:15 -0400

    output example

--- official/nlp/modeling/layers/position_embedding_temp.py
+++ official/nlp/modeling/layers/position_embedding_temp.py
@@ -31,3 +31,9 @@ input_tensor = tf.keras.Input(shape=(97, 32))
 output = pos_layer(input_tensor)
 print ('input shape', input_tensor.shape.as_list())
 print ('output shape', output.shape.as_list())
+
+pos_layer = position_embedding.PositionEmbeddingRelative(
+    hidden_size=hidden_size)
+input_tensor = tf.constant([[0,0,0], [1,1,1]])
+output = pos_layer(input_tensor)
+tf.print('output', output)
