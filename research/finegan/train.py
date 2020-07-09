eac644b6051f10aa534b6e7b25dde01a5646dd07
Vishal-V <vishal114186@gmail.com>
2020-07-09 23:55:39 +0530

    Added FineGAN Train Utils

--- research/finegan/train.py
+++ research/finegan/train.py
@@ -15,3 +15,59 @@
 """Trains the FineGAN Model for Fine Grained Object Generation and Discovery.
 """
 
+import tensorflow as tf
+assert tf.version.VERSION.startswith('2.')
+
+from .model import GeneratorArchitecture, DiscriminatorArchitecture
+from .model import child_to_parent
+from .config.config import Config
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+def save_images():
+    # TODO: Save images to the folder
+    pass
+
+
+class FineGAN(object):
+    """The FineGAN Architecture"""
+    def __init__(self, cfg, out_path, img_path, dataset, **kwargs):
+        super(FineGAN, self).__init__(**kwargs)
+        self.batch_size = cfg.TRAIN['BATCH_SIZE']
+        self.num_epochs = cfg.TRAIN['MAX_EPOCH']
+        self.dataset = dataset
+        self.num_batches = len(dataset)
+        
+    def prepare_dataset(self):
+        data = tf.data.Dataset.list_files('./dataset/*'))
+        data = data.map(lambda image: tf.image.resize(image, (128,128,3)), num_parallel_calls=AUTOTUNE)
+        data = data.cache().shuffle(1000).repeat(2)
+        return data.batch(self.batch_size).prefetch(num_parallel_calls=AUTOTUNE)
+
+    def plot_images(images, num_cols=None):
+	num_cols = num_cols or len(images)
+	num_rows = (len(images) - 1) // num_cols + 1
+	if images.shape[-1] == 1:
+		images = np.squeeze(images, axis=-1)
+	plt.figure(figsize=(num_cols, num_rows))
+	for index, image in enumerate(images):
+		plt.subplot(num_rows, num_cols, index + 1)
+		plt.imshow(image)
+		plt.axis("off")
+
+    def train_generator():
+        # TODO: Train the Generators
+        pass
+
+    def train_discriminator():
+        # TODO: Train the Discriminator
+        pass
+
+    def train():
+        # TODO: Train the entire FineGAN
+        # TODO: Only for Background and Child phases
+        pass
+
+if __name__ == '__main__':
+    cfg = Config(32)
+    gen = GeneratorArchitecture(cfg, '', '')
