4f5b951cafe355580e4440d9fd14363fcacf5c99
Kaushik Shivakumar <kaushikshiv@google.com>
2020-07-10 07:44:50 +0000

    add colab utils file

--- research/object_detection/utils/colab_utils.py
+++ research/object_detection/utils/colab_utils.py
@@ -15,9 +15,32 @@
 """Utils for the colab tutorials located in colab_tutorials/
 """
 
+import matplotlib
+import matplotlib.pyplot as plt
+
+import os
+import random
+import uuid
+import io
+import scipy.misc
+import numpy as np
+from six import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+
+import tensorflow as tf
+
+from object_detection.utils import label_map_util
+from object_detection.utils import config_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.utils import colab_utils
+from object_detection.builders import model_builder
+
+from IPython.display import display, Javascript
 from google.colab import output
 from google.colab.output import eval_js
+from base64 import b64decode, b64encode
 from typing import List, Dict, Union
+import operator
 import json
 
 def draw_bbox(image_urls, callbackId):
