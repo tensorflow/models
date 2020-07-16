cca97db0e2cda358dd52d1f6b3a7822c1b8a4374
Kaushik Shivakumar <kaushikshiv@google.com>
2020-07-16 20:43:12 +0000

    add setup back

---
+++ research/setup.py
@@ -0,0 +1,16 @@
+"""Setup script for object_detection."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+REQUIRED_PACKAGES = ['Pillow>=1.0', 'Matplotlib>=2.1', 'Cython>=0.28.1']
+
+setup(
+    name='object_detection',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    include_package_data=True,
+    packages=[p for p in find_packages() if p.startswith('object_detection')],
+    description='Tensorflow Object Detection Library',
+)
