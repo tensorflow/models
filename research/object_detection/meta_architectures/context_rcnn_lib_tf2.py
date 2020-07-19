e9f620aff555c9f4e7e55b3e462092504961873a
Kaushik Shivakumar <kaushikshiv@google.com>
2020-07-19 22:29:09 +0000

    make some changes for PR" ;

--- research/object_detection/meta_architectures/context_rcnn_lib_tf2.py
+++ research/object_detection/meta_architectures/context_rcnn_lib_tf2.py
@@ -14,10 +14,6 @@
 # limitations under the License.
 # ==============================================================================
 """Library functions for ContextRCNN."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
 
 # The negative value used in padding the invalid weights.
