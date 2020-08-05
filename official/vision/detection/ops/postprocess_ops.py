e0b8f878fac1b96bda0c83d3bedc065200ccc2c2
Srihari Humbarwadi <sriharihumbarwadi97@gmail.com>
2020-08-05 19:36:00 +0530

    Removes normalization/denomalization in postprocess_ops
    sets `clip_boxes=False`, as it makes `tf.image.combined_non_max_suppression` is coordinate agnostic after https://github.com/tensorflow/tensorflow/commit/35e39cd3f102d663d198074e95ff77197d2e0a1d

--- official/vision/detection/ops/postprocess_ops.py
+++ official/vision/detection/ops/postprocess_ops.py
@@ -272,11 +272,6 @@ def _generate_detections_batched(boxes,
       `valid_detections` boxes are valid detections.
   """
   with tf.name_scope('generate_detections'):
-    # TODO(tsungyi): Removes normalization/denomalization once the
-    # tf.image.combined_non_max_suppression is coordinate system agnostic.
-    # Normalizes maximum box cooridinates to 1.
-    normalizer = tf.reduce_max(boxes)
-    boxes /= normalizer
     (nmsed_boxes, nmsed_scores, nmsed_classes,
      valid_detections) = tf.image.combined_non_max_suppression(
          boxes,
@@ -285,9 +280,8 @@ def _generate_detections_batched(boxes,
          max_total_size=max_total_size,
          iou_threshold=nms_iou_threshold,
          score_threshold=score_threshold,
-         pad_per_class=False,)
-    # De-normalizes box cooridinates.
-    nmsed_boxes *= normalizer
+         pad_per_class=False,
+         clip_boxes=False)
   nmsed_classes = tf.cast(nmsed_classes, tf.int32)
   return nmsed_boxes, nmsed_scores, nmsed_classes, valid_detections
 
