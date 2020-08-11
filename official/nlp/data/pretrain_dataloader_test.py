878f6caff0522fdbc9a887b5048e4de9d7e81b37
A. Unique TensorFlower <gardener@tensorflow.org>
2020-08-10 22:27:50 -0700

    Internal change
    PiperOrigin-RevId: 325956876

---
+++ official/nlp/data/pretrain_dataloader_test.py
@@ -0,0 +1,107 @@
+# Lint as: python3
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for official.nlp.data.pretrain_dataloader."""
+import itertools
+import os
+
+from absl.testing import parameterized
+import numpy as np
+import tensorflow as tf
+
+from official.nlp.data import pretrain_dataloader
+
+
+def _create_fake_dataset(output_path, seq_length, max_predictions_per_seq,
+                         use_position_id, use_next_sentence_label):
+  """Creates a fake dataset."""
+  writer = tf.io.TFRecordWriter(output_path)
+
+  def create_int_feature(values):
+    f = tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
+    return f
+
+  def create_float_feature(values):
+    f = tf.train.Feature(float_list=tf.train.FloatList(value=list(values)))
+    return f
+
+  for _ in range(100):
+    features = {}
+    input_ids = np.random.randint(100, size=(seq_length))
+    features["input_ids"] = create_int_feature(input_ids)
+    features["input_mask"] = create_int_feature(np.ones_like(input_ids))
+    features["segment_ids"] = create_int_feature(np.ones_like(input_ids))
+
+    features["masked_lm_positions"] = create_int_feature(
+        np.random.randint(100, size=(max_predictions_per_seq)))
+    features["masked_lm_ids"] = create_int_feature(
+        np.random.randint(100, size=(max_predictions_per_seq)))
+    features["masked_lm_weights"] = create_float_feature(
+        [1.0] * max_predictions_per_seq)
+
+    if use_next_sentence_label:
+      features["next_sentence_labels"] = create_int_feature([1])
+
+    if use_position_id:
+      features["position_ids"] = create_int_feature(range(0, seq_length))
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature=features))
+    writer.write(tf_example.SerializeToString())
+  writer.close()
+
+
+class BertPretrainDataTest(tf.test.TestCase, parameterized.TestCase):
+
+  @parameterized.parameters(itertools.product(
+      (False, True),
+      (False, True),
+  ))
+  def test_load_data(self, use_next_sentence_label, use_position_id):
+    train_data_path = os.path.join(self.get_temp_dir(), "train.tf_record")
+    seq_length = 128
+    max_predictions_per_seq = 20
+    _create_fake_dataset(
+        train_data_path,
+        seq_length,
+        max_predictions_per_seq,
+        use_next_sentence_label=use_next_sentence_label,
+        use_position_id=use_position_id)
+    data_config = pretrain_dataloader.BertPretrainDataConfig(
+        input_path=train_data_path,
+        max_predictions_per_seq=max_predictions_per_seq,
+        seq_length=seq_length,
+        global_batch_size=10,
+        is_training=True,
+        use_next_sentence_label=use_next_sentence_label,
+        use_position_id=use_position_id)
+
+    dataset = pretrain_dataloader.BertPretrainDataLoader(data_config).load()
+    features = next(iter(dataset))
+    self.assertLen(features,
+                   6 + int(use_next_sentence_label) + int(use_position_id))
+    self.assertIn("input_word_ids", features)
+    self.assertIn("input_mask", features)
+    self.assertIn("input_type_ids", features)
+    self.assertIn("masked_lm_positions", features)
+    self.assertIn("masked_lm_ids", features)
+    self.assertIn("masked_lm_weights", features)
+
+    self.assertEqual("next_sentence_labels" in features,
+                     use_next_sentence_label)
+    self.assertEqual("position_ids" in features, use_position_id)
+
+
+if __name__ == "__main__":
+  tf.test.main()
---
+++ official/nlp/data/tagging_data_lib_test.py
@@ -0,0 +1,109 @@
+# Lint as: python3
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for official.nlp.data.tagging_data_lib."""
+import os
+import random
+
+from absl.testing import parameterized
+import tensorflow as tf
+
+from official.nlp.bert import tokenization
+from official.nlp.data import tagging_data_lib
+
+
+def _create_fake_file(filename, labels, is_test):
+
+  def write_one_sentence(writer, length):
+    for _ in range(length):
+      line = "hiworld"
+      if not is_test:
+        line += "\t%s" % (labels[random.randint(0, len(labels) - 1)])
+      writer.write(line + "\n")
+
+  # Writes two sentences with length of 3 and 12 respectively.
+  with tf.io.gfile.GFile(filename, "w") as writer:
+    write_one_sentence(writer, 3)
+    writer.write("\n")
+    write_one_sentence(writer, 12)
+
+
+class TaggingDataLibTest(tf.test.TestCase, parameterized.TestCase):
+
+  def setUp(self):
+    super(TaggingDataLibTest, self).setUp()
+
+    self.processors = {
+        "panx": tagging_data_lib.PanxProcessor,
+        "udpos": tagging_data_lib.UdposProcessor,
+    }
+    self.vocab_file = os.path.join(self.get_temp_dir(), "vocab.txt")
+    with tf.io.gfile.GFile(self.vocab_file, "w") as writer:
+      writer.write("\n".join(["[CLS]", "[SEP]", "hi", "##world", "[UNK]"]))
+
+  @parameterized.parameters(
+      {"task_type": "panx"},
+      {"task_type": "udpos"},
+  )
+  def test_generate_tf_record(self, task_type):
+    processor = self.processors[task_type]()
+    input_data_dir = os.path.join(self.get_temp_dir(), task_type)
+    tf.io.gfile.mkdir(input_data_dir)
+    # Write fake train file.
+    _create_fake_file(
+        os.path.join(input_data_dir, "train-en.tsv"),
+        processor.get_labels(),
+        is_test=False)
+
+    # Write fake dev file.
+    _create_fake_file(
+        os.path.join(input_data_dir, "dev-en.tsv"),
+        processor.get_labels(),
+        is_test=False)
+
+    # Write fake test files.
+    for lang in processor.supported_languages:
+      _create_fake_file(
+          os.path.join(input_data_dir, "test-%s.tsv" % lang),
+          processor.get_labels(),
+          is_test=True)
+
+    output_path = os.path.join(self.get_temp_dir(), task_type, "output")
+    tokenizer = tokenization.FullTokenizer(
+        vocab_file=self.vocab_file, do_lower_case=True)
+    metadata = tagging_data_lib.generate_tf_record_from_data_file(
+        processor,
+        input_data_dir,
+        tokenizer,
+        max_seq_length=8,
+        train_data_output_path=os.path.join(output_path, "train.tfrecord"),
+        eval_data_output_path=os.path.join(output_path, "eval.tfrecord"),
+        test_data_output_path=os.path.join(output_path, "test_{}.tfrecord"),
+        text_preprocessing=tokenization.convert_to_unicode)
+
+    self.assertEqual(metadata["train_data_size"], 5)
+    files = tf.io.gfile.glob(output_path + "/*")
+    expected_files = []
+    expected_files.append(os.path.join(output_path, "train.tfrecord"))
+    expected_files.append(os.path.join(output_path, "eval.tfrecord"))
+    for lang in processor.supported_languages:
+      expected_files.append(
+          os.path.join(output_path, "test_%s.tfrecord" % lang))
+
+    self.assertCountEqual(files, expected_files)
+
+
+if __name__ == "__main__":
+  tf.test.main()
