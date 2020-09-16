9ad396aa167527a0cd8bee6b81c90e2a06df6392
Chen Chen <chendouble@google.com>
2020-09-16 14:09:19 -0700

    internal change
    PiperOrigin-RevId: 332084664

--- official/nlp/transformer/transformer_main.py
+++ official/nlp/transformer/transformer_main.py
@@ -159,6 +159,7 @@ class TransformerTask(object):
     params["enable_metrics_in_training"] = flags_obj.enable_metrics_in_training
     params["steps_between_evals"] = flags_obj.steps_between_evals
     params["enable_checkpointing"] = flags_obj.enable_checkpointing
+    params["save_weights_only"] = flags_obj.save_weights_only
 
     self.distribution_strategy = distribute_utils.get_distribution_strategy(
         distribution_strategy=flags_obj.distribution_strategy,
