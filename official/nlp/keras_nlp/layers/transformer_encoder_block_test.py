0c1c74c7cdac0a6fc636c8684728ae2109c9498e
Zhenyu Tan <tanzheny@google.com>
2020-09-18 12:17:52 -0700

    Internal change
    PiperOrigin-RevId: 332501400

--- official/nlp/keras_nlp/layers/transformer_encoder_block_test.py
+++ official/nlp/keras_nlp/layers/transformer_encoder_block_test.py
@@ -14,11 +14,6 @@
 # ==============================================================================
 """Tests for Keras-based transformer block layer."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-# Import libraries
 from absl.testing import parameterized
 import numpy as np
 import tensorflow as tf
