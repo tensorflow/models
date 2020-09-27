2b3f851b76eb795eb8465b3aed8463ed92dc0387
Shixin Luo <luoshixin@google.com>
2020-09-27 22:50:22 +0800

    remove future import

--- official/modeling/activations/relu_test.py
+++ official/modeling/activations/relu_test.py
@@ -14,10 +14,6 @@
 # ==============================================================================
 """Tests for the customized Relu activation."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
 
 from tensorflow.python.keras import \
