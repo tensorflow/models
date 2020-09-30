2986bcafb9eaa8fed4d78f17a04c4c5afc8f6691
moneypi <1483586698@qq.com>
2020-09-30 08:05:33 +0800

    replace tf.compat.v1.logging with absl.logging for deep_speech (#9222)

--- research/deep_speech/data/download.py
+++ research/deep_speech/data/download.py
@@ -32,6 +32,7 @@ import pandas
 from six.moves import urllib
 from sox import Transformer
 import tensorflow as tf
+from absl import logging
 
 LIBRI_SPEECH_URLS = {
     "train-clean-100":
@@ -65,7 +66,7 @@ def download_and_extract(directory, url):
   _, tar_filepath = tempfile.mkstemp(suffix=".tar.gz")
 
   try:
-    tf.compat.v1.logging.info("Downloading %s to %s" % (url, tar_filepath))
+    logging.info("Downloading %s to %s" % (url, tar_filepath))
 
     def _progress(count, block_size, total_size):
       sys.stdout.write("\r>> Downloading {} {:.1f}%".format(
@@ -75,7 +76,7 @@ def download_and_extract(directory, url):
     urllib.request.urlretrieve(url, tar_filepath, _progress)
     print()
     statinfo = os.stat(tar_filepath)
-    tf.compat.v1.logging.info(
+    logging.info(
         "Successfully downloaded %s, size(bytes): %d" % (url, statinfo.st_size))
     with tarfile.open(tar_filepath, "r") as tar:
       tar.extractall(directory)
@@ -112,7 +113,7 @@ def convert_audio_and_split_transcript(input_dir, source_name, target_name,
     output_file: the name of the newly generated csv file. e.g. test-clean.csv
   """
 
-  tf.compat.v1.logging.info("Preprocessing audio and transcript for %s" % source_name)
+  logging.info("Preprocessing audio and transcript for %s" % source_name)
   source_dir = os.path.join(input_dir, source_name)
   target_dir = os.path.join(input_dir, target_name)
 
@@ -149,7 +150,7 @@ def convert_audio_and_split_transcript(input_dir, source_name, target_name,
   df = pandas.DataFrame(
       data=files, columns=["wav_filename", "wav_filesize", "transcript"])
   df.to_csv(csv_file_path, index=False, sep="\t")
-  tf.compat.v1.logging.info("Successfully generated csv file {}".format(csv_file_path))
+  logging.info("Successfully generated csv file {}".format(csv_file_path))
 
 
 def download_and_process_datasets(directory, datasets):
@@ -160,10 +161,10 @@ def download_and_process_datasets(directory, datasets):
     datasets: list of dataset names that will be downloaded and processed.
   """
 
-  tf.compat.v1.logging.info("Preparing LibriSpeech dataset: {}".format(
+  logging.info("Preparing LibriSpeech dataset: {}".format(
       ",".join(datasets)))
   for dataset in datasets:
-    tf.compat.v1.logging.info("Preparing dataset %s", dataset)
+    logging.info("Preparing dataset %s", dataset)
     dataset_dir = os.path.join(directory, dataset)
     download_and_extract(dataset_dir, LIBRI_SPEECH_URLS[dataset])
     convert_audio_and_split_transcript(
@@ -202,7 +203,7 @@ def main(_):
 
 
 if __name__ == "__main__":
-  tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+  logging.set_verbosity(logging.INFO)
   define_data_download_flags()
   FLAGS = absl_flags.FLAGS
   absl_app.run(main)
