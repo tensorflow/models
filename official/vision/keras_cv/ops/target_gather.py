98c20111d0a88366cb75b97614ba9e543ca63ecb
Zhenyu Tan <tanzheny@google.com>
2020-10-09 13:21:39 -0700

    Internal change
    PiperOrigin-RevId: 336353171

--- official/vision/keras_cv/ops/target_gather.py
+++ official/vision/keras_cv/ops/target_gather.py
@@ -12,58 +12,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Definition of anchor labeler, which assigns ground truth boxes to anchors."""
+"""Definition of target gather, which gathers targets from indices."""
 
 import tensorflow as tf
 
 
-class AnchorLabeler:
-  """Labeler for dense object detector."""
+class TargetGather:
+  """Targer gather for dense object detector."""
 
-  def __call__(self, labels, match_indices, mask, mask_val=0.0):
+  def __call__(self, labels, match_indices, mask=None, mask_val=0.0):
     """Labels anchors with ground truth inputs.
 
     B: batch_size
     N: number of groundtruth boxes.
 
     Args:
-      labels: An integer tensor with shape [N, 1] or [B, N, 1] representing
+      labels: An integer tensor with shape [N, dims] or [B, N, ...] representing
         groundtruth labels.
-      match_indices: An integer tensor with shape [N] or [B, N] representing
+      match_indices: An integer tensor with shape [M] or [B, M] representing
         match label index.
-      mask: An integer tensor with shape [N] or [B, N] representing match
-        labels, e.g., 1 for positive, -1 for negative, -2 for ignore.
+      mask: An boolean tensor with shape [M, dims] or [B, M,...] representing
+        match labels.
       mask_val: An integer to fill in for mask.
 
     Returns:
-      class_targets: A integer Tensor with shape [num_anchors].
-      box_targets: A float Tensor with shape [num_anchors, 4].
-      class_weights: A float Tensor with shape [num_anchors], that
-        serves as masking / sample weight for classification loss. Its value
-        is 1.0 for positive and negative matched anchors, and 0.0 for ignored
-        anchors.
-      box_weights: A float Tensor with shape [num_anchors], that
-        serves as masking / sample weight for regression loss. Its value is
-        1.0 for positive matched anchors, and 0.0 for negative and ignored
-        anchors.
+      target: An integer Tensor with shape [M] or [B, M]
+    Raises:
+      ValueError: If `labels` is higher than rank 3.
     """
     if len(labels.shape) <= 2:
       return self._gather_unbatched(labels, match_indices, mask, mask_val)
     elif len(labels.shape) == 3:
       return self._gather_batched(labels, match_indices, mask, mask_val)
+    else:
+      raise ValueError("`TargetGather` does not support `labels` with rank "
+                       "larger than 3, got {}".format(len(labels.shape)))
 
   def _gather_unbatched(self, labels, match_indices, mask, mask_val):
     """Gather based on unbatched labels and boxes."""
     num_gt_boxes = tf.shape(labels)[0]
-    masked_targets = tf.cast(mask_val, labels.dtype) * tf.ones_like(
-        mask, dtype=labels.dtype)
 
     def _assign_when_rows_empty():
-      return masked_targets
+      if len(labels.shape) > 1:
+        mask_shape = [match_indices.shape[0], labels.shape[-1]]
+      else:
+        mask_shape = [match_indices.shape[0]]
+      return tf.cast(mask_val, labels.dtype) * tf.ones(
+          mask_shape, dtype=labels.dtype)
 
     def _assign_when_rows_not_empty():
       targets = tf.gather(labels, match_indices)
-      return tf.where(mask, masked_targets, targets)
+      if mask is None:
+        return targets
+      else:
+        masked_targets = tf.cast(mask_val, labels.dtype) * tf.ones_like(
+            mask, dtype=labels.dtype)
+        return tf.where(mask, masked_targets, targets)
 
     return tf.cond(tf.greater(num_gt_boxes, 0),
                    _assign_when_rows_not_empty,
@@ -73,9 +77,14 @@ class AnchorLabeler:
     """Gather based on batched labels."""
     batch_size = labels.shape[0]
     if batch_size == 1:
-      result = self._gather_unbatched(
-          tf.squeeze(labels, axis=0), tf.squeeze(match_indices, axis=0),
-          tf.squeeze(mask, axis=0), mask_val)
+      if mask is not None:
+        result = self._gather_unbatched(
+            tf.squeeze(labels, axis=0), tf.squeeze(match_indices, axis=0),
+            tf.squeeze(mask, axis=0), mask_val)
+      else:
+        result = self._gather_unbatched(
+            tf.squeeze(labels, axis=0), tf.squeeze(match_indices, axis=0),
+            None, mask_val)
       return tf.expand_dims(result, axis=0)
     else:
       indices_shape = tf.shape(match_indices)
@@ -86,4 +95,9 @@ class AnchorLabeler:
       gather_nd_indices = tf.stack(
           [batch_indices, match_indices], axis=-1)
       targets = tf.gather_nd(labels, gather_nd_indices)
-      return targets
+      if mask is None:
+        return targets
+      else:
+        masked_targets = tf.cast(mask_val, labels.dtype) * tf.ones_like(
+            mask, dtype=labels.dtype)
+        return tf.where(mask, masked_targets, targets)
