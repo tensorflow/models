64755c7f7690cdb54def915539df5c80e349be10
Zongwei Zhou <zongweiz@google.com>
2020-10-12 12:13:37 -0700

    Reduce batch size of resnet fp16 eager benchmark to prevent occasional OOM
    PiperOrigin-RevId: 336715941

--- official/benchmark/resnet_ctl_imagenet_benchmark.py
+++ official/benchmark/resnet_ctl_imagenet_benchmark.py
@@ -296,7 +296,7 @@ class Resnet50CtlBenchmarkBase(CtlBenchmark):
     FLAGS.num_gpus = 1
     FLAGS.distribution_strategy = 'one_device'
     FLAGS.model_dir = self._get_model_dir('benchmark_1_gpu_fp16_eager')
-    FLAGS.batch_size = 240
+    FLAGS.batch_size = 232
     FLAGS.dtype = 'fp16'
     FLAGS.use_tf_function = False
     FLAGS.use_tf_while_loop = False
