68068c08204efa3ceabfa07eecb3681d7e1e4414
Reed Wanderman-Milne <reedwm@google.com>
2020-11-17 13:47:28 -0800

    Internal change
    PiperOrigin-RevId: 342937744

--- official/nlp/transformer/transformer_main.py
+++ official/nlp/transformer/transformer_main.py
@@ -175,9 +175,8 @@ class TransformerTask(object):
     else:
       logging.info("Not using any distribution strategy.")
 
-    performance.set_mixed_precision_policy(
-        params["dtype"],
-        flags_core.get_loss_scale(flags_obj, default_for_fp16="dynamic"))
+    performance.set_mixed_precision_policy(params["dtype"],
+                                           use_experimental_api=False)
 
   @property
   def use_tpu(self):
@@ -443,7 +442,8 @@ class TransformerTask(object):
         use_float16=params["dtype"] == tf.float16,
         use_graph_rewrite=self.flags_obj.fp16_implementation == "graph_rewrite",
         loss_scale=flags_core.get_loss_scale(
-            self.flags_obj, default_for_fp16="dynamic"))
+            self.flags_obj, default_for_fp16="dynamic"),
+        use_experimental_api=False)
 
     return opt
 
