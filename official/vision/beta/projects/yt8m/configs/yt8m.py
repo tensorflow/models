03f71de127d7618f0f2bb1dd50a803dacde17f7e
Hye Yoon <hylee817@gmail.com>
2020-11-20 03:39:12 +0900

    update yt8m.py

--- official/vision/beta/projects/yt8m/configs/yt8m.py
+++ official/vision/beta/projects/yt8m/configs/yt8m.py
@@ -5,37 +5,67 @@ from official.core import config_definitions as cfg
 from official.core import exp_factory
 from official.modeling import hyperparams
 from official.modeling import optimization
-from official.vision.beta.configs import backbones_3d
 from official.vision.beta.configs import common
 
 class DataConfig(cfg.DataConfig):
   """The base configuration for building datasets."""
   name: Optional[str] = None
   file_type: Optional[str] = 'tfrecord'
-  compressed_input: bool = False
   split: str = 'train'
-  feature_shape: Tuple[int, ...] = (64, 224, 224, 3)
+  feature_sizes: Tuple[int, ...] = None
+  feature_names: Tuple[str, ...] = None
+  segment_size: int = 1
+  segment_labels: bool = False
   temporal_stride: int = 1
-  num_test_clips: int = 1
+  max_frames: int = -1
   num_classes: int = -1
   num_channels: int = 3
-  num_examples: int = -1
-  global_batch_size: int = 128
   num_devices: int = 1
-  data_format: str = 'channels_last'
   dtype: str = 'float32'
-  one_hot: bool = True
-  shuffle_buffer_size: int = 64
-  cache: bool = False
   input_path: str = ''
   is_training: bool = True
-  cycle_length: int = 10
-  min_image_size: int = 256
-
-class YT8MTask(Task):
-  """The task config."""
 
 
+def yt8m(is_training):
+  """YT8M dataset configs."""
+  return DataConfig(
+    name='yt8m',
+    num_classes=3862,
+    feature_sizes=[1024, 128],
+    feature_names=["rgb", "audio"],
+    max_frames=300,
+    segment_labels=False,
+    segment_size=5,
+    is_training=is_training,
+    split='train' if is_training else 'valid',
+  )
 
-class DbofModel(Config):
+class YT8MModel(hyperparams.Config):
   """The model config."""
+  num_classes : int = 3862, #TODO: get from reader, should be removed
+  num_frames : int = 32,    #TODO: get from reader, should be removed
+  iterations : int = 30
+  cluster_size : int = 8192
+  hidden_size : int = 1024
+  add_batch_norm : bool = True
+  sample_random_frame : bool = True
+  is_training : bool = True
+  activation : str = "sigmoid"
+  pooling_method : str = "max"
+  yt8m_agg_classifier_model : str = "MoeModel"
+
+class Losses(hyperparams.Config):
+  name = 'binary_crossentropy'
+  from_logits: bool = False
+  label_smoothing: float = 0.0
+
+
+class YT8MTask(cfg.TaskConfig):
+  """The task config."""
+  model: YT8MModel = YT8MModel()
+  train_data: DataConfig = DataConfig(is_training=True)
+  validation_data: DataConfig = DataConfig(is_training=False)
+  losses: Losses = Losses()
+
+
+
