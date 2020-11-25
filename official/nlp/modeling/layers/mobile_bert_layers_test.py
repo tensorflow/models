3039634d31f6f3f25cc7207f7e4d61bbaf54cd1f
Chen Chen <chendouble@google.com>
2020-11-24 22:19:31 -0800

    Add get_config() methods for mobile_bert_layers.
    PiperOrigin-RevId: 344190250

--- official/nlp/modeling/layers/mobile_bert_layers_test.py
+++ official/nlp/modeling/layers/mobile_bert_layers_test.py
@@ -51,6 +51,20 @@ class MobileBertEncoderTest(parameterized.TestCase, tf.test.TestCase):
     expected_shape = [1, 4, 16]
     self.assertListEqual(output_shape, expected_shape, msg=None)
 
+  def test_embedding_layer_get_config(self):
+    layer = mobile_bert_layers.MobileBertEmbedding(
+        word_vocab_size=16,
+        word_embed_size=32,
+        type_vocab_size=4,
+        output_embed_size=32,
+        max_sequence_length=32,
+        normalization_type='layer_norm',
+        initializer=tf.keras.initializers.TruncatedNormal(stddev=0.01),
+        dropout_rate=0.5)
+    layer_config = layer.get_config()
+    new_layer = mobile_bert_layers.MobileBertEmbedding.from_config(layer_config)
+    self.assertEqual(layer_config, new_layer.get_config())
+
   def test_no_norm(self):
     layer = mobile_bert_layers.NoNorm()
     feature = tf.random.normal([2, 3, 4])
@@ -91,6 +105,26 @@ class MobileBertEncoderTest(parameterized.TestCase, tf.test.TestCase):
     expected_shape = [2, num_attention_heads, sequence_length, sequence_length]
     self.assertListEqual(
         attention_score.shape.as_list(), expected_shape, msg=None)
+
+  def test_transformer_get_config(self):
+    layer = mobile_bert_layers.MobileBertTransformer(
+        hidden_size=32,
+        num_attention_heads=2,
+        intermediate_size=48,
+        intermediate_act_fn='gelu',
+        hidden_dropout_prob=0.5,
+        attention_probs_dropout_prob=0.4,
+        intra_bottleneck_size=64,
+        use_bottleneck_attention=True,
+        key_query_shared_bottleneck=False,
+        num_feedforward_networks=2,
+        normalization_type='layer_norm',
+        initializer=tf.keras.initializers.TruncatedNormal(stddev=0.01),
+        name='block')
+    layer_config = layer.get_config()
+    new_layer = mobile_bert_layers.MobileBertTransformer.from_config(
+        layer_config)
+    self.assertEqual(layer_config, new_layer.get_config())
 
 
 if __name__ == '__main__':
