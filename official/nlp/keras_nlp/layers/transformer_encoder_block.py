d9dd573ca02e6b4d745f47d5211973bea4d739d4
Claudio Verdú Ruiz <claverru@gmail.com>
2020-12-22 09:06:01 +0100

    Fixed headers

--- official/nlp/keras_nlp/layers/transformer_encoder_block.py
+++ official/nlp/keras_nlp/layers/transformer_encoder_block.py
@@ -1,3 +1,6 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -8,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+# ===============================================================================
 """Keras-based TransformerEncoder block layer."""
 
 import tensorflow as tf
