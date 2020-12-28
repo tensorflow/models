d062cab59e376efc18d25d838cfdb385d1a5cd73
Haitang Hu <hthu@google.com>
2020-12-28 12:06:16 -0800

    Fix documentation for AdamWeightDecayConfig.
    PiperOrigin-RevId: 349314516

--- official/modeling/optimization/configs/optimizer_config.py
+++ official/modeling/optimization/configs/optimizer_config.py
@@ -112,8 +112,10 @@ class AdamWeightDecayConfig(BaseOptimizerConfig):
     weight_decay_rate: float. Weight decay rate. Default to 0.
     include_in_weight_decay: list[str], or None. List of weight names to include
       in weight decay.
-    include_in_weight_decay: list[str], or None. List of weight names to not
+    exclude_from_weight_decay: list[str], or None. List of weight names to not
       include in weight decay.
+    gradient_clip_norm: A positive float. Clips the gradients to this maximum
+      L2-norm. Default to 1.0.
   """
   name: str = "AdamWeightDecay"
   beta_1: float = 0.9
