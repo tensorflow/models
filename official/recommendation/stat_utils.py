cca677c98adbb49dc65269270d920346039bdd70
Frederick Liu <frederickliu@google.com>
2021-03-10 09:58:37 -0800

    Internal change
    PiperOrigin-RevId: 362075728

--- official/recommendation/stat_utils.py
+++ official/recommendation/stat_utils.py
@@ -1,4 +1,4 @@
-# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,14 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+
 """Statistics utility functions of NCF."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
-import os
 
 import numpy as np
 
@@ -37,7 +35,8 @@ def permutation(args):
     args: A size two tuple that will unpacked into the size of the permutation
       and the random seed. This form is used because starmap is not universally
       available.
-  returns: A NumPy array containing a random permutation.
+  Returns:
+    A NumPy array containing a random permutation.
   """
   x, seed = args
 
