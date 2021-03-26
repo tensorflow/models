f7260d3eaa0698ec65af061732f2b4850bfb27a6
Frederick Liu <frederickliu@google.com>
2021-03-26 11:02:19 -0700

    [nlp][progressive] Revert progressive tasks.
    PiperOrigin-RevId: 365254703

--- official/nlp/configs/wmt_transformer_experiments.py
+++ official/nlp/configs/wmt_transformer_experiments.py
@@ -19,9 +19,7 @@
 from official.core import config_definitions as cfg
 from official.core import exp_factory
 from official.modeling import optimization
-from official.modeling.progressive import trainer as prog_trainer_lib
 from official.nlp.data import wmt_dataloader
-from official.nlp.tasks import progressive_translation
 from official.nlp.tasks import translation
 
 
@@ -105,68 +103,6 @@ def wmt_transformer_large() -> cfg.ExperimentConfig:
                   }
               }
           })),
-      restrictions=[
-          'task.train_data.is_training != None',
-          'task.sentencepiece_model_path != None',
-      ])
-  return config
-
-
-@exp_factory.register_config_factory('wmt_transformer/large_progressive')
-def wmt_transformer_large_progressive() -> cfg.ExperimentConfig:
-  """WMT Transformer Larger with progressive training.
-
-  Please refer to
-  tensorflow_models/official/nlp/data/train_sentencepiece.py
-  to generate sentencepiece_model
-  and pass
-  --params_override=task.sentencepiece_model_path='YOUR_PATH'
-  to the train script.
-  """
-  hidden_size = 1024
-  train_steps = 300000
-  token_batch_size = 24576
-  encdecoder = translation.EncDecoder(
-      num_attention_heads=16, intermediate_size=hidden_size * 4)
-  config = cfg.ExperimentConfig(
-      task=progressive_translation.ProgTranslationConfig(
-          model=translation.ModelConfig(
-              encoder=encdecoder,
-              decoder=encdecoder,
-              embedding_width=hidden_size,
-              padded_decode=True,
-              decode_max_length=100),
-          train_data=wmt_dataloader.WMTDataConfig(
-              tfds_name='wmt14_translate/de-en',
-              tfds_split='train',
-              src_lang='en',
-              tgt_lang='de',
-              is_training=True,
-              global_batch_size=token_batch_size,
-              static_batch=True,
-              max_seq_length=64
-          ),
-          validation_data=wmt_dataloader.WMTDataConfig(
-              tfds_name='wmt14_translate/de-en',
-              tfds_split='test',
-              src_lang='en',
-              tgt_lang='de',
-              is_training=False,
-              global_batch_size=32,
-              static_batch=True,
-              max_seq_length=100,
-          ),
-          sentencepiece_model_path=None,
-      ),
-      trainer=prog_trainer_lib.ProgressiveTrainerConfig(
-          train_steps=train_steps,
-          validation_steps=-1,
-          steps_per_loop=1000,
-          summary_interval=1000,
-          checkpoint_interval=5000,
-          validation_interval=5000,
-          optimizer_config=None,
-      ),
       restrictions=[
           'task.train_data.is_training != None',
           'task.sentencepiece_model_path != None',
