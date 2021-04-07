c5dcc0f9505593460a2bc77cffe5cd369c2b0858
Jaroslavceva, Jekatěrina <jarosjek@fel.cvut.cz>
2021-04-07 10:18:07 +0200

    Test dataset update

--- research/delf/delf/python/datasets/revisited_op/dataset.py
+++ research/delf/delf/python/datasets/revisited_op/dataset.py
@@ -18,6 +18,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import pickle
+
 import numpy as np
 from scipy.io import matlab
 import tensorflow as tf
@@ -102,14 +105,14 @@ def ParseEasyMediumHardGroundTruth(ground_truth):
   hard_ground_truth = []
   for i in range(num_queries):
     easy_ground_truth.append(
-        _ParseGroundTruth([ground_truth[i]['easy']],
-                          [ground_truth[i]['junk'], ground_truth[i]['hard']]))
+      _ParseGroundTruth([ground_truth[i]['easy']],
+                        [ground_truth[i]['junk'], ground_truth[i]['hard']]))
     medium_ground_truth.append(
-        _ParseGroundTruth([ground_truth[i]['easy'], ground_truth[i]['hard']],
-                          [ground_truth[i]['junk']]))
+      _ParseGroundTruth([ground_truth[i]['easy'], ground_truth[i]['hard']],
+                        [ground_truth[i]['junk']]))
     hard_ground_truth.append(
-        _ParseGroundTruth([ground_truth[i]['hard']],
-                          [ground_truth[i]['junk'], ground_truth[i]['easy']]))
+      _ParseGroundTruth([ground_truth[i]['hard']],
+                        [ground_truth[i]['junk'], ground_truth[i]['easy']]))
 
   return easy_ground_truth, medium_ground_truth, hard_ground_truth
 
@@ -147,7 +150,8 @@ def ComputeAveragePrecision(positive_ranks):
 
   Note that average precision computation here does NOT use the finite sum
   method (see
-  https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision)
+  https://en.wikipedia.org/wiki/Evaluation_measures_(
+  information_retrieval)#Average_precision)
   which is common in information retrieval literature. Instead, the method
   implemented here integrates over the precision-recall curve by averaging two
   adjacent precision points, then multiplying by the recall step. This is the
@@ -213,13 +217,13 @@ def ComputePRAtRanks(positive_ranks, desired_pr_ranks):
   positive_ranks_one_indexed = positive_ranks + 1
   for i, desired_pr_rank in enumerate(desired_pr_ranks):
     recalls[i] = np.sum(
-        positive_ranks_one_indexed <= desired_pr_rank) / num_expected_positives
+      positive_ranks_one_indexed <= desired_pr_rank) / num_expected_positives
 
     # If `desired_pr_rank` is larger than last positive's rank, only compute
     # precision with respect to last positive's position.
     precision_rank = min(max(positive_ranks_one_indexed), desired_pr_rank)
     precisions[i] = np.sum(
-        positive_ranks_one_indexed <= precision_rank) / precision_rank
+      positive_ranks_one_indexed <= precision_rank) / precision_rank
 
   return precisions, recalls
 
@@ -269,8 +273,8 @@ def ComputeMetrics(sorted_index_ids, ground_truth, desired_pr_ranks):
 
   if sorted_desired_pr_ranks[-1] > num_index_images:
     raise ValueError(
-        'Requested PR ranks up to %d, however there are only %d images' %
-        (sorted_desired_pr_ranks[-1], num_index_images))
+      'Requested PR ranks up to %d, however there are only %d images' %
+      (sorted_desired_pr_ranks[-1], num_index_images))
 
   # Instantiate all outputs, then loop over each query and gather metrics.
   mean_average_precision = 0.0
@@ -292,7 +296,7 @@ def ComputeMetrics(sorted_index_ids, ground_truth, desired_pr_ranks):
       continue
 
     positive_ranks = np.arange(num_index_images)[np.in1d(
-        sorted_index_ids[i], ok_index_images)]
+      sorted_index_ids[i], ok_index_images)]
     junk_ranks = np.arange(num_index_images)[np.in1d(sorted_index_ids[i],
                                                      junk_index_images)]
 
@@ -332,9 +336,9 @@ def SaveMetricsFile(mean_average_precision, mean_precisions, mean_recalls,
   with tf.io.gfile.GFile(output_path, 'w') as f:
     for k in sorted(mean_average_precision.keys()):
       f.write('{}\n  mAP={}\n  mP@k{} {}\n  mR@k{} {}\n'.format(
-          k, np.around(mean_average_precision[k] * 100, decimals=2),
-          np.array(pr_ranks), np.around(mean_precisions[k] * 100, decimals=2),
-          np.array(pr_ranks), np.around(mean_recalls[k] * 100, decimals=2)))
+        k, np.around(mean_average_precision[k] * 100, decimals=2),
+        np.array(pr_ranks), np.around(mean_precisions[k] * 100, decimals=2),
+        np.array(pr_ranks), np.around(mean_recalls[k] * 100, decimals=2)))
 
 
 def _ParseSpaceSeparatedStringsInBrackets(line, prefixes, ind):
@@ -375,8 +379,8 @@ def _ParsePrRanks(line):
     ValueError: If input line is malformed.
   """
   return [
-      int(pr_rank) for pr_rank in _ParseSpaceSeparatedStringsInBrackets(
-          line, ['  mP@k['], 0) if pr_rank
+    int(pr_rank) for pr_rank in _ParseSpaceSeparatedStringsInBrackets(
+      line, ['  mP@k['], 0) if pr_rank
   ]
 
 
@@ -394,8 +398,8 @@ def _ParsePrScores(line, num_pr_ranks):
     ValueError: If input line is malformed.
   """
   pr_scores = [
-      float(pr_score) for pr_score in _ParseSpaceSeparatedStringsInBrackets(
-          line, ('  mP@k[', '  mR@k['), 1) if pr_score
+    float(pr_score) for pr_score in _ParseSpaceSeparatedStringsInBrackets(
+      line, ('  mP@k[', '  mR@k['), 1) if pr_score
   ]
 
   if len(pr_scores) != num_pr_ranks:
@@ -427,8 +431,8 @@ def ReadMetricsFile(metrics_path):
 
   if len(file_contents_stripped) % 4:
     raise ValueError(
-        'Malformed input %s: number of lines must be a multiple of 4, '
-        'but it is %d' % (metrics_path, len(file_contents_stripped)))
+      'Malformed input %s: number of lines must be a multiple of 4, '
+      'but it is %d' % (metrics_path, len(file_contents_stripped)))
 
   mean_average_precision = {}
   pr_ranks = []
@@ -439,13 +443,13 @@ def ReadMetricsFile(metrics_path):
     protocol = file_contents_stripped[i]
     if protocol in protocols:
       raise ValueError(
-          'Malformed input %s: protocol %s is found a second time' %
-          (metrics_path, protocol))
+        'Malformed input %s: protocol %s is found a second time' %
+        (metrics_path, protocol))
     protocols.add(protocol)
 
     # Parse mAP.
     mean_average_precision[protocol] = float(
-        file_contents_stripped[i + 1].split('=')[1]) / 100.0
+      file_contents_stripped[i + 1].split('=')[1]) / 100.0
 
     # Parse (or check consistency of) pr_ranks.
     parsed_pr_ranks = _ParsePrRanks(file_contents_stripped[i + 2])
@@ -458,12 +462,83 @@ def ReadMetricsFile(metrics_path):
 
     # Parse mean precisions.
     mean_precisions[protocol] = np.array(
-        _ParsePrScores(file_contents_stripped[i + 2], len(pr_ranks)),
-        dtype=float) / 100.0
+      _ParsePrScores(file_contents_stripped[i + 2], len(pr_ranks)),
+      dtype=float) / 100.0
 
     # Parse mean recalls.
     mean_recalls[protocol] = np.array(
-        _ParsePrScores(file_contents_stripped[i + 3], len(pr_ranks)),
-        dtype=float) / 100.0
+      _ParsePrScores(file_contents_stripped[i + 3], len(pr_ranks)),
+      dtype=float) / 100.0
 
   return mean_average_precision, pr_ranks, mean_precisions, mean_recalls
+
+
+def configdataset(dataset, dir_main):
+  """Configures the paths to the test dataset files.
+
+  Args:
+    dataset: String, dataset name: either 'roxford5k' or 'rparis6k'.
+    dir_main: String, path to the folder containing ground truth files.
+
+  Returns:
+    cfg: Dataset configuration.
+
+  Raises:
+    ValueError: If an unknown dataset name is provided as an argument.
+  """
+  DATASETS = ['roxford5k', 'rparis6k']
+  dataset = dataset.lower()
+
+  if dataset not in DATASETS:
+    raise ValueError('Unknown dataset: {}!'.format(dataset))
+
+  # Loading imlist, qimlist, and gnd in configuaration as a dictionary.
+  gnd_fname = os.path.join(dir_main, 'gnd_{}.pkl'.format(dataset))
+  with tf.io.gfile.GFile(gnd_fname, 'rb') as f:
+    cfg = pickle.load(f)
+  cfg['gnd_fname'] = gnd_fname
+  if dataset == 'rparis6k':
+    dir_images = 'paris6k_images'
+  elif dataset == 'roxford5k':
+    dir_images = 'oxford5k_images'
+
+  cfg['ext'] = '.jpg'
+  cfg['qext'] = '.jpg'
+  cfg['dir_data'] = os.path.join(dir_main)
+  cfg['dir_images'] = os.path.join(cfg['dir_data'], dir_images)
+
+  cfg['n'] = len(cfg['imlist'])
+  cfg['nq'] = len(cfg['qimlist'])
+
+  cfg['im_fname'] = config_imname
+  cfg['qim_fname'] = config_qimname
+
+  cfg['dataset'] = dataset
+
+  return cfg
+
+
+def config_imname(cfg, i):
+  """Configures the path to the dataset image.
+
+  Args:
+    cfg: Configuration of the test dataset.
+    i: Integer, dataset image index.
+
+  Returns:
+    path: Path to the dataset image with the index `i`.
+  """
+  return os.path.join(cfg['dir_images'], cfg['imlist'][i] + cfg['ext'])
+
+
+def config_qimname(cfg, i):
+  """Configures the path to the query image.
+
+  Args:
+    cfg: Configuration of the test dataset.
+    i: Integer, query image index.
+
+  Returns:
+    path: Path to the query image with the index `i`.
+  """
+  return os.path.join(cfg['dir_images'], cfg['qimlist'][i] + cfg['qext'])
