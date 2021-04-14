fdb07a6b971a02bce65c08fb10c4ab854a8f1053
Mark Daoust <markdaoust@google.com>
2021-04-14 07:28:13 -0700

    Rerun npz/ tests with tflite_compatible=True

--- research/audioset/yamnet/yamnet_test.py
+++ research/audioset/yamnet/yamnet_test.py
@@ -33,29 +33,29 @@ FLAGS = flags.FLAGS
 HERE = pathlib.Path(__file__).parent
 
 class YAMNetTest(tf.test.TestCase):
-
-  _params = None
+  _params = params.Params()
   _yamnet = None
   _yamnet_classes = None
+  _atol = 1e-6
+  _rtol = 1e-6
 
   @classmethod
   def setUpClass(cls):
     super().setUpClass()
-    cls._params = params.Params()
     cls._yamnet = export.YAMNet(cls._params)
     cls._yamnet.load_weights('yamnet.h5')
     cls._yamnet_classes = yamnet.class_names('yamnet_class_map.csv')
 
   def clip_test(self, waveform, expected_class_name, top_n=10):
     """Run the model on the waveform, check that expected class is in top-n."""
-    outputs = YAMNetTest._yamnet(
+    outputs = self._yamnet(
         waveform, as_dict=True,
         returns=('predictions', 'embeddings',
                  'log_mel_spectrogram', 'logits'))
     clip_predictions = np.mean(outputs['predictions'], axis=0)
     top_n_indices = np.argsort(clip_predictions)[-top_n:]
     top_n_scores = clip_predictions[top_n_indices]
-    top_n_class_names = YAMNetTest._yamnet_classes[top_n_indices]
+    top_n_class_names = self._yamnet_classes[top_n_indices]
     top_n_predictions = list(zip(top_n_class_names, top_n_scores))
     self.assertIn(
         expected_class_name, top_n_class_names,
@@ -63,9 +63,12 @@ class YAMNetTest(tf.test.TestCase):
         expected_class_name, top_n, top_n_predictions))
     return outputs
 
+  def _name(self):
+    return self.id().replace("_","").replace('.','_')
+
   def check_close(self, outputs):
-    test_id = self.id().replace("_","").replace('.','_')
-    file_path = HERE/'npz'/test_id
+    file_path = HERE/'npz'/self._name()
+
     file_path = file_path.with_suffix(".npz")
     file_path.parent.mkdir(exist_ok=True, parents=True)
     if FLAGS.update:
@@ -76,38 +79,40 @@ class YAMNetTest(tf.test.TestCase):
         out_value = outputs[key]
         ref_value = reference[key]
         self.assertAllClose(out_value, ref_value,
+                            atol=self._atol, rtol=self._rtol,
                             msg=f"`{key}` didn't match")
 
   def testZeros(self):
     outputs = self.clip_test(
-        waveform=np.zeros((int(3 * YAMNetTest._params.sample_rate),)),
+        waveform=np.zeros((int(3 * self._params.sample_rate),)),
         expected_class_name='Silence')
+
     self.check_close(outputs)
 
   def testRandom(self):
     np.random.seed(51773)  # Ensure repeatability.
     outputs = self.clip_test(
         waveform=np.random.uniform(-1.0, +1.0,
-                                   (int(3 * YAMNetTest._params.sample_rate),)),
+                                   (int(3 * self._params.sample_rate),)),
         expected_class_name='White noise')
     self.check_close(outputs)
 
   def testSine(self):
     outputs = self.clip_test(
         waveform=np.sin(2 * np.pi * 440 *
-                        np.arange(0, 3, 1 / YAMNetTest._params.sample_rate)),
+                        np.arange(0, 3, 1 / self._params.sample_rate)),
         expected_class_name='Sine wave')
     self.check_close(outputs)
 
   def testBatchOne(self):
     sine_wave = np.sin(2 * np.pi * 440 *
-                        np.arange(0, 3, 1 / YAMNetTest._params.sample_rate))
+                        np.arange(0, 3, 1 / self._params.sample_rate))
     sine_outputs = self.clip_test(
         waveform=sine_wave, expected_class_name='Sine wave')
 
 
     batch_one = sine_wave[None, ...]
-    batch_one_outputs = YAMNetTest._yamnet(
+    batch_one_outputs = self._yamnet(
         waveform=batch_one, as_dict=True,
         returns=('predictions', 'embeddings',
                  'log_mel_spectrogram', 'logits'))
@@ -118,13 +123,13 @@ class YAMNetTest(tf.test.TestCase):
 
   def testBatchReplicate(self):
     sine_wave = np.sin(2 * np.pi * 440 *
-                        np.arange(0, 3, 1 / YAMNetTest._params.sample_rate))
+                        np.arange(0, 3, 1 / self._params.sample_rate))
     sine_outputs = self.clip_test(
         waveform=sine_wave, expected_class_name='Sine wave')
 
 
     batch_one = np.stack([sine_wave, sine_wave, sine_wave])
-    batch_one_outputs = YAMNetTest._yamnet(
+    batch_one_outputs = self._yamnet(
         waveform=batch_one, as_dict=True,
         returns=('predictions', 'embeddings',
                  'log_mel_spectrogram', 'logits'))
@@ -139,18 +144,18 @@ class YAMNetTest(tf.test.TestCase):
 
   def testBatch(self):
     sine_wave = np.sin(2 * np.pi * 440 *
-                        np.arange(0, 3, 1 / YAMNetTest._params.sample_rate))
+                        np.arange(0, 3, 1 / self._params.sample_rate))
     sine_outputs = self.clip_test(
         waveform=sine_wave, expected_class_name='Sine wave')
 
     random_wave = np.random.uniform(-1.0, +1.0,
-                                   (int(3 * YAMNetTest._params.sample_rate),))
+                                   (int(3 * self._params.sample_rate),))
     random_outputs = self.clip_test(
         waveform=random_wave,
         expected_class_name='White noise')
 
     stacked = np.stack([sine_wave, random_wave], axis=0)
-    stacked_output = YAMNetTest._yamnet(
+    stacked_output = self._yamnet(
         waveform=stacked, as_dict=True,
         returns=('predictions', 'embeddings',
                  'log_mel_spectrogram', 'logits'))
@@ -160,6 +165,23 @@ class YAMNetTest(tf.test.TestCase):
                           msg=f"sine_outputs[{name}] didn't match")
       self.assertAllClose(value[1], random_outputs[name],
                           msg=f"random_outputs[{name}] didn't match")
+
+class YAMNetLiteTest(YAMNetTest):
+  # Same tests with the TFLite model. Use the files generated by the base class.
+
+  _params = params.Params(tflite_compatible=True)
+  _atol = 0.05
+  _rtol = 0.01
+
+
+  def _name(self):
+    return super()._name().replace('YAMNetLite', 'YAMNet')
+
+  def check_close(self, outputs):
+    # Compare to the saved lite outputs
+    if not FLAGS.update:
+      super().check_close(outputs)
+
 
 if __name__ == '__main__':
   tf.test.main()
