fa9c975f151f9cae4c87df52c0f61a0f1cbdc401
Abdullah Rashwan <arashwan@google.com>
2021-04-28 11:45:22 -0700

    Internal change
    PiperOrigin-RevId: 370951507

--- official/vision/beta/configs/image_classification.py
+++ official/vision/beta/configs/image_classification.py
@@ -16,13 +16,15 @@
 """Image classification configuration definition."""
 import os
 from typing import List, Optional
+
 import dataclasses
+
 from official.core import config_definitions as cfg
 from official.core import exp_factory
 from official.modeling import hyperparams
 from official.modeling import optimization
-from official.vision.beta.configs import backbones
 from official.vision.beta.configs import common
+from official.vision.beta.configs import backbones
 
 
 @dataclasses.dataclass
