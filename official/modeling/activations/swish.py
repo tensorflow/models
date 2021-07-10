8f3e9098966146aa5b9a605f69b907e7d0766202
apidwalin <84679338+Apidwalin@users.noreply.github.com>
2021-07-10 22:35:28 +0700

    Add files via upload

--- official/modeling/activations/swish.py
+++ official/modeling/activations/swish.py
@@ -14,6 +14,10 @@
 # ==============================================================================
 """Customized Swish activation."""
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import tensorflow as tf
 
 
