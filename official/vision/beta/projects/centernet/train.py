331ea0de247f8bd3dc6fc4750bd1baf5119a453c
Shixin Luo <luoshixin@google.com>
2021-07-28 15:21:40 +0800

    fix registration bug

--- official/vision/beta/projects/centernet/train.py
+++ official/vision/beta/projects/centernet/train.py
@@ -25,7 +25,7 @@ from official.core import task_factory
 from official.core import train_lib
 from official.core import train_utils
 from official.modeling import performance
-from official.vision.beta.projects.yolo.common import registry_imports  # pylint: disable=unused-import
+from official.vision.beta.projects.centernet.common import registry_imports  # pylint: disable=unused-import
 
 FLAGS = flags.FLAGS
 
