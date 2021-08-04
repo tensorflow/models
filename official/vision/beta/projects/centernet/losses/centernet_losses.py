822916a975ce3f5d2372258923b23e9a39c0a8e3
Shixin Luo <luoshixin@google.com>
2021-08-04 14:38:32 +0800

    assign a default weights for L1LLoss

--- official/vision/beta/projects/centernet/losses/centernet_losses.py
+++ official/vision/beta/projects/centernet/losses/centernet_losses.py
@@ -23,6 +23,50 @@ import tensorflow as tf
 from tensorflow.python.keras.utils import losses_utils
 
 
+def absolute_difference(
+    labels,
+    predictions,
+    sample_weight=None,
+    reduction=tf.keras.losses.Reduction.NONE):
+  """Adds an Absolute Difference loss to the training procedure.
+  `weights` acts as a coefficient for the loss. If a scalar is provided, then
+  the loss is simply scaled by the given value. If `weights` is a `Tensor` of
+  shape `[batch_size]`, then the total loss for each sample of the batch is
+  rescaled by the corresponding element in the `weights` vector. If the shape
+  of `weights` matches the shape of `predictions`, then the loss of each
+  measurable element of `predictions` is scaled by the corresponding value of
+  `weights`.
+  Args:
+    labels: The ground truth output tensor, same dimensions as 'predictions'.
+    predictions: The predicted outputs.
+    sample_weight: Optional `Tensor` whose rank is either 0, or the same rank as
+      `labels`, and must be broadcastable to `labels` (i.e., all dimensions
+      must be either `1`, or the same as the corresponding `losses`
+      dimension).
+    reduction: Type of reduction to apply to loss.
+  Returns:
+    Weighted loss float `Tensor`. If `reduction` is `NONE`, this has the same
+    shape as `labels`; otherwise, it is scalar.
+  Raises:
+    ValueError: If the shape of `predictions` doesn't match that of
+      `labels` or if the shape of `weights` is invalid or if `labels`
+      or `predictions` is None.
+  """
+  if labels is None:
+    raise ValueError("labels must not be None.")
+  if predictions is None:
+    raise ValueError("predictions must not be None.")
+  with tf.name_scope("absolute_difference"):
+    predictions = tf.cast(predictions, dtype=tf.float32)
+    labels = tf.cast(labels, dtype=tf.float32)
+    predictions.get_shape().assert_is_compatible_with(labels.get_shape())
+    losses = tf.abs(tf.subtract(predictions, labels))
+    return losses_utils.compute_weighted_loss(
+        losses=losses,
+        sample_weight=sample_weight,
+        reduction=reduction)
+
+
 class Loss(six.with_metaclass(abc.ABCMeta, object)):
   """Abstract base class for loss functions."""
   
@@ -96,50 +140,6 @@ class Loss(six.with_metaclass(abc.ABCMeta, object)):
     pass
 
 
-def absolute_difference(
-    labels,
-    predictions,
-    sample_weight=None,
-    reduction=tf.keras.losses.Reduction.NONE):
-  """Adds an Absolute Difference loss to the training procedure.
-  `weights` acts as a coefficient for the loss. If a scalar is provided, then
-  the loss is simply scaled by the given value. If `weights` is a `Tensor` of
-  shape `[batch_size]`, then the total loss for each sample of the batch is
-  rescaled by the corresponding element in the `weights` vector. If the shape
-  of `weights` matches the shape of `predictions`, then the loss of each
-  measurable element of `predictions` is scaled by the corresponding value of
-  `weights`.
-  Args:
-    labels: The ground truth output tensor, same dimensions as 'predictions'.
-    predictions: The predicted outputs.
-    sample_weight: Optional `Tensor` whose rank is either 0, or the same rank as
-      `labels`, and must be broadcastable to `labels` (i.e., all dimensions
-      must be either `1`, or the same as the corresponding `losses`
-      dimension).
-    reduction: Type of reduction to apply to loss.
-  Returns:
-    Weighted loss float `Tensor`. If `reduction` is `NONE`, this has the same
-    shape as `labels`; otherwise, it is scalar.
-  Raises:
-    ValueError: If the shape of `predictions` doesn't match that of
-      `labels` or if the shape of `weights` is invalid or if `labels`
-      or `predictions` is None.
-  """
-  if labels is None:
-    raise ValueError("labels must not be None.")
-  if predictions is None:
-    raise ValueError("predictions must not be None.")
-  with tf.name_scope("absolute_difference"):
-    predictions = tf.cast(predictions, dtype=tf.float32)
-    labels = tf.cast(labels, dtype=tf.float32)
-    predictions.get_shape().assert_is_compatible_with(labels.get_shape())
-    losses = tf.abs(tf.subtract(predictions, labels))
-    return losses_utils.compute_weighted_loss(
-        losses=losses,
-        sample_weight=sample_weight,
-        reduction=reduction)
-
-
 class PenaltyReducedLogisticFocalLoss(Loss):
   """Penalty-reduced pixelwise logistic regression with focal loss.
 
@@ -210,7 +210,7 @@ class L1LocalizationLoss(Loss):
   When used in a per-pixel manner, each pixel should be given as an anchor.
   """
   
-  def _compute_loss(self, prediction_tensor, target_tensor, weights):
+  def _compute_loss(self, prediction_tensor, target_tensor, weights=1.0):
     """Compute loss function.
 
     Args:
