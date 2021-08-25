90156e0d6d1d7713597b0b38772d3eff4ce4257f
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-08-25 15:08:45 +0530

    added __init__.py for tasks; fixed imports

--- official/vision/beta/projects/panoptic_maskrcnn/__init__.py
+++ official/vision/beta/projects/panoptic_maskrcnn/__init__.py
@@ -25,3 +25,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from official.vision.beta.projects.panoptic_maskrcnn import configs
+from official.vision.beta.projects.panoptic_maskrcnn import tasks
