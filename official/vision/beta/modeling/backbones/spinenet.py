889708373c44d293fa4497432ae1a459923a9b03
A. Unique TensorFlower <gardener@tensorflow.org>
2021-09-17 11:59:31 -0700

    Copy SpineNet-143 from Cloud TPU into TF-Vision.
    PiperOrigin-RevId: 397369633

--- official/vision/beta/modeling/backbones/spinenet.py
+++ official/vision/beta/modeling/backbones/spinenet.py
@@ -87,6 +87,13 @@ SCALING_MAP = {
         'resample_alpha': 1.0,
         'block_repeats': 3,
     },
+    # SpineNet-143 with 1.3x filter_size_scale.
+    '143L': {
+        'endpoints_num_filters': 256,
+        'filter_size_scale': 1.3,
+        'resample_alpha': 1.0,
+        'block_repeats': 3,
+    },
     '190': {
         'endpoints_num_filters': 512,
         'filter_size_scale': 1.3,
