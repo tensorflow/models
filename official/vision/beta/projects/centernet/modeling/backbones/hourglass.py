e1568f6e254334d17c029a363da5b25bbd2f1466
Shixin Luo <luoshixin@google.com>
2021-10-01 15:15:51 +0800

    fix a type hint

--- official/vision/beta/projects/centernet/modeling/backbones/hourglass.py
+++ official/vision/beta/projects/centernet/modeling/backbones/hourglass.py
@@ -244,7 +244,7 @@ def build_hourglass(
     input_specs: tf.keras.layers.InputSpec,
     backbone_config: hyperparams.Config,
     norm_activation_config: hyperparams.Config,
-    l2_regularizer: tf.keras.regularizers.Regularizer = None) -> tf.keras.Model:
+    l2_regularizer: Optional[tf.keras.regularizers.Regularizer] = None) -> tf.keras.Model:
   """Builds Hourglass backbone from a config."""
   backbone_type = backbone_config.type
   backbone_cfg = backbone_config.get()
