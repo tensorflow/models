a3bb60f868b4f194a5917909fec6d42069700796
jacob-zietek <xoqcpn@gmail.com>
2021-10-15 06:30:11 -0400

    WIP Completed backbone of fcn create_tf_example, still needs work to reorganize different categories and make sure evertything is compatable with convert_to_feature

--- official/vision/beta/projects/mesh_rcnn/data/create_pix3d_tf_record.py
+++ official/vision/beta/projects/mesh_rcnn/data/create_pix3d_tf_record.py
@@ -30,6 +30,7 @@ import json
 import logging
 import os
 import json
+from re import I
 
 from absl import app  # pylint:disable=unused-import
 from absl import flags
@@ -67,21 +68,65 @@ def create_tf_example(image):
       of the Pix3D folder is incorrect.
     """
 
-    
-
     with tf.io.gfile.GFile(os.join(image["pix3d_dir"], image["img"]), 'rb') as fid:
-      encoded_jpg = fid.read()
+      encoded_img_jpg = fid.read()
 
     img_width, img_height = image["img_size"]
     img_filename = image["img"]
     img_category = image["category"]
-    
-    feature_dict = {"img/height": dataset_util.int64_feature(img_height),
-                    "img/width": dataset_util.int64_feature(img_width),
-                    "img/category": dataset_util.bytes_feature(img_category),
-                    "img/filename": dataset_util.bytes_feature(img_filename),
-                    "img/encoded": dataset_util.bytes_feature(encoded_jpg)}
+    keypoints_2d = image["2d_keypoints"]
+
+    feature_dict = {"img/height": convert_to_feature(img_height),
+                    "img/width": convert_to_feature(img_width),
+                    "img/category": convert_to_feature(img_category),
+                    "img/filename": convert_to_feature(img_filename),
+                    "img/encoded": convert_to_feature(encoded_img_jpg),
+                    "img/2d_keypoints": convert_to_feature(keypoints_2d)}
+
+    with tf.io.gfile.GFile(os.join(image["pix3d_dir"], image["mask"]), 'rb') as fid:
+      encoded_mask_jpg = fid.read()
+
+    feature_dict.update({"mask": convert_to_feature(encoded_mask_jpg)})
+
+    with tf.io.gfile.GFile(os.join(image["pix3d_dir"], image["model"]), 'rb') as fid:
+      encoded_model = fid.read()
   
+    with tf.io.gfile.GFile(os.join(image["pix3d_dir"], image["3d_keypoints"]), 'rb') as fid:
+      keypoints_3d = fid.read()
+
+    model_raw = image["model_raw"]
+    model_source = image["model_source"]
+
+    feature_dict.update({"model": convert_to_feature(encoded_model),
+                         "model/raw": convert_to_feature(model_raw),
+                         "model/source": convert_to_feature(model_source),
+                         "model/3d_keypoints": convert_to_feature(keypoints_3d)})
+
+    with tf.io.gfile.GFile(os.join(image["pix3d_dir"], image["voxel"]), 'rb') as fid:
+      encoded_voxel = fid.read()
+
+    rot_mat = image["rot_mat"]
+    trans_mat = image["trans_mat"]
+    focal_length = image["focal_length"]
+    cam_position = image["cam_positon"]
+    inplane_rotation = image["inplane_rotation"]
+    truncated = image["truncated"]
+    occluded = image["occluded"]
+    slightly_occluded = image["slightly_occluded"]
+    bbox = image["bbox"]
+
+    # Where are these supposed to be categorized???
+    feature_dict.update({"voxel": convert_to_feature(encoded_voxel),
+                         "rot_mat": convert_to_feature(rot_mat),
+                         "trans_mat": convert_to_feature(trans_mat),
+                         "focal_length": convert_to_feature(focal_length),
+                         "cam_position": convert_to_feature(cam_position),
+                         "inplane_rotation": convert_to_feature(inplane_rotation),
+                         "truncated": convert_to_feature(truncated),
+                         "occluded": convert_to_feature(occluded),
+                         "slightly_occluded": convert_to_feature(slightly_occluded),
+                         "bbox": convert_to_feature(bbox)})
+
     example = tf.train.Example(features=tf.train.Features(feature=feature_dict))
 
     return example, 0
