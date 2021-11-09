f3897b5512025c32796b6aba7df00aaa499efd53
jacob-zietek <xoqcpn@gmail.com>
2021-11-09 04:12:42 -0500

    Beautifying

--- official/vision/beta/projects/mesh_rcnn/data/create_pix3d_tf_record.py
+++ official/vision/beta/projects/mesh_rcnn/data/create_pix3d_tf_record.py
@@ -40,71 +40,77 @@ import multiprocessing as mp
 
 #from official.vision.beta.data import tfrecord_lib
 #from research.object_detection.utils import dataset_util
-#^pythonpath is messed up ^ cant import so copied function
+# ^pythonpath is messed up ^ cant import so copied function
+
+
 def write_tf_record_dataset(output_path, annotation_iterator,
                             process_func, num_shards,
                             use_multiprocessing=True, unpack_arguments=True):
-  """Iterates over annotations, processes them and writes into TFRecords.
+    """Iterates over annotations, processes them and writes into TFRecords.
 
-  Args:
-    output_path: The prefix path to create TF record files.
-    annotation_iterator: An iterator of tuples containing details about the
-      dataset.
-    process_func: A function which takes the elements from the tuples of
-      annotation_iterator as arguments and returns a tuple of (tf.train.Example,
-      int). The integer indicates the number of annotations that were skipped.
-    num_shards: int, the number of shards to write for the dataset.
-    use_multiprocessing:
-      Whether or not to use multiple processes to write TF Records.
-    unpack_arguments:
-      Whether to unpack the tuples from annotation_iterator as individual
-        arguments to the process func or to pass the returned value as it is.
+    Args:
+      output_path: The prefix path to create TF record files.
+      annotation_iterator: An iterator of tuples containing details about the
+        dataset.
+      process_func: A function which takes the elements from the tuples of
+        annotation_iterator as arguments and returns a tuple of (tf.train.Example,
+        int). The integer indicates the number of annotations that were skipped.
+      num_shards: int, the number of shards to write for the dataset.
+      use_multiprocessing:
+        Whether or not to use multiple processes to write TF Records.
+      unpack_arguments:
+        Whether to unpack the tuples from annotation_iterator as individual
+          arguments to the process func or to pass the returned value as it is.
 
-  Returns:
-    num_skipped: The total number of skipped annotations.
-  """
+    Returns:
+      num_skipped: The total number of skipped annotations.
+    """
 
-  writers = [
-      tf.io.TFRecordWriter(
-          output_path + '-%05d-of-%05d.tfrecord' % (i, num_shards))
-      for i in range(num_shards)
-  ]
+    writers = [
+        tf.io.TFRecordWriter(
+            output_path + '-%05d-of-%05d.tfrecord' % (i, num_shards))
+        for i in range(num_shards)
+    ]
 
-  total_num_annotations_skipped = 0
+    total_num_annotations_skipped = 0
 
-  if use_multiprocessing:
-    pool = mp.Pool()
-    if unpack_arguments:
-      tf_example_iterator = pool.starmap(process_func, annotation_iterator)
+    if use_multiprocessing:
+        pool = mp.Pool()
+        if unpack_arguments:
+            tf_example_iterator = pool.starmap(
+                process_func, annotation_iterator)
+        else:
+            tf_example_iterator = pool.imap(process_func, annotation_iterator)
     else:
-      tf_example_iterator = pool.imap(process_func, annotation_iterator)
-  else:
-    if unpack_arguments:
-      tf_example_iterator = itertools.starmap(process_func, annotation_iterator)
-    else:
-      tf_example_iterator = map(process_func, annotation_iterator)
+        if unpack_arguments:
+            tf_example_iterator = itertools.starmap(
+                process_func, annotation_iterator)
+        else:
+            tf_example_iterator = map(process_func, annotation_iterator)
 
-  for idx, (tf_example, num_annotations_skipped) in enumerate(
-      tf_example_iterator):
-    if idx % 100 == 0:
-      logging.info('On image %d', idx)
+    for idx, (tf_example, num_annotations_skipped) in enumerate(
+            tf_example_iterator):
+        if idx % 100 == 0:
+            logging.info('On image %d', idx)
 
-    total_num_annotations_skipped += num_annotations_skipped
-    writers[idx % num_shards].write(tf_example.SerializeToString())
+        total_num_annotations_skipped += num_annotations_skipped
+        writers[idx % num_shards].write(tf_example.SerializeToString())
 
-  if use_multiprocessing:
-    pool.close()
-    pool.join()
+    if use_multiprocessing:
+        pool.close()
+        pool.join()
 
-  for writer in writers:
-    writer.close()
+    for writer in writers:
+        writer.close()
 
-  logging.info('Finished writing, skipped %d annotations.',
-               total_num_annotations_skipped)
-  return total_num_annotations_skipped
+    logging.info('Finished writing, skipped %d annotations.',
+                 total_num_annotations_skipped)
+    return total_num_annotations_skipped
 
 #from official.vision.beta.data.tfrecord_lib import convert_to_feature
 # ^pythonpath is messed up ^ cant import so copied function
+
+
 def convert_to_feature(value, value_type=None):
     """Converts the given python object to a tf.train.Feature.
 
@@ -117,57 +123,57 @@ def convert_to_feature(value, value_type=None):
     """
 
     if value_type is None:
- 
-      element = value[0] if isinstance(value, list) else value
 
-      if isinstance(element, bytes):
-        value_type = 'bytes'
+        element = value[0] if isinstance(value, list) else value
 
-      elif isinstance(element, (int, np.integer)):
-        value_type = 'int64'
+        if isinstance(element, bytes):
+            value_type = 'bytes'
 
-      elif isinstance(element, (float, np.floating)):
-        value_type = 'float'
+        elif isinstance(element, (int, np.integer)):
+            value_type = 'int64'
 
-      elif isinstance(element, str):
-        value_type = "str"
+        elif isinstance(element, (float, np.floating)):
+            value_type = 'float'
 
-      elif isinstance(element, list) and isinstance(value, list):
-        value_type = "2d"
-      
-      elif element is None:
-        value_type = "none"
+        elif isinstance(element, str):
+            value_type = "str"
 
-      else:
-        raise ValueError('Cannot convert type {} to feature'.
-                       format(type(element)))
+        elif isinstance(element, list) and isinstance(value, list):
+            value_type = "2d"
 
-      if isinstance(value, list):
-        value_type = value_type + '_list'
+        elif element is None:
+            value_type = "none"
+
+        else:
+            raise ValueError('Cannot convert type {} to feature'.
+                             format(type(element)))
+
+        if isinstance(value, list):
+            value_type = value_type + '_list'
 
     if value_type == 'int64':
-      return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+        return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
     elif value_type == 'int64_list':
-      value = np.asarray(value).astype(np.int64).reshape(-1)
-      return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+        value = np.asarray(value).astype(np.int64).reshape(-1)
+        return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
     elif value_type == 'float':
-      return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+        return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
 
     elif value_type == 'float_list':
-      value = np.asarray(value).astype(np.float32).reshape(-1)
-      return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+        value = np.asarray(value).astype(np.float32).reshape(-1)
+        return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
     elif value_type == 'bytes':
-      return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
     elif value_type == 'bytes_list':
-      return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
-    
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
     elif value_type == "str":
         return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value.encode("utf-8")]))
-    
+
     elif value_type == "2d_list":
         data = tf.convert_to_tensor(value)
         serialized_data = tf.io.serialize_tensor(data)
@@ -178,12 +184,15 @@ def convert_to_feature(value, value_type=None):
         return tf.train.Feature()
 
     else:
-      raise ValueError('Unknown value_type parameter - {}'.format(value_type))
+        raise ValueError(
+            'Unknown value_type parameter - {}'.format(value_type))
+
 
 flags.DEFINE_multi_string('pix3d_dir', '', 'Directory containing Pix3d.')
 flags.DEFINE_string('output_file_prefix', '/tmp/train', 'Path to output file')
 flags.DEFINE_integer('num_shards', 32, 'Number of shards for output file.')
-flags.DEFINE_multi_string("pix3d_json_file", "pix3d.json", "Json file containing all pix3d info")
+flags.DEFINE_multi_string("pix3d_json_file", "pix3d.json",
+                          "Json file containing all pix3d info")
 
 FLAGS = flags.FLAGS
 
@@ -212,7 +221,6 @@ def create_tf_example(image):
     #image_arr = np.array(Image.open(os.path.join(image["pix3d_dir"], image["img"])).getdata()).tolist()
     #image_arr = cv2.imread(os.path.join(image["pix3d_dir"], image["img"]), -1).tolist()
 
-
     img_width, img_height = image["img_size"]
     img_filename = image["img"]
     img_category = image["category"]
@@ -233,7 +241,8 @@ def create_tf_example(image):
 
     feature_dict.update({"mask": convert_to_feature(encoded_mask)})
 
-    model_vertices, model_faces = parse_obj_file(os.path.join(image["pix3d_dir"], image["model"]))
+    model_vertices, model_faces = parse_obj_file(
+        os.path.join(image["pix3d_dir"], image["model"]))
 
     with tf.io.gfile.GFile(os.path.join(image["pix3d_dir"], image["3d_keypoints"]), 'rb') as fid:
         keypoints_3d = fid.read()
@@ -278,6 +287,7 @@ def create_tf_example(image):
 
     return example, 0
 
+
 def parse_mat_file(file):
     """
     Parses relevant data out of a .mat file. This contains all of the voxel information.
@@ -290,6 +300,7 @@ def parse_mat_file(file):
         return sio.loadmat(file)["voxel"].tolist()
     except:
         return [[[]]]
+
 
 def parse_obj_file(file):
     """
@@ -308,7 +319,7 @@ def parse_obj_file(file):
 
     for line in lines:
         lineID = line[0:2]
-        
+
         if lineID == "v ":
             vertex = line[2:].split(" ")
             for i, v in enumerate(vertex):
@@ -323,6 +334,7 @@ def parse_obj_file(file):
 
     return vertices, faces
 
+
 def generate_annotations(images, pix3d_dir):
     """Generator for Pix3D annotations."""
 
@@ -330,11 +342,11 @@ def generate_annotations(images, pix3d_dir):
 
     for image in images:
         annotations.append({"img": image["img"], "category": image["category"], "img_size": image["img_size"], "2d_keypoints": image["2d_keypoints"],
-               "mask": image["mask"], "img_source": image["img_source"], "model": image["model"], "model_raw": image["model_raw"],
-               "model_source": image["model_source"], "3d_keypoints": image["3d_keypoints"], "voxel": image["voxel"], "rot_mat": image["rot_mat"],
-               "trans_mat": image["trans_mat"], "focal_length": image["focal_length"], "cam_position": image["cam_position"],
-               "inplane_rotation": image["inplane_rotation"], "truncated": image["truncated"], "occluded": image["occluded"],
-               "slightly_occluded": image["slightly_occluded"], "bbox": image["bbox"], "pix3d_dir": pix3d_dir})
+                            "mask": image["mask"], "img_source": image["img_source"], "model": image["model"], "model_raw": image["model_raw"],
+                            "model_source": image["model_source"], "3d_keypoints": image["3d_keypoints"], "voxel": image["voxel"], "rot_mat": image["rot_mat"],
+                            "trans_mat": image["trans_mat"], "focal_length": image["focal_length"], "cam_position": image["cam_position"],
+                            "inplane_rotation": image["inplane_rotation"], "truncated": image["truncated"], "occluded": image["occluded"],
+                            "slightly_occluded": image["slightly_occluded"], "bbox": image["bbox"], "pix3d_dir": pix3d_dir})
 
     return annotations
 
@@ -357,7 +369,8 @@ def _create_tf_record_from_pix3d_dir(pix3d_dir,
     pix3d_annotations_iter = generate_annotations(
         images=images, pix3d_dir=pix3d_dir)
 
-    num_skipped = write_tf_record_dataset(output_path, pix3d_annotations_iter, create_tf_example, num_shards, unpack_arguments=False, use_multiprocessing=True)
+    num_skipped = write_tf_record_dataset(
+        output_path, pix3d_annotations_iter, create_tf_example, num_shards, unpack_arguments=False, use_multiprocessing=True)
 
     logging.info('Finished writing, skipped %d annotations.', num_skipped)
 
@@ -369,8 +382,9 @@ def main(_):
     if not tf.io.gfile.isdir(directory):
         tf.io.gfile.makedirs(directory)
 
-    _create_tf_record_from_pix3d_dir(FLAGS.pix3d_dir[0], FLAGS.output_file_prefix, FLAGS.num_shards, FLAGS.pix3d_json_file[0])
+    _create_tf_record_from_pix3d_dir(
+        FLAGS.pix3d_dir[0], FLAGS.output_file_prefix, FLAGS.num_shards, FLAGS.pix3d_json_file[0])
 
-    
+
 if __name__ == '__main__':
     app.run(main)
--- official/vision/beta/projects/mesh_rcnn/data/pix3d_visualizer.py
+++ official/vision/beta/projects/mesh_rcnn/data/pix3d_visualizer.py
@@ -31,8 +31,10 @@ import cv2
 import tensorflow as tf
 
 flags.DEFINE_multi_string('pix3d_dir', '', 'Directory containing Pix3d.')
-flags.DEFINE_string('output_file_prefix', '/tmp/output', 'Path to output files')
-flags.DEFINE_integer('num_models', 2, 'Number of models rebuilt from TFRecord.')
+flags.DEFINE_string('output_file_prefix', '/tmp/output',
+                    'Path to output files')
+flags.DEFINE_integer(
+    'num_models', 2, 'Number of models rebuilt from TFRecord.')
 
 FLAGS = flags.FLAGS
 
@@ -51,16 +53,18 @@ def write_obj_file(vertices,
     """
     logging.info(f"Logging file {filename}")
     with open(filename, 'w') as f:
+        
         for vertex in vertices:
             print("v " + ' '.join(map(str, vertex)), file=f)
 
         for face in faces:
             ret = "f "
-            
+
             for coordinate in face:
                 ret += str(coordinate[0]) + " "
 
             print(ret, file=f)
+
 
 def write_masked_image(mask,
                        image,
@@ -74,14 +78,14 @@ def write_masked_image(mask,
     logging.info(f"Logging file {filename}")
     dim1 = len(mask)
     dim2 = len(mask[0])
-    
-    for i in range (dim1):
-        for j in range (dim2):
-            if mask[i][j] > 0: # add green mask to image
+
+    for i in range(dim1):
+        for j in range(dim2):
+            if mask[i][j] > 0:  # add green mask to image
                 image[i][j][1] += 50
 
     cv2.imwrite(filename, np.array(image))
-    
+
 
 def visualize_tf_record(pix3d_dir,
                         output_path,
@@ -93,31 +97,38 @@ def visualize_tf_record(pix3d_dir,
       num_models: Number of output files to create.
     """
 
-    logging.info(f"Starting to visualize {num_models} models from the TFRecords in directory {pix3d_dir} into {output_path}.")
+    logging.info(
+        f"Starting to visualize {num_models} models from the TFRecords in directory {pix3d_dir} into {output_path}.")
 
     filenames = [os.path.join(pix3d_dir, x) for x in os.listdir(pix3d_dir)]
-    
+
     raw_dataset = tf.data.TFRecordDataset(filenames)
-    
+
     for raw_record in raw_dataset.take(num_models):
         example = tf.train.Example()
         example.ParseFromString(raw_record.numpy())
         features = example.features.feature
 
-        vertices = tf.io.parse_tensor(features["model/vertices"].bytes_list.value[0], tf.float32).numpy().tolist()
-        faces = tf.io.parse_tensor(features["model/faces"].bytes_list.value[0], tf.int32).numpy().tolist()
-        mask = cv2.imdecode(np.fromstring(features["mask"].bytes_list.value[0], np.uint8), cv2.IMREAD_GRAYSCALE).tolist()
-        image = cv2.imdecode(np.fromstring(features["img/encoded"].bytes_list.value[0], np.uint8), flags=1).tolist()
-        
+        vertices = tf.io.parse_tensor(
+            features["model/vertices"].bytes_list.value[0], tf.float32).numpy().tolist()
+        faces = tf.io.parse_tensor(
+            features["model/faces"].bytes_list.value[0], tf.int32).numpy().tolist()
+        mask = cv2.imdecode(np.fromstring(
+            features["mask"].bytes_list.value[0], np.uint8), cv2.IMREAD_GRAYSCALE).tolist()
+        image = cv2.imdecode(np.fromstring(
+            features["img/encoded"].bytes_list.value[0], np.uint8), flags=1).tolist()
+
         #mask = tf.io.parse_tensor(features["mask"].bytes_list.value[0], tf.int32).numpy().tolist()
         #image = tf.io.parse_tensor(features["img/encoded"].bytes_list.value[0], tf.int32).numpy().tolist()
 
-        filename = str(features["img/filename"].bytes_list.value[0]).split("/")[2][:-1]
+        filename = str(
+            features["img/filename"].bytes_list.value[0]).split("/")[2][:-1]
         filename = filename.split(".")[0]
         filename = os.path.join(output_path, filename)
 
         write_obj_file(vertices, faces, filename + ".obj")
         write_masked_image(mask, image, filename + ".png")
+
 
 def main(_):
     assert FLAGS.pix3d_dir, '`pix3d_dir` missing.'
@@ -126,8 +137,9 @@ def main(_):
     if not tf.io.gfile.isdir(directory):
         tf.io.gfile.makedirs(directory)
 
-    visualize_tf_record(FLAGS.pix3d_dir[0], FLAGS.output_file_prefix, FLAGS.num_models)
+    visualize_tf_record(
+        FLAGS.pix3d_dir[0], FLAGS.output_file_prefix, FLAGS.num_models)
 
-    
+
 if __name__ == '__main__':
     app.run(main)
