d01f8e56303d75da8473c7ac1da45f482a838022
Tyan3001 <60082495+Tyan3001@users.noreply.github.com>
2021-11-30 17:39:57 -0500

    yolo serving module

--- official/vision/beta/serving/export_module_factory.py
+++ official/vision/beta/serving/export_module_factory.py
@@ -25,6 +25,8 @@ from official.vision.beta.modeling import factory
 from official.vision.beta.serving import export_base_v2 as export_base
 from official.vision.beta.serving import export_utils
 from official.vision.beta.projects.yolo.modeling import factory as yolo_factory
+from official.vision.beta.projects.yolo import configs as yolo_config
+from official.vision.beta.projects.yolo.serving import model_fn as yolo_model_fn
 
 
 def create_classification_export_module(params: cfg.ExperimentConfig,
@@ -32,7 +34,7 @@ def create_classification_export_module(params: cfg.ExperimentConfig,
                                         batch_size: int,
                                         input_image_size: List[int],
                                         num_channels: int = 3):
-  """Creats classification export module."""
+  """Creates classification export module."""
   input_signature = export_utils.get_image_input_signatures(
       input_type, batch_size, input_image_size, num_channels)
   input_specs = tf.keras.layers.InputSpec(
@@ -73,72 +75,73 @@ def create_classification_export_module(params: cfg.ExperimentConfig,
                                            postprocessor=postprocess_fn)
   return export_module
 
-def create_yolo_export_module(params,
-                              input_type,
-                              batch_size,
-                              input_image_size,
-                              num_channels):
-    input_signature = export_utils.get_image_input_signatures(
-      input_type, batch_size, input_image_size, num_channels)
-    input_specs = tf.keras.layers.InputSpec(
-        shape=[batch_size] + input_image_size + [num_channels])
-    model,_ = yolo_factory.build_yolo(input_specs = input_specs,
+def create_yolo_export_module(params: cfg.ExperimentConfig,
+                              input_type: str,
+                              batch_size: int,
+                              input_image_size: List[int],
+                              num_channels: int = 3):
+  """Creates YOLO export module."""                            
+  input_signature = export_utils.get_image_input_signatures(
+    input_type, batch_size, input_image_size, num_channels)
+  input_specs = tf.keras.layers.InputSpec(
+      shape=[batch_size] + input_image_size + [num_channels])
+  model,_ = yolo_factory.build_yolo(input_specs = input_specs,
                                     model_config = params.task.model,
                                     l2_regularization = None)
 
-    def preprocess_fn(inputs):
-      image_tensor = export_utils.parse_image(inputs, input_type,
-                                              input_image_size, num_channels)
-      # If input_type is `tflite`, do not apply image preprocessing.
-      if input_type == 'tflite':
-        return image_tensor
+  def preprocess_fn(inputs):
+    image_tensor = export_utils.parse_image(inputs, input_type,
+                                            input_image_size, num_channels)
+    # If input_type is `tflite`, do not apply image preprocessing.
+    if input_type == 'tflite':
+      return image_tensor
 
-      def preprocess_image_fn(inputs):
-          image = tf.cast(inputs, dtype=tf.float32)
-          image = image / 255.
-          (image, image_info) = model_fn.letterbox(image, input_image_size,
-                    letter_box = params.task.validation_data.parser.letter_box)
-          return image, image_info
+    def preprocess_image_fn(inputs):
+      image = tf.cast(inputs, dtype=tf.float32)
+      image = image / 255.
+      (image, image_info) = yolo_model_fn.letterbox(image, input_image_size,
+                letter_box = params.task.validation_data.parser.letter_box)
+      return image, image_info
 
-      images_spec = tf.TensorSpec(shape=input_image_size + [3],
-                                  dtype=tf.float32)
+    images_spec = tf.TensorSpec(shape=input_image_size + [3],
+                              dtype=tf.float32)
 
-      image_info_spec = tf.TensorSpec(shape=[4, 2], dtype=tf.float32)
+    image_info_spec = tf.TensorSpec(shape=[4, 2], dtype=tf.float32)
 
-      images, image_info = tf.nest.map_structure(
-          tf.identity,
-          tf.map_fn(
-              preprocess_image_fn,
-              elems=inputs,
-              fn_output_signature=(images_spec, image_info_spec),
-              parallel_iterations=32))
+    images, image_info = tf.nest.map_structure(
+        tf.identity,
+        tf.map_fn(
+            preprocess_image_fn,
+            elems=inputs,
+            fn_output_signature=(images_spec, image_info_spec),
+            parallel_iterations=32))
 
-      return images, image_info
+    return images, image_info
       
-    def inference_steps(inputs, model):
-      images, image_info = inputs
-      detection = model(images, training = False)
-      detection['bbox'] = model_fn.undo_info(detection['bbox'],
-                                          detection['num_detections'],
-                                          image_info,
-                                          expand=False)
-                                        
-      final_outputs = {
-        'detection_boxes': detection['bbox'],
-        'detection_scores': detection['confidence'],
-        'detection_classes': detection['classes'],
-        'num_detections': detection['num_detections']
-      }
+  def inference_steps(inputs, model):
+    images, image_info = inputs
+    detection = model(images, training = False)
+    detection['bbox'] = yolo_model_fn.undo_info(detection['bbox'],
+                                        detection['num_detections'],
+                                        image_info,
+                                        expand=False)
+                                      
+    final_outputs = {
+      'detection_boxes': detection['bbox'],
+      'detection_scores': detection['confidence'],
+      'detection_classes': detection['classes'],
+      'num_detections': detection['num_detections']
+    }
 
-      return final_outputs
+    return final_outputs
 
-    export_module = export_base.ExportModule(params,
-                                           model=model,
-                                           input_signature=input_signature,
-                                           preprocessor=preprocess_fn,
-                                           inference_step=inference_steps)
+  export_module = export_base.ExportModule(params,
+                                          model=model,
+                                          input_signature=input_signature,
+                                          preprocessor=preprocess_fn,
+                                          inference_step=inference_steps)
 
-    return export_module
+  return export_module
 
 def get_export_module(params: cfg.ExperimentConfig,
                       input_type: str,
@@ -150,6 +153,10 @@ def get_export_module(params: cfg.ExperimentConfig,
                 configs.image_classification.ImageClassificationTask):
     export_module = create_classification_export_module(
         params, input_type, batch_size, input_image_size, num_channels)
+  elif isinstance(params.task,
+                  yolo_config.yolo.YoloTask):
+    export_module = create_yolo_export_module(
+      params, input_type, batch_size, input_image_size, num_channels)
   else:
     raise ValueError('Export module not implemented for {} task.'.format(
         type(params.task)))
