f428c400b875f415f26f5c93bf565ba3cb4057f1
Yeqing Li <yeqing@google.com>
2022-03-02 18:04:55 -0800

    Internal change
    PiperOrigin-RevId: 432074123

--- official/projects/assemblenet/configs/assemblenet_test.py
+++ official/projects/assemblenet/configs/assemblenet_test.py
@@ -18,7 +18,7 @@ import tensorflow as tf
 from official.core import config_definitions as cfg
 from official.core import exp_factory
 from official.projects.assemblenet.configs import assemblenet
-from official.vision.beta.configs import video_classification as exp_cfg
+from official.vision.configs import video_classification as exp_cfg
 
 
 class AssemblenetTest(tf.test.TestCase, parameterized.TestCase):
