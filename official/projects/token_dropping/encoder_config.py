8f8153650a3ad86ce8aad0982a7ef02a5b114502
Le Hou <lehou@google.com>
2022-03-10 20:56:00 -0800

    Internal change
    PiperOrigin-RevId: 433920851

---
+++ official/projects/token_dropping/encoder_config.py
@@ -0,0 +1,67 @@
+# Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Token dropping encoder configuration and instantiation."""
+import dataclasses
+from typing import Tuple
+import tensorflow as tf
+
+from official.modeling import tf_utils
+from official.modeling.hyperparams import base_config
+from official.nlp.configs import encoders
+from official.projects.token_dropping import encoder
+
+
+@dataclasses.dataclass
+class TokenDropBertEncoderConfig(encoders.BertEncoderConfig):
+  token_loss_init_value: float = 10.0
+  token_loss_beta: float = 0.995
+  token_keep_k: int = 256
+  token_allow_list: Tuple[int, ...] = (100, 101, 102, 103)
+  token_deny_list: Tuple[int, ...] = (0,)
+
+
+@base_config.bind(TokenDropBertEncoderConfig)
+def get_encoder(encoder_cfg: TokenDropBertEncoderConfig):
+  """Instantiates 'TokenDropBertEncoder'.
+
+  Args:
+    encoder_cfg: A 'TokenDropBertEncoderConfig'.
+
+  Returns:
+    A 'encoder.TokenDropBertEncoder' object.
+  """
+  return encoder.TokenDropBertEncoder(
+      vocab_size=encoder_cfg.vocab_size,
+      hidden_size=encoder_cfg.hidden_size,
+      num_layers=encoder_cfg.num_layers,
+      num_attention_heads=encoder_cfg.num_attention_heads,
+      intermediate_size=encoder_cfg.intermediate_size,
+      activation=tf_utils.get_activation(encoder_cfg.hidden_activation),
+      dropout_rate=encoder_cfg.dropout_rate,
+      attention_dropout_rate=encoder_cfg.attention_dropout_rate,
+      max_sequence_length=encoder_cfg.max_position_embeddings,
+      type_vocab_size=encoder_cfg.type_vocab_size,
+      initializer=tf.keras.initializers.TruncatedNormal(
+          stddev=encoder_cfg.initializer_range),
+      output_range=encoder_cfg.output_range,
+      embedding_width=encoder_cfg.embedding_size,
+      return_all_encoder_outputs=encoder_cfg.return_all_encoder_outputs,
+      dict_outputs=True,
+      norm_first=encoder_cfg.norm_first,
+      token_loss_init_value=encoder_cfg.token_loss_init_value,
+      token_loss_beta=encoder_cfg.token_loss_beta,
+      token_keep_k=encoder_cfg.token_keep_k,
+      token_allow_list=encoder_cfg.token_allow_list,
+      token_deny_list=encoder_cfg.token_deny_list)
---
+++ official/projects/token_dropping/masked_lm_test.py
@@ -0,0 +1,63 @@
+# Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for official.nlp.tasks.masked_lm."""
+
+import tensorflow as tf
+
+from official.nlp.configs import bert
+from official.nlp.configs import encoders
+from official.nlp.data import pretrain_dataloader
+from official.projects.token_dropping import encoder_config
+from official.projects.token_dropping import masked_lm
+
+
+class MLMTaskTest(tf.test.TestCase):
+
+  def test_task(self):
+    config = masked_lm.TokenDropMaskedLMConfig(
+        init_checkpoint=self.get_temp_dir(),
+        scale_loss=True,
+        model=bert.PretrainerConfig(
+            encoder=encoders.EncoderConfig(
+                any=encoder_config.TokenDropBertEncoderConfig(
+                    vocab_size=30522, num_layers=1, token_keep_k=64),
+                type="any"),
+            cls_heads=[
+                bert.ClsHeadConfig(
+                    inner_dim=10, num_classes=2, name="next_sentence")
+            ]),
+        train_data=pretrain_dataloader.BertPretrainDataConfig(
+            input_path="dummy",
+            max_predictions_per_seq=20,
+            seq_length=128,
+            global_batch_size=1))
+    task = masked_lm.TokenDropMaskedLMTask(config)
+    model = task.build_model()
+    metrics = task.build_metrics()
+    dataset = task.build_inputs(config.train_data)
+
+    iterator = iter(dataset)
+    optimizer = tf.keras.optimizers.SGD(lr=0.1)
+    task.train_step(next(iterator), model, optimizer, metrics=metrics)
+    task.validation_step(next(iterator), model, metrics=metrics)
+
+    # Saves a checkpoint.
+    ckpt = tf.train.Checkpoint(model=model, **model.checkpoint_items)
+    ckpt.save(config.init_checkpoint)
+    task.initialize(model)
+
+
+if __name__ == "__main__":
+  tf.test.main()
