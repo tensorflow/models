af788dabb141f161dcbe4aae52276f5c465a2c5f
Hongkun Yu <hongkuny@google.com>
2022-03-16 11:12:00 -0700

    Remove experimental optimizer usage on github.
    PiperOrigin-RevId: 435102510

--- official/modeling/optimization/optimizer_factory.py
+++ official/modeling/optimization/optimizer_factory.py
@@ -28,9 +28,9 @@ from official.nlp import optimization as nlp_optimization
 
 OPTIMIZERS_CLS = {
     'sgd': tf.keras.optimizers.SGD,
-    'sgd_experimental': tf.keras.optimizers.experimental.SGD,
+    # TODO(chenmoneygithub): experimental.SGD
     'adam': tf.keras.optimizers.Adam,
-    'adam_experimental': tf.keras.optimizers.experimental.Adam,
+    # TODO(chenmoneygithub): experimental.Adam
     'adamw': nlp_optimization.AdamWeightDecay,
     'lamb': tfa_optimizers.LAMB,
     'rmsprop': tf.keras.optimizers.RMSprop,
