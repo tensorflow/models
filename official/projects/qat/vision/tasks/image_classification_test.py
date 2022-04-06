580aa3f6c0864d778c822066cdccbba7f40da100
Fan Yang <fyangf@google.com>
2022-04-05 17:44:03 -0700

    Internal change.
    PiperOrigin-RevId: 439718605

--- official/projects/qat/vision/tasks/image_classification_test.py
+++ official/projects/qat/vision/tasks/image_classification_test.py
@@ -15,6 +15,8 @@
 """Tests for image classification task."""
 
 # pylint: disable=unused-import
+import os
+
 from absl.testing import parameterized
 import orbit
 import tensorflow as tf
@@ -23,16 +25,31 @@ from official import vision
 from official.core import exp_factory
 from official.modeling import optimization
 from official.projects.qat.vision.tasks import image_classification as img_cls_task
+from official.vision.dataloaders import tfexample_utils
 
 
 class ImageClassificationTaskTest(tf.test.TestCase, parameterized.TestCase):
 
+  def _create_test_tfrecord(self, tfrecord_file, example, num_samples):
+    examples = [example] * num_samples
+    tfexample_utils.dump_to_tfrecord(
+        record_file=tfrecord_file, tf_examples=examples)
+
   @parameterized.parameters(('resnet_imagenet_qat'),
                             ('mobilenet_imagenet_qat'))
   def test_task(self, config_name):
+    input_image_size = [224, 224]
+    test_tfrecord_file = os.path.join(self.get_temp_dir(), 'cls_test.tfrecord')
+    example = tf.train.Example.FromString(
+        tfexample_utils.create_classification_example(
+            image_height=input_image_size[0], image_width=input_image_size[1]))
+    self._create_test_tfrecord(
+        tfrecord_file=test_tfrecord_file, example=example, num_samples=10)
+
     config = exp_factory.get_exp_config(config_name)
     config.task.train_data.global_batch_size = 2
-
+    config.task.validation_data.input_path = test_tfrecord_file
+    config.task.train_data.input_path = test_tfrecord_file
     task = img_cls_task.ImageClassificationTask(config.task)
     model = task.build_model()
     metrics = task.build_metrics()
