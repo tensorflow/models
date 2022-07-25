38262a099353e7a118c0d5515f944b60fb0c3dc5
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2022-07-25 11:53:00 +0200

    corrected use first projection bool

--- official/vision/modeling/backbones/resnet.py
+++ official/vision/modeling/backbones/resnet.py
@@ -298,8 +298,9 @@ class ResNet(tf.keras.Model):
       else:
         raise ValueError('Block fn `{}` is not supported.'.format(spec[0]))
       use_first_projection = (
-          not (i == 0 and spec[0] == 'residual') and
-          not self._use_first_projection
+        spec[0] == 'bottleneck'
+        or i > 0
+        or (i == 0 and self._use_first_projection)
       )
       x = self._block_group(
           inputs=x,
