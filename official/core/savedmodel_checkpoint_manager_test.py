e8771fe1d25ffd4217e607e38ad4b48307cde5e7
Chaochao Yan <allenyan@google.com>
2022-08-22 13:07:29 -0700

    Internal change
    PiperOrigin-RevId: 469264237

--- official/core/savedmodel_checkpoint_manager_test.py
+++ official/core/savedmodel_checkpoint_manager_test.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import os
+import time
 from typing import Iterable
 
 import tensorflow as tf
@@ -32,12 +33,20 @@ def _models_exist(checkpoint_path: str, models: Iterable[str]) -> bool:
 
 class CheckpointManagerTest(tf.test.TestCase):
 
-  def testSimpleTest(self):
+  def _create_manager(self, max_to_keep: int = 1) -> tf.train.CheckpointManager:
+    """Sets up SavedModelCheckpointManager object.
+
+    Args:
+      max_to_keep: max number of savedmodels to keep.
+
+    Returns:
+      created savedmodel manager.
+    """
     models = {
-        "model_1":
+        'model_1':
             tf.keras.Sequential(
                 layers=[tf.keras.layers.Dense(8, input_shape=(16,))]),
-        "model_2":
+        'model_2':
             tf.keras.Sequential(
                 layers=[tf.keras.layers.Dense(16, input_shape=(32,))]),
     }
@@ -45,9 +54,13 @@ class CheckpointManagerTest(tf.test.TestCase):
     manager = savedmodel_checkpoint_manager.SavedModelCheckpointManager(
         checkpoint=checkpoint,
         directory=self.get_temp_dir(),
-        max_to_keep=1,
+        max_to_keep=max_to_keep,
         modules_to_export=models)
+    return manager
 
+  def test_max_to_keep(self):
+    manager = self._create_manager()
+    models = manager.modules_to_export
     first_path = manager.save()
     second_path = manager.save()
 
@@ -57,6 +70,45 @@ class CheckpointManagerTest(tf.test.TestCase):
     self.assertTrue(_models_exist(second_path, models.keys()))
     self.assertFalse(_models_exist(first_path, models.keys()))
 
+  def test_returns_none_after_timeout(self):
+    manager = self._create_manager()
+    start = time.time()
+    ret = manager.wait_for_new_savedmodel(
+        None, timeout=1.0, seconds_to_sleep=0.5)
+    end = time.time()
+    self.assertIsNone(ret)
+    # We've waited 0.5 second.
+    self.assertGreater(end, start + 0.5)
+    # The timeout kicked in.
+    self.assertLess(end, start + 0.6)
 
-if __name__ == "__main__":
+  def test_saved_model_iterator(self):
+    manager = self._create_manager(max_to_keep=2)
+    self.assertIsNotNone(manager.save(checkpoint_number=1))
+    self.assertIsNotNone(manager.save(checkpoint_number=2))
+    self.assertIsNotNone(manager.save(checkpoint_number=3))
+
+    # Savedmodels are in time order.
+    expected_savedmodels = manager.savedmodels
+    # Order not guaranteed.
+    existing_savedmodels = manager.get_existing_savedmodels()
+    savedmodels = list(manager.savedmodels_iterator(timeout=3.0))
+    self.assertEqual(savedmodels, expected_savedmodels)
+    self.assertEqual(set(savedmodels), set(existing_savedmodels))
+
+  def test_saved_model_iterator_timeout_fn(self):
+    manager = self._create_manager()
+    timeout_fn_calls = [0]
+
+    def timeout_fn():
+      timeout_fn_calls[0] += 1
+      return timeout_fn_calls[0] > 3
+
+    results = list(
+        manager.savedmodels_iterator(timeout=0.1, timeout_fn=timeout_fn))
+    self.assertEqual([], results)
+    self.assertEqual(4, timeout_fn_calls[0])
+
+
+if __name__ == '__main__':
   tf.test.main()
