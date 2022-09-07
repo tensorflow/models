2e948ccb3a66b0f6c2ab7041dd635925fc89779e
Fan Yang <fyangf@google.com>
2022-09-07 12:46:47 -0700

    Improve support in TFLite model conversion.
    PiperOrigin-RevId: 472790049

--- official/vision/serving/export_tflite_lib.py
+++ official/vision/serving/export_tflite_lib.py
@@ -19,17 +19,21 @@ from typing import Iterator, List, Optional
 from absl import logging
 import tensorflow as tf
 
+from official.core import base_task
 from official.core import config_definitions as cfg
 from official.vision import configs
 from official.vision import tasks
 
 
 def create_representative_dataset(
-    params: cfg.ExperimentConfig) -> tf.data.Dataset:
+    params: cfg.ExperimentConfig,
+    task: Optional[base_task.Task] = None) -> tf.data.Dataset:
   """Creates a tf.data.Dataset to load images for representative dataset.
 
   Args:
     params: An ExperimentConfig.
+    task: An optional task instance. If it is None, task will be built according
+      to the task type in params.
 
   Returns:
     A tf.data.Dataset instance.
@@ -37,19 +41,20 @@ def create_representative_dataset(
   Raises:
     ValueError: If task is not supported.
   """
-  if isinstance(params.task,
-                configs.image_classification.ImageClassificationTask):
+  if task is None:
+    if isinstance(params.task,
+                  configs.image_classification.ImageClassificationTask):
 
-    task = tasks.image_classification.ImageClassificationTask(params.task)
-  elif isinstance(params.task, configs.retinanet.RetinaNetTask):
-    task = tasks.retinanet.RetinaNetTask(params.task)
-  elif isinstance(params.task, configs.maskrcnn.MaskRCNNTask):
-    task = tasks.maskrcnn.MaskRCNNTask(params.task)
-  elif isinstance(params.task,
-                  configs.semantic_segmentation.SemanticSegmentationTask):
-    task = tasks.semantic_segmentation.SemanticSegmentationTask(params.task)
-  else:
-    raise ValueError('Task {} not supported.'.format(type(params.task)))
+      task = tasks.image_classification.ImageClassificationTask(params.task)
+    elif isinstance(params.task, configs.retinanet.RetinaNetTask):
+      task = tasks.retinanet.RetinaNetTask(params.task)
+    elif isinstance(params.task, configs.maskrcnn.MaskRCNNTask):
+      task = tasks.maskrcnn.MaskRCNNTask(params.task)
+    elif isinstance(params.task,
+                    configs.semantic_segmentation.SemanticSegmentationTask):
+      task = tasks.semantic_segmentation.SemanticSegmentationTask(params.task)
+    else:
+      raise ValueError('Task {} not supported.'.format(type(params.task)))
   # Ensure batch size is 1 for TFLite model.
   params.task.train_data.global_batch_size = 1
   params.task.train_data.dtype = 'float32'
@@ -59,17 +64,20 @@ def create_representative_dataset(
 
 def representative_dataset(
     params: cfg.ExperimentConfig,
+    task: Optional[base_task.Task] = None,
     calibration_steps: int = 2000) -> Iterator[List[tf.Tensor]]:
   """"Creates representative dataset for input calibration.
 
   Args:
     params: An ExperimentConfig.
+    task: An optional task instance. If it is None, task will be built according
+      to the task type in params.
     calibration_steps: The steps to do calibration.
 
   Yields:
     An input image tensor.
   """
-  dataset = create_representative_dataset(params=params)
+  dataset = create_representative_dataset(params=params, task=task)
   for image, _ in dataset.take(calibration_steps):
     # Skip images that do not have 3 channels.
     if image.shape[-1] != 3:
@@ -80,7 +88,9 @@ def representative_dataset(
 def convert_tflite_model(saved_model_dir: str,
                          quant_type: Optional[str] = None,
                          params: Optional[cfg.ExperimentConfig] = None,
-                         calibration_steps: Optional[int] = 2000) -> bytes:
+                         task: Optional[base_task.Task] = None,
+                         calibration_steps: Optional[int] = 2000,
+                         denylisted_ops: Optional[list[str]] = None) -> bytes:
   """Converts and returns a TFLite model.
 
   Args:
@@ -90,7 +100,11 @@ def convert_tflite_model(saved_model_dir: str,
       fallback), `int8_full` (integer only) and None (no quantization).
     params: An optional ExperimentConfig to load and preprocess input images to
       do calibration for integer quantization.
+    task: An optional task instance. If it is None, task will be built according
+      to the task type in params.
     calibration_steps: The steps to do calibration.
+    denylisted_ops: A list of strings containing ops that are excluded from
+      integer quantization.
 
   Returns:
     A converted TFLite model with optional PTQ.
@@ -106,6 +120,7 @@ def convert_tflite_model(saved_model_dir: str,
       converter.representative_dataset = functools.partial(
           representative_dataset,
           params=params,
+          task=task,
           calibration_steps=calibration_steps)
       if quant_type.startswith('int8_full'):
         converter.target_spec.supported_ops = [
@@ -117,6 +132,20 @@ def convert_tflite_model(saved_model_dir: str,
       if quant_type == 'int8_full_int8_io':
         converter.inference_input_type = tf.int8
         converter.inference_output_type = tf.int8
+
+      if denylisted_ops:
+        debug_options = tf.lite.experimental.QuantizationDebugOptions(
+            denylisted_ops=denylisted_ops)
+        debugger = tf.lite.experimental.QuantizationDebugger(
+            converter=converter,
+            debug_dataset=functools.partial(
+                representative_dataset,
+                params=params,
+                calibration_steps=calibration_steps),
+            debug_options=debug_options)
+        debugger.run()
+        return debugger.get_nondebug_quantized_model()
+
     elif quant_type == 'fp16':
       converter.optimizations = [tf.lite.Optimize.DEFAULT]
       converter.target_spec.supported_types = [tf.float16]
