b2926f2608c5b780e217fe30c7787e6ddf0ecce6
A. Unique TensorFlower <gardener@tensorflow.org>
2022-11-30 13:17:23 -0800

    Internal change
    PiperOrigin-RevId: 492000777

--- official/vision/modeling/layers/edgetpu_test.py
+++ official/vision/modeling/layers/edgetpu_test.py
@@ -44,16 +44,75 @@ def _maximum_activation_size(model):
   return max_size
 
 
+def _deviation_and_margin(reference, valid, optimized):
+  """Returns deviation and margin between two batched sets of indices."""
+  deviation_rate = 0
+  min_union = reference.shape[1] + optimized.shape[1]
+  runs = reference.shape[0]
+  for run in range(runs):
+    reference_slice = {*reference[run, :valid[run]].numpy().tolist()}
+    optimized_slice = {*optimized[run].numpy().astype(int).tolist()} - {-1}
+    union_size = len(optimized_slice | reference_slice)
+    symdiff_size = len(optimized_slice ^ reference_slice)
+    deviation_rate += symdiff_size / union_size
+    min_union = min(min_union, union_size)
+  deviation_rate = deviation_rate / runs
+  # six sigma estimate via LLN theorem
+  margin = 6 * (deviation_rate / np.sqrt(runs) + 1 / (runs * min_union))
+  return deviation_rate, margin
+
+
 class NonMaxSuppressionTest(parameterized.TestCase, tf.test.TestCase):
 
   def setUp(self):
     super().setUp()
     tf.random.set_seed(42)
 
-  @parameterized.parameters((16, 8, 200, 0.009), (31, 17, 100, 0.013),
-                            (71, 41, 100, 0.045), (150, 100, 100, 0.129),
-                            (300, 300, 100, 0.116), (600, 600, 50, 0.176))
-  def test_reference_match(self, n, top, runs, max_deviation):
+  def test_refinement_sample(self):
+    """Tests difference in NMS behaviours.
+
+    Runs on four boxes with following IOU table (only neighbours will qualify
+    as similar boxes)
+
+    box | 0    | 1    | 2    | 3
+    --- | ---- | ---- | ---- | ----
+    0   | 1    | 7/13 | 1/4  | 1/19
+    1   | 7/13 | 1    | 7/13 | 1/4
+    2   | 1/4  | 7/13 | 1    | 7/13
+    3   | 1/19 | 1/4  | 7/13 | 1
+
+    So 0 is best box, it eliminates 1, next is box 2 which is eleminated by 1
+    if it is allowed (depending on number of refinements).
+    """
+    boxes: tf.Tensor = tf.constant(
+        [
+            # y1,  x1,  y2,  x2
+            [0.0, 0.0, 1.0, 1.0],
+            [0.0, 0.3, 1.0, 1.3],
+            [0.0, 0.6, 1.0, 1.6],
+            [0.0, 0.9, 1.0, 1.9],
+        ],
+        dtype=tf.float32)
+    scores: tf.Tensor = tf.constant([
+        1.0,
+        0.9,
+        0.8,
+        0.7,
+    ], dtype=tf.float32)
+    self.assertAllEqual(
+        edgetpu.non_max_suppression_padded(boxes, scores, 4, refinements=0),
+        tf.constant([0.0, -1.0, -1.0, -1.0], dtype=tf.float32))
+    self.assertAllEqual(
+        edgetpu.non_max_suppression_padded(boxes, scores, 4, refinements=1),
+        tf.constant([0.0, 2.0, -1.0, -1.0], dtype=tf.float32))
+
+  @parameterized.parameters((16, 8, 200, [0.009, 0.004, 0.004]),
+                            (31, 17, 100, [0.013, 0.004, 0.004]),
+                            (71, 41, 100, [0.045, 0.003, 0.002]),
+                            (150, 100, 100, [0.129, 0.010, 0.001]),
+                            (300, 300, 100, [0.116, 0.016, 0.002]),
+                            (600, 600, 50, [0.176, 0.032, 0.003]))
+  def test_reference_match(self, n, top, runs, max_devs):
     """Compares that new optimized method is close to reference method.
 
     Runs two algorithms with same sets of input boxes and scores, and measures
@@ -71,32 +130,26 @@ class NonMaxSuppressionTest(parameterized.TestCase, tf.test.TestCase):
       top: limit of output boxes count.
       runs: for the statistical testing number of runs to performs to avoid
         tests flakiness.
-      max_deviation: mean limit on deviation between optimized and reference
-        algorithms. Please read notes why this number may be set higher to avoid
-        flaky testing.
+      max_devs: series of mean limits on deviation between optimized and
+        reference algorithms with different number of refinements. (Indexes of
+        elements correspond to number of refinements) Please use margin based
+        values proposed by failed test to avoid flaky testing.
     """
-    deviation_rate = 0
-    min_union = 2*n
     boxes = random_boxes([runs, n])
     scores = tf.random.uniform(shape=[runs, n])
-    test = edgetpu.non_max_suppression_padded(boxes, scores, top)
-    for run in range(runs):
-      reference = tf.image.non_max_suppression(boxes[run], scores[run], top)
-      reference = {*reference.numpy().tolist()}
-      optimized = {*test[run].numpy().astype(int).tolist()} - {-1}
-      union_size = len(optimized | reference)
-      deviation_rate += len(optimized ^ reference) / union_size
-      min_union = min(min_union, union_size)
-    deviation_rate = deviation_rate / runs
-    # six sigma estimate via LLN theorem
-    safe_margin = 6 * (deviation_rate / np.sqrt(runs) + 1/(runs*min_union))
-    self.assertLess(
-        deviation_rate,
-        max_deviation,
-        msg='Deviation rate between optimized and reference implementations is '
-        'higher than expected. If you are tuning the test, recommended safe '
-        'deviation rate is '
-        f'{deviation_rate} + {safe_margin} = {deviation_rate + safe_margin}')
+    reference, valid = tf.image.non_max_suppression_padded(
+        boxes, scores, top, pad_to_max_output_size=True)
+    for refinements, max_deviation in enumerate(max_devs):
+      optimized = edgetpu.non_max_suppression_padded(
+          boxes, scores, top, refinements=refinements)
+      deviation, margin = _deviation_and_margin(reference, valid, optimized)
+      self.assertLess(
+          deviation,
+          max_deviation,
+          msg='Deviation rate between optimized and reference implementations is '
+          'higher than expected. If you are tuning the test, recommended safe '
+          'deviation rate is '
+          f'{deviation} + {margin} = {deviation + margin}')
 
   @parameterized.parameters(([16], 8), ([91, 150], 100), ([20, 20, 200], 10))
   def test_sharded_match(self, shape: list[int], top: int):
