bc4d52dfd72b1986ecc9e6b39249738fb0fcc1bc
Fan Yang <fyangf@google.com>
2022-12-12 09:53:08 -0800

    Internal change
    PiperOrigin-RevId: 494756436

--- official/projects/qat/nlp/tasks/__init__.py
+++ official/projects/qat/nlp/tasks/__init__.py
@@ -12,6 +12,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tasks package definition."""
-
-from official.projects.qat.nlp.tasks import question_answering
