b23b3268890e180b6c58092b8b5814019bb41deb
A. Unique TensorFlower <gardener@tensorflow.org>
2023-05-09 23:12:11 -0700

    Pass CheckpoitOptions to Orbit Controller _maybe_save_checkpoint().
    PiperOrigin-RevId: 530813040

--- orbit/controller.py
+++ orbit/controller.py
@@ -544,7 +544,8 @@ class Controller:
     if self.checkpoint_manager and self.checkpoint_manager.checkpoint_interval:
       ckpt_path = self.checkpoint_manager.save(
           checkpoint_number=self.global_step.numpy(),
-          check_interval=check_interval)
+          check_interval=check_interval,
+          options=self._checkpoint_options)
       if ckpt_path is not None:
         _log(f"saved checkpoint to {ckpt_path}.")
         return True
