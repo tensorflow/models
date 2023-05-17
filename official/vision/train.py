8f17df9ae9f15b7347128ea5a098dab268bb3b01
Yuexin Wu <crickwu@google.com>
2023-05-17 11:33:47 -0700

    Disable preemption_watcher as it's not supported externally.
    PiperOrigin-RevId: 532853592

--- official/vision/train.py
+++ official/vision/train.py
@@ -46,7 +46,9 @@ def _run_experiment_with_preemption_recovery(params, model_dir):
           tpu_address=params.runtime.tpu)
       with distribution_strategy.scope():
         task = task_factory.get_task(params.task, logging_dir=model_dir)
-      preemption_watcher = tf.distribute.experimental.PreemptionWatcher()
+      # pylint: disable=line-too-long
+      preemption_watcher = None  # copybara-replace
+      # pylint: enable=line-too-long
 
       train_lib.run_experiment(
           distribution_strategy=distribution_strategy,
