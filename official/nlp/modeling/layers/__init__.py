a46e3dcedffc64e5127fb70cf6da2334d83ecf7e
A. Unique TensorFlower <gardener@tensorflow.org>
2023-05-31 15:55:39 -0700

    Internal change
    PiperOrigin-RevId: 536847471

--- official/nlp/modeling/layers/__init__.py
+++ official/nlp/modeling/layers/__init__.py
@@ -58,7 +58,7 @@ from official.nlp.modeling.layers.reuse_attention import ReuseMultiHeadAttention
 from official.nlp.modeling.layers.reuse_transformer import ReuseTransformer
 from official.nlp.modeling.layers.rezero_transformer import ReZeroTransformer
 from official.nlp.modeling.layers.routing import *
-from official.nlp.modeling.layers.self_attention_mask import SelfAttentionMask
+from official.nlp.modeling.layers.self_attention_mask import *
 from official.nlp.modeling.layers.spectral_normalization import *
 from official.nlp.modeling.layers.talking_heads_attention import TalkingHeadsAttention
 from official.nlp.modeling.layers.text_layers import BertPackInputs
