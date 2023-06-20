242534b011e7c7c12e0a991b9a5a49d65936d195
Chaochao Yan <allenyan@google.com>
2023-06-20 09:38:39 -0700

    Internal change
    PiperOrigin-RevId: 541953332

--- official/core/savedmodel_checkpoint_manager.py
+++ official/core/savedmodel_checkpoint_manager.py
@@ -72,13 +72,19 @@ class SavedModelCheckpointManager(tf.train.CheckpointManager):
 
     # Save the models for the checkpoint that just got written.
     saved_modules_directory = make_saved_modules_directory_name(checkpoint_path)
+    # Atomic export of SavedModel. Write into a temporary direcotory and then
+    # rename as the final direcotory after finishing the writing.
+    # This can avoid trying to read an unfinished savedmodel.
+    saved_modules_directory_tmp = saved_modules_directory + '_temp'
     for model_name, model in self._modules_to_export.items():
       signatures = getattr(model, 'saved_model_signatures', None)
       if signatures is not None:
         tf.saved_model.save(
             obj=model,
-            export_dir=os.path.join(saved_modules_directory, model_name),
+            export_dir=os.path.join(saved_modules_directory_tmp, model_name),
             signatures=signatures)
+    if tf.io.gfile.exists(saved_modules_directory_tmp):
+      tf.io.gfile.rename(saved_modules_directory_tmp, saved_modules_directory)
 
     saved_modules_directories_to_keep = [
         make_saved_modules_directory_name(ckpt) for ckpt in self.checkpoints
@@ -105,7 +111,14 @@ class SavedModelCheckpointManager(tf.train.CheckpointManager):
     """
     saved_modules_glob = make_saved_modules_directory_name(
         self._checkpoint_prefix + '-*')
-    return tf.io.gfile.glob(saved_modules_glob)
+    savedmodels = tf.io.gfile.glob(saved_modules_glob)
+    # Filter out temporary savedmodel.
+    savedmodels = [
+        savedmodel
+        for savedmodel in savedmodels
+        if savedmodel.endswith(SAVED_MODULES_PATH_SUFFIX)
+    ]
+    return savedmodels
 
   @property
   def latest_savedmodel(self) -> Union[str, None]:
