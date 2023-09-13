70487b52b34119d0ad0d94be113cd56124ab7dc0
A. Unique TensorFlower <gardener@tensorflow.org>
2023-09-13 12:01:48 -0700

    Update pip package  for 2.13.2 patch release
    PiperOrigin-RevId: 565120605

--- official/pip_package/setup.py
+++ official/pip_package/setup.py
@@ -20,7 +20,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.13.1'
+version = '2.13.2'
 tf_version = '2.13.0'  # Major version.
 
 project_name = 'tf-models-official'
