f479bf74f8adf921a9e2a40f3ef24a9a1a139d8f
mjyun01 <87511647+mjyun01@users.noreply.github.com>
2024-01-14 13:43:51 +0900

    Delete official/projects/RNGDET directory

--- official/projects/RNGDET/do_train.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=7 python3 train.py \
-  --mode=train \
-  --experiment=rngdet_cityscale  \
-  --model_dir=/home/mjyun/01_ghpark/ckpt/06_test_please_final \
-  --config_file=./configs/experiments/cityscale_rngdet_r50_gpu.yaml \
