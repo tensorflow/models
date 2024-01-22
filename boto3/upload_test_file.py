3c54152acd4083cc9ec67e787391be83c4847bf0
MatsukageRyo <mat.3.tun@gmail.com>
2024-01-22 14:04:58 +0000

    テストファイルをアップロード

---
+++ boto3/upload_test_file.py
@@ -0,0 +1,24 @@
+import s3_utils
+
+if __name__ == "__main__":
+    # create bucket
+    s3 = s3_utils.s3_utils('bounding-box-suggestion')
+
+    # create sample user directory
+    if not s3.exist_dir('sample-id/'):
+        s3.mk_dir('sample-id/')
+    
+    # create input and output directory
+    if not s3.exist_dir('sample-id/input/'):
+        s3.mk_dir('sample-id/input/')
+    if not s3.exist_dir('sample-id/output/'):
+        s3.mk_dir('sample-id/output/')
+    
+    # upload sample file
+    s3.upload_file('boto3/images.zip', 'sample-id/input/')
+
+    # Test
+    assert s3.exist_dir('sample-id/')
+    assert s3.exist_dir('sample-id/input/')
+    assert s3.check_uploaded_file('boto3/images.zip', 'sample-id/input/')
+    assert s3.exist_dir('sample-id/output/')
