af5e7307252e997731cda5819eb5046c49af6649
mjyun01 <87511647+mjyun01@users.noreply.github.com>
2024-02-07 22:18:36 +0900

    Update do_train.sh

--- official/projects/rngdet/do_train.sh
+++ official/projects/rngdet/do_train.sh
@@ -1,6 +1,5 @@
-python3 train.py \
+CUDA_VISIBLE_DEVICES=7 python3 train.py \
   --mode=train \
   --experiment=rngdet_cityscale  \
-  --model_dir=gs://ghpark-ckpts/rngdet/test_15 \
-  --config_file=./configs/experiments/cityscale_rngdet_r50_tpu.yaml \
-  --tpu=postech-tpu-5
+  --model_dir=./ckpt/06_test_rot \
+  --config_file=./configs/experiments/cityscale_rngdet_r50_gpu.yaml \
