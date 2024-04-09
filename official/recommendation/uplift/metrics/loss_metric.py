9a8dcc8ec5295cf12e435ed51783a6ea944371b0
Nicolas Aagnes <nicolasaagnes@google.com>
2024-04-08 18:00:43 -0700

    Allow a metric instance to be passed to the loss metric.
    PiperOrigin-RevId: 623003645

--- official/recommendation/uplift/metrics/loss_metric.py
+++ official/recommendation/uplift/metrics/loss_metric.py
@@ -40,7 +40,7 @@ class LossMetric(tf_keras.metrics.Metric):
   ...     true_logits=tf.constant([1, 2, 3, 4])
   ...     is_treatment=tf.constant([True, False, True, False]),
   ... )
-  >>> sliced_loss(y_true=tf.zeros(4), y_pred=y_pred)
+  >>> sliced_loss(y_true=y_true, y_pred=y_pred)
   {
       "loss": 2.5
       "loss/control": 4.0
@@ -58,7 +58,9 @@ class LossMetric(tf_keras.metrics.Metric):
 
   def __init__(
       self,
-      loss_fn: Callable[[tf.Tensor, tf.Tensor], tf.Tensor],
+      loss_fn: (
+          Callable[[tf.Tensor, tf.Tensor], tf.Tensor] | tf_keras.metrics.Metric
+      ),
       from_logits: bool = True,
       name: str = "loss",
       dtype: tf.DType = tf.float32,
@@ -67,27 +69,59 @@ class LossMetric(tf_keras.metrics.Metric):
     """Initializes the instance.
 
     Args:
-      loss_fn: The loss function to apply between the targets and model outputs,
-        with signature `loss_fn(y_true, y_pred, **loss_fn_kwargs)`.
+      loss_fn: The loss function or Keras metric to apply with call signature
+        `__call__(y_true: tf,Tensor, y_pred: tf.Tensor, **loss_fn_kwargs)`. Note
+        that the `loss_fn_kwargs` will not be passed to the `__call__` method if
+        `loss_fn` is a Keras metric.
       from_logits: Specifies whether the true logits or true predictions should
         be used from the model outputs to compute the loss. Defaults to using
         the true logits.
-      name: Optional name for the instance.
-      dtype: Optional data type for the instance.
+      name: Optional name for the instance. If `loss_fn` is a Keras metric then
+        its name will be used instead.
+      dtype: Optional data type for the instance. If `loss_fn` is a Keras metric
+        then its `dtype` will be used instead.
       **loss_fn_kwargs: The keyword arguments that are passed on to `loss_fn`.
+        These arguments will be ignored if `loss_fn` is a Keras metric.
     """
+    # Do not accept Loss objects as they reduce tensors before weighting.
+    if isinstance(loss_fn, tf_keras.losses.Loss):
+      raise TypeError(
+          "`loss_fn` cannot be a Keras `Loss` object, pass a non-reducing loss"
+          " function or a metric instance instead."
+      )
+
+    if isinstance(loss_fn, tf_keras.metrics.Metric):
+      name = loss_fn.name
+      dtype = loss_fn.dtype
+
     super().__init__(name=name, dtype=dtype)
 
     self._loss_fn = loss_fn
     self._from_logits = from_logits
     self._loss_fn_kwargs = loss_fn_kwargs
 
-    if "from_logits" in inspect.signature(loss_fn).parameters:
-      self._loss_fn_kwargs.update({"from_logits": from_logits})
+    if isinstance(loss_fn, tf_keras.metrics.Metric):
+      metric_from_logits = loss_fn.get_config().get("from_logits", from_logits)
+      if from_logits != metric_from_logits:
+        raise ValueError(
+            f"Value passed to `from_logits` ({from_logits}) is conflicting with"
+            " the `from_logits` value passed to the `loss_fn` metric"
+            f" ({metric_from_logits}). Ensure that they have the same value."
+        )
 
-    self._treatment_sliced_loss = treatment_sliced_metric.TreatmentSlicedMetric(
-        tf_keras.metrics.Mean(name=name, dtype=dtype)
-    )
+      self._treatment_sliced_loss = (
+          treatment_sliced_metric.TreatmentSlicedMetric(loss_fn)
+      )
+
+    else:
+      if "from_logits" in inspect.signature(loss_fn).parameters:
+        self._loss_fn_kwargs.update({"from_logits": from_logits})
+
+      self._treatment_sliced_loss = (
+          treatment_sliced_metric.TreatmentSlicedMetric(
+              tf_keras.metrics.Mean(name=name, dtype=dtype)
+          )
+      )
 
   def update_state(
       self,
@@ -115,16 +149,21 @@ class LossMetric(tf_keras.metrics.Metric):
           f" {type(y_pred)} instead."
       )
 
-    if self._from_logits:
-      loss = self._loss_fn(y_true, y_pred.true_logits)
-    else:
-      loss = self._loss_fn(y_true, y_pred.true_predictions)
+    pred = y_pred.true_logits if self._from_logits else y_pred.true_predictions
 
-    self._treatment_sliced_loss.update_state(
-        values=loss,
-        is_treatment=y_pred.is_treatment,
-        sample_weight=sample_weight,
-    )
+    if isinstance(self._loss_fn, tf_keras.metrics.Metric):
+      self._treatment_sliced_loss.update_state(
+          y_true,
+          y_pred=pred,
+          is_treatment=y_pred.is_treatment,
+          sample_weight=sample_weight,
+      )
+    else:
+      self._treatment_sliced_loss.update_state(
+          values=self._loss_fn(y_true, pred, **self._loss_fn_kwargs),
+          is_treatment=y_pred.is_treatment,
+          sample_weight=sample_weight,
+      )
 
   def result(self) -> dict[str, tf.Tensor]:
     return self._treatment_sliced_loss.result()
