6a33488a2c254aa9702cc1075d95d39f269a1585
A. Unique TensorFlower <gardener@tensorflow.org>
2024-07-17 10:17:55 -0700

    [TFMG] Update pip package version number for 2.17.0 release.
    PiperOrigin-RevId: 653276186

--- official/pip_package/setup.py
+++ official/pip_package/setup.py
@@ -20,8 +20,8 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.16.0'
-tf_version = '2.16.1'  # Major version.
+version = '2.17.0'
+tf_version = '2.17.0'  # Major version.
 
 project_name = 'tf-models-official'
 
