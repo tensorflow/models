b339981c038b99f6289c7ab42b6b674dbdf6874d
A. Unique TensorFlower <gardener@tensorflow.org>
2025-10-14 12:58:31 -0700

    No public description
    PiperOrigin-RevId: 819359586

--- official/projects/waste_identification_ml/llm_applications/milk_pouch_detection/classify_images.py
+++ official/projects/waste_identification_ml/llm_applications/milk_pouch_detection/classify_images.py
@@ -12,6 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Copyright 2025 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Classifies images based on Image Classifier."""
 
 import glob
@@ -27,10 +41,8 @@ from official.projects.waste_identification_ml.llm_applications.milk_pouch_detec
 
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string(
-    "input_dir", "tempdir", "Directory containing image frames to process."
-)
-
+INPUT_DIR = "input_images"
+CLASSIFICATION_DIR = "objects_for_classification"
 
 # Path to the custom trained model for Image Classifier.
 IMAGE_CLASSIFIER_WEIGHTS = (
@@ -40,10 +52,13 @@ CLASS_NAMES = ["dairy", "other"]
 
 
 def main(_) -> None:
-  dairy_folder = os.path.join(FLAGS.input_dir, "dairy")
-  other_folder = os.path.join(FLAGS.input_dir, "others")
-  os.makedirs(dairy_folder, exist_ok=True)
-  os.makedirs(other_folder, exist_ok=True)
+  parent_dir = os.path.dirname(os.path.abspath(INPUT_DIR))
+  predictions_dir = os.path.join(parent_dir, "predictions")
+
+  dairy_predictions = os.path.join(predictions_dir, "dairy")
+  other_predictions = os.path.join(predictions_dir, "others")
+  os.makedirs(dairy_predictions, exist_ok=True)
+  os.makedirs(other_predictions, exist_ok=True)
 
   classifier = models.ImageClassifier(
       model_path=IMAGE_CLASSIFIER_WEIGHTS,
@@ -51,7 +66,9 @@ def main(_) -> None:
       device="cuda" if torch.cuda.is_available() else "cpu",
   )
 
-  files = glob.glob(os.path.join(FLAGS.input_dir, "tempdir", "*"))
+  files = glob.glob(
+      os.path.join(INPUT_DIR, CLASSIFICATION_DIR, "*")
+  )
   print(f"Found {len(files)} images to process...")
 
   total_dairy_packets = 0
@@ -59,9 +76,9 @@ def main(_) -> None:
     pred_class, _ = classifier.classify(path)
     if pred_class == "dairy":
       total_dairy_packets += 1
-      shutil.move(path, os.path.join(dairy_folder, os.path.basename(path)))
+      shutil.move(path, os.path.join(dairy_predictions, os.path.basename(path)))
     else:
-      shutil.move(path, os.path.join(other_folder, os.path.basename(path)))
+      shutil.move(path, os.path.join(other_predictions, os.path.basename(path)))
 
 if __name__ == "__main__":
   app.run(main)
--- official/projects/waste_identification_ml/llm_applications/milk_pouch_detection/run_pipeline.sh
+++ official/projects/waste_identification_ml/llm_applications/milk_pouch_detection/run_pipeline.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -o errexit
+
 # --- Parse command-line arguments ---
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-    --input_dir=*) input_dir="${1#*=}"; shift ;;
+    --gcs_path=*) gcs_path="${1#*=}"; shift ;;
     *) echo "❌ Unknown parameter passed: $1"; exit 1 ;;
   esac
 done
 # --- Check if required arguments were provided ---
-if [ -z "$input_dir" ]; then
-  echo "❌ Error: --input_dir must be specified"
-  echo "✅ Usage: ./run_pipeline.sh --input_dir=/path/to/images"
+if [ -z "$gcs_path" ]; then
+  echo "❌ Error: --gcs_path must be specified"
+  echo "✅ Usage: ./run_pipeline.sh --gcs_path=/path/to/images"
   exit 1
 fi
+
 # --- Run pipeline ---
 echo "✅ Activating virtual environment..."
 source myenv/bin/activate
 
-echo "🚀 Running detect_and_segment.py..."
-echo "   Input directory: $input_dir"
-python3 extract_objects.py --input_dir="$input_dir"
+echo "🖨️ Copying images files from GCS bucket: $gcs_path"
+mkdir -p input_images
+gsutil -m cp "$gcs_path"* input_images/
 
-echo "🧠 Running classify.py..."
-python3 classify_images.py --input_dir="$input_dir"
+echo "🔎 Extracting objects from images..."
+python3 extract_objects.py
+
+echo "🧠 Classifying objects"
+python3 classify_images.py
+
+echo "🖨️ Moving predictions back to GCS bucket..."
+gsutil -m cp -r predictions/ "$gcs_path"
+
 echo "🧹 Deactivating virtual environment..."
-
 deactivate
 echo "✅ Done."
